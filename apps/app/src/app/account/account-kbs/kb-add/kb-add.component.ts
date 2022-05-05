0d4fa713136e3ddd1dbef38fbde372100fdc0a12
Eric BREHAULT <ebrehault@gmail.com>
2022-05-05 09:21:25 +0200

    fix anonymization value (#16)

--- apps/app/src/app/account/account-kbs/kb-add/kb-add.component.ts
+++ apps/app/src/app/account/account-kbs/kb-add/kb-add.component.ts
@@ -59,7 +59,7 @@ export class KbAddComponent {
       title: this.kbForm.value.title,
       description: this.kbForm.value.description,
       sentence_embedder: this.languageMode[0] === 'multilingual' ? 'multilingual' : this.kbForm.value.selectedLanguage,
-      anonymization: this.useAnonymization[0] === 'yes' ? 'anonymization' : '',
+      anonymization: this.useAnonymization[0] === 'yes' ? 'multilingual' : '',
     });
   }
 
