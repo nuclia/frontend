a82b43dbb508ae11aad714eb99e8682bb823b559
oriol <o.perramon@iskra.cat>
2022-05-11 16:16:19 +0200

    play encoded videos instead of source videos

--- libs/search-widget/src/viewer/previewers/Player.svelte
+++ libs/search-widget/src/viewer/previewers/Player.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
   import { onDestroy } from 'svelte';
-  import { getRegionalBackend } from '../../core/api';
+  import { getTempToken, isPrivateKnowledgeBox } from '../../core/api';
 
   export let src: string;
   export let type: string;
-  export let time: number = null;
+  export let time: number | undefined;
   let firstLoad: boolean = false;
-  let currentTime: number = null;
+  let currentTime: number | undefined;
   let paused: boolean = true;
   let player: HTMLMediaElement;
   let isVideo: boolean;
 
+  let dashPlayer: any;
+  let isDashVideo: boolean;
+
   onDestroy(() => {
     if (player && !paused) {
       player.pause();
+    }
+    if (dashPlayer) {
+      dashPlayer.reset();
+      dashPlayer.destroy();
     }
   });
 
@@ -21,15 +28,50 @@
     currentTime = time;
     play();
   }
-
-  $: isVideo = type.slice(0, 5) === 'video';
+  $: isVideo = type.slice(0, 5) === 'video' || type === 'text/xml';
+  $: isDashVideo = type === 'text/xml';
 
   const play = () => {
     if (paused) {
       player.play();
     }
   };
+
+  const onLoadDash = () => {
+    if (isPrivateKnowledgeBox()) {
+      getTempToken().subscribe((token) => {
+        initDash(token);
+      });
+    } else {
+      return initDash();
+    }
+  };
+
+  const initDash = (token?: string) => {
+    const dashjs = (window as any)['dashjs'];
+    if (dashjs) {
+      dashPlayer = dashjs.MediaPlayer().create();
+      dashPlayer.extend('RequestModifier', () => {
+        return {
+          modifyRequestHeader: (xhr: any) => xhr,
+          modifyRequestURL: (url: string) => {
+            if (token && url.indexOf('eph-token') < 0) {
+              return url + `?eph-token=${token}`;
+            }
+            return url;
+          },
+        };
+      });
+      dashPlayer.initialize(player, src, true);
+    }
+  };
 </script>
+
+<svelte:head>
+  {#if isDashVideo}
+    <script src="https://cdn.dashjs.org/v4.3.0/dash.all.min.js" on:load={onLoadDash}></script>
+  {/if}
+</svelte:head>
 
 <div class="player">
   {#if isVideo}
