894be799f8e1d9434b1c5b8b2156644c9abd7dab
operramon <52449656+operramon@users.noreply.github.com>
2022-05-17 11:31:59 +0200

    refresh calculation (#34)

--- libs/sdk-core/src/lib/auth/auth.ts
+++ libs/sdk-core/src/lib/auth/auth.ts
@@ -124,12 +124,12 @@ export class Authentication implements IAuthentication {
       const helper = new JwtHelper(token);
       const expiration = helper.getTokenExpirationDate()?.getTime();
       if (expiration) {
-        const nowInSeconds = new Date().getTime() / 1000;
-        if (expiration < nowInSeconds) {
+        const now = new Date().getTime();
+        if (expiration < now) {
           this.logout();
         } else {
           // we refresh the token 2 days before expiration (or immediately if it's less than 2 days)
-          const timeout = Math.max(expiration - nowInSeconds - 48 * 60 * 60, 0);
+          const timeout = Math.max(expiration - now - 48 * 60 * 60 * 1000, 0);
           this.timerSubscription?.unsubscribe();
           this.timerSubscription = timer(timeout)
             .pipe(switchMap(() => this.refresh()))
