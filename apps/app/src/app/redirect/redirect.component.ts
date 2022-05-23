a3a7a3c85ef6581dbcfd2112d8af5e7945bbcea3
Eric BREHAULT <ebrehault@gmail.com>
2022-05-23 16:46:11 +0200

    avoid error if no redirect param

--- apps/app/src/app/redirect/redirect.component.ts
+++ apps/app/src/app/redirect/redirect.component.ts
@@ -9,7 +9,7 @@ import { take } from 'rxjs';
 export class RedirectComponent {
   constructor(private sdk: SDKService, private route: ActivatedRoute) {
     this.route.queryParams.pipe(take(1)).subscribe((params) => {
-      let redirectUrl: string = params['redirect'];
+      let redirectUrl: string = params['redirect'] || '';
       redirectUrl += redirectUrl.includes('?') ? '&' : '?';
       const tokens = `access_token=${this.sdk.nuclia.auth.getToken()}&refresh_token=${this.sdk.nuclia.auth.getRefreshToken()}`;
       location.href = redirectUrl + tokens;
