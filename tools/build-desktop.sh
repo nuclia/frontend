731268aa6dce10acb8a45af5aed2a702e34c80e7
Eric BREHAULT <ebrehault@gmail.com>
2022-06-02 16:18:37 +0200

    Ericbrehault/sc 1439/nda installer 2 (#56)
    * try to build NDA on github
    
    * random change
    
    * fix script

--- tools/build-desktop.sh
+++ tools/build-desktop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-nx build desktop -c stage
-nx build desktop-electron -c production
+./node_modules/.bin/nx build desktop -c stage
+./node_modules/.bin/nx build desktop-electron -c production
 if [ -e dist/apps/desktop-electron/main.preload.ts.js ]
 then
   mv dist/apps/desktop-electron/main.preload.ts.js dist/apps/desktop-electron/main.preload.js
 fi
-nx run desktop-electron:make
+./node_modules/.bin/nx run desktop-electron:make
