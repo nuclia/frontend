ec63e03608c1687ad959e6cccbf4856816a876b4
mpellerin42 <mpellerin42@gmail.com>
2022-06-16 15:38:50 +0200

    Add activity button to understanding API key

--- apps/app/src/app/account/account-nua/account-nua.component.ts
+++ apps/app/src/app/account/account-nua/account-nua.component.ts
@@ -8,7 +8,6 @@ import { AccountNUAService } from './account-nua.service';
 import { ClientDialogComponent, ClientDialogData } from './client-dialog/client-dialog.component';
 import { TokenDialogComponent } from '../../components/token-dialog/token-dialog.component';
 
-
 @Component({
   selector: 'app-account-nua',
   templateUrl: './account-nua.component.html',
@@ -16,35 +15,32 @@ import { TokenDialogComponent } from '../../components/token-dialog/token-dialog
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccountNUAComponent {
-
   @Input() zones: Zone[] | undefined;
 
   clients = this.nua.clients;
   expanded: string[] = [];
   unsubscribeAll = new Subject<void>();
 
-  constructor(
-    private nua: AccountNUAService,
-    private dialog: MatDialog,
-    private cdr: ChangeDetectorRef
-  ) { 
+  constructor(private nua: AccountNUAService, private dialog: MatDialog, private cdr: ChangeDetectorRef) {
     this.nua.updateClients();
   }
 
   askConfirmation(client: NUAClient): void {
-    this.dialog.open(STFConfirmComponent, {
-      width: '480px',
-      data: {
-        title: 'generic.alert',
-        message: 'account.token_generation_warning',
-        confirmText: 'account.new_code',
-        minWidthButtons: '110px'
-      }
-    }).afterClosed()
+    this.dialog
+      .open(STFConfirmComponent, {
+        width: '480px',
+        data: {
+          title: 'generic.alert',
+          message: 'account.token_generation_warning',
+          confirmText: 'account.new_code',
+          minWidthButtons: '110px',
+        },
+      })
+      .afterClosed()
       .pipe(
         takeUntil(this.unsubscribeAll),
         filter((result) => !!result),
-        switchMap(() => this.nua.renewClient(client.client_id))
+        switchMap(() => this.nua.renewClient(client.client_id)),
       )
       .subscribe(({ token }) => {
         this.showToken(token);
@@ -52,21 +48,27 @@ export class AccountNUAComponent {
       });
   }
 
+  goToActivity(client: NUAClient): void {
+    console.log('Go to Activity logs for', client);
+  }
+
   showToken(token: string) {
     this.dialog.open(TokenDialogComponent, {
       width: '780px',
       data: {
-        token: token
-      }
+        token: token,
+      },
     });
   }
 
   openDialog(zones: Zone[], client?: NUAClient) {
     const data: ClientDialogData = { zones, client };
-    this.dialog.open(ClientDialogComponent, {
-      width: '780px',
-      data: data
-    }).afterClosed()
+    this.dialog
+      .open(ClientDialogComponent, {
+        width: '780px',
+        data: data,
+      })
+      .afterClosed()
       .pipe(
         takeUntil(this.unsubscribeAll),
         filter((result) => typeof result === 'string'),
@@ -78,19 +80,20 @@ export class AccountNUAComponent {
   }
 
   deleteClient(id: string) {
-    this.nua.deleteClient(id).subscribe(() => { this.nua.updateClients() });
+    this.nua.deleteClient(id).subscribe(() => {
+      this.nua.updateClients();
+    });
   }
 
   toggleClient(id: string): void {
     if (this.isExpanded(id)) {
-      this.expanded = this.expanded.filter(item => item !== id);
-    }
-    else {
+      this.expanded = this.expanded.filter((item) => item !== id);
+    } else {
       this.expanded = [...this.expanded, id];
     }
     this.cdr?.markForCheck();
   }
-  
+
   isExpanded(id: string): boolean {
     return this.expanded.includes(id);
   }
