6a014ca4beee576827702dab246f6849f1139577
oriol <o.perramon@iskra.cat>
2022-07-01 17:13:51 +0200

    Remove old unused components

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,13 +2,8 @@
  * Public API Surface of @flaps/common
  */
 
-export * from './lib/page-policy/page-policy.module';
-export * from './lib/page-policy/page-policy.component';
-export * from './lib/page-terms/page-terms.module';
-export * from './lib/page-terms/page-terms.component';
 export * from './lib/user/user.module';
 export * from './lib/useful/useful.module';
-export * from './lib/log/log.module';
 export * from './lib/page-not-found/page-not-found.module';
 export * from './lib/page-not-found/page-not-found.component';
 export * from './lib/sidebar/sidebar.module';
