e4b5bd1419d378bb6fe8d01dfb811fd62258e8be
oriol <o.perramon@iskra.cat>
2022-07-01 17:12:55 +0200

    Fix auth lib tests

--- libs/auth/src/lib/user.service.spec.ts
+++ libs/auth/src/lib/user.service.spec.ts
@@ -1,9 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { SDKService } from './sdk.service';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: SDKService,
+          useValue: {
+            nuclia: {
+              auth: {
+                logout: () => {},
+                isAuthenticated: () => of(),
+              },
+              db: { getWelcome: () => of() },
+            },
+          },
+        },
+      ],
+    }),
+  );
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
