f2e0f3f77e87d3784955be6cd11d38a6b9501814
mpellerin42 <mpellerin42@gmail.com>
2022-07-08 10:08:46 +0200

    Fix home page counts

--- apps/desktop/src/app/home/home.component.ts
+++ apps/desktop/src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { map, take } from 'rxjs';
+import { map, Observable, take } from 'rxjs';
 import { FileStatus } from '../sync/models';
 import { SyncService } from '../sync/sync.service';
 import { Router } from '@angular/router';
 
 type SectionType = 'pending' | 'active' | 'completed';
+
+interface SectionSync {
+  date: string;
+  from: string;
+  to: string;
+  total: number;
+  progress: number;
+  started: boolean | undefined;
+  completed: boolean | undefined;
+}
 
 @Component({
   selector: 'nde-home',
@@ -13,7 +23,7 @@ type SectionType = 'pending' | 'active' | 'completed';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  private queue = this.sync.queue.pipe(
+  private queue: Observable<SectionSync[]> = this.sync.queue.pipe(
     map((syncs) =>
       syncs.map((sync) => ({
         date: sync.date.slice(0, 10),
@@ -27,13 +37,18 @@ export class HomeComponent {
     ),
   );
   pending = this.queue.pipe(map((syncs) => syncs.filter((sync) => !sync.completed && !sync.started)));
+  pendingCount = this.pending.pipe(map((syncs) => this.getFileCount(syncs)));
+
   active = this.queue.pipe(map((syncs) => syncs.filter((sync) => !sync.completed && sync.started)));
+  activeCount = this.active.pipe(map((syncs) => this.getFileCount(syncs)));
+
   completed = this.queue.pipe(map((syncs) => syncs.filter((sync) => sync.completed)));
+  completedCount = this.completed.pipe(map((syncs) => this.getFileCount(syncs)));
 
   sections = [
-    { title: 'home.pending', type: 'pending', items: this.pending },
-    { title: 'home.active', type: 'active', items: this.active },
-    { title: 'home.completed', type: 'completed', items: this.completed },
+    { title: 'home.pending', type: 'pending', items: this.pending, count: this.pendingCount },
+    { title: 'home.active', type: 'active', items: this.active, count: this.activeCount },
+    { title: 'home.completed', type: 'completed', items: this.completed, count: this.completedCount },
   ];
 
   selected: SectionType | null = null;
@@ -52,5 +67,12 @@ export class HomeComponent {
 
   addUpload() {
     this.router.navigateByUrl('/add-upload');
+  }
+
+  private getFileCount(syncs: SectionSync[]): number {
+    return syncs.reduce((total, sync) => {
+      total += sync.total;
+      return total;
+    }, 0);
   }
 }
