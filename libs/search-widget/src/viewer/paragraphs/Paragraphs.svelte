0ac25895b8b85064a683c237e120b93fe65e9efb
oriol <o.perramon@iskra.cat>
2022-07-26 10:20:46 +0200

    Always show labels

--- libs/search-widget/src/viewer/paragraphs/Paragraphs.svelte
+++ libs/search-widget/src/viewer/paragraphs/Paragraphs.svelte
@@ -2,12 +2,10 @@
   import type { WidgetParagraph, PdfWidgetParagraph, MediaWidgetParagraph } from '../../core/models';
   import { PreviewKind } from '../../core/models';
   import { formatTime } from '../../core/utils';
-  import { nucliaState } from '../../core/store';
   import { viewerState, viewerStore, selectedParagraphIndex } from './../store';
   import Paragraph from './Paragraph.svelte';
   import ParagraphWithIcon from './ParagraphWithIcon.svelte';
   import { ParagraphIcon } from './ParagraphWithIcon.svelte';
-  import { filter, map } from 'rxjs';
 
   export let paragraphs: WidgetParagraph[] = [];
   const onlySelected = viewerState.onlySelected;
@@ -19,10 +17,6 @@
       paragraph: paragraph.paragraph,
     });
   };
-  const showLabels = nucliaState().widget.pipe(
-    filter((widget) => !!widget),
-    map((widget) => widget.features.editLabels)
-  )
 </script>
 
 <div class="paragraph-list">
@@ -34,7 +28,7 @@
             text={paragraph.text}
             textIcon={'p. ' + (paragraph.page + 1)}
             icon={ParagraphIcon.EXPAND}
-            labels={$showLabels &&  paragraph.paragraph.classifications || []}
+            labels={paragraph.paragraph.classifications || []}
             active={$selectedParagraphIndex === i}
             on:click={() => previewParagraph(paragraph)}
           />
@@ -43,11 +37,11 @@
             text={paragraph.text}
             textIcon={formatTime(paragraph.time)}
             icon={ParagraphIcon.PLAY}
-            labels={$showLabels && paragraph.paragraph.classifications || []}
+            labels={paragraph.paragraph.classifications || []}
             on:click={() => previewParagraph(paragraph)}
           />
         {:else}
-          <Paragraph labels={$showLabels && paragraph.paragraph.classifications || []}>
+          <Paragraph labels={paragraph.paragraph.classifications || []}>
             <span slot="content">{paragraph.text}</span>
           </Paragraph>
         {/if}
