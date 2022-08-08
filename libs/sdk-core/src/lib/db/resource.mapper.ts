6501b54fcf4dbf642266bf27afc576a5cd3664a5
oriol <o.perramon@iskra.cat>
2022-08-08 10:50:31 +0200

    Replace "user_metadata" property

--- libs/sdk-core/src/lib/db/resource.mapper.ts
+++ libs/sdk-core/src/lib/db/resource.mapper.ts
@@ -16,8 +16,7 @@ export const resourceToAlgoliaFormat = (resource: Resource, backend: string): Al
       .map((extracted) => extracted.text as string),
   };
 
-  // The typing is incorrect: the type is usermetadata while what we really got is user_metadata (but usermetadata is in use in other places, so we need to be careful when fixing it)
-  (resource as any).user_metadata?.classifications?.forEach((classification: Classification) => {
+  resource.usermetadata?.classifications?.forEach((classification: Classification) => {
     if (classification.labelset && classification.label) {
       // Adding labels as properties of the final json
       (record as any)[classification.labelset] = classification.label;
