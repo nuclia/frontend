19120421647b6c986616822c7bcdb32dc9699605
mpellerin42 <mpellerin42@gmail.com>
2022-08-12 17:03:58 +0200

    Remove STF tag component from component lib
     It was not used anywhere

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -5,6 +5,4 @@
 export * from './lib/confirm/confirm.module';
 export * from './lib/confirm/confirm.component';
 export * from './lib/context-menu/context-menu.module';
-export * from './lib/tag/tag.module';
-export * from './lib/tag/tag/tag.component';
 export * from './lib/user-avatar/user.module';
