e7368889122d76558eebb4ee357f4c7994d61f44
operramon <52449656+operramon@users.noreply.github.com>
2022-08-26 14:55:06 +0200

    [sc-2133] Allow to select label from Chrome extension (#210)
    * [sc-2133] Allow to select label from Chrome extension
    
    * Add help messge
    
    * Keep menu in sync with actual labelsets
    
    * Adjust Google translations
    
    * Add zone field

--- libs/chrome-ext/src/lib/background.js
+++ libs/chrome-ext/src/lib/background.js
@@ -4,34 +4,111 @@ try {
   console.error(e);
 }
 
+const MENU_LABELSET_PREFIX = `NUCLIA_LABELSET_`;
+const KB_SETTINGS = ['NUCLIA_KB', 'NUCLIA_ZONE', 'NUCLIA_KEY'];
+
+const baseMenuOptions = {
+  targetUrlPatterns: ['https://*/*'],
+  contexts: ['link'],
+};
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'NucliaContextMenu',
-    title: 'Upload to Nuclia',
-    targetUrlPatterns: ['https://*/*'],
-    contexts: ['link'],
-  });
+  createMenu();
 });
 
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.action === 'UPDATE_MENU') {
+    createMenu();
+  }
+});
+
+function createMenu() {
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: `NUCLIA_UPLOAD`,
+      title: 'Upload link to Nuclia',
+      ...baseMenuOptions,
+    });
+    chrome.storage.local.get(KB_SETTINGS, (settings) => {
+      if (settings.NUCLIA_KB && settings.NUCLIA_ZONE && settings.NUCLIA_KEY) {
+        getLabels(settings).subscribe((labelsets) => {
+          if (labelsets.length > 0) {
+            createSubmenus(labelsets);
+          }
+        });
+      }
+    });
+  });
+}
+
+function createSubmenus(labelsets) {
+  chrome.contextMenus.create({
+    id: `NUCLIA_UPLOAD_WITH_LABEL`,
+    title: 'Upload link to Nuclia with label…',
+    ...baseMenuOptions,
+  });
+  labelsets.forEach(([key, labelset]) => {
+    const labelsetMenuId = `${MENU_LABELSET_PREFIX}${key}`;
+    chrome.contextMenus.create({
+      id: labelsetMenuId,
+      title: labelset.title,
+      parentId: 'NUCLIA_UPLOAD_WITH_LABEL',
+      ...baseMenuOptions,
+    });
+    labelset.labels.forEach((label) => {
+      chrome.contextMenus.create({
+        id: `${labelsetMenuId}_${label.title}`,
+        title: label.title,
+        parentId: labelsetMenuId,
+        ...baseMenuOptions,
+      });
+    });
+  });
+}
+
 chrome.contextMenus.onClicked.addListener((info) => {
-  chrome.storage.local.get(['NUCLIA_KB', 'NUCLIA_KEY'], ({ NUCLIA_KB, NUCLIA_KEY }) => {
-    if (NUCLIA_KB && NUCLIA_KEY) {
-      uploadLink(info.linkUrl, NUCLIA_KB, NUCLIA_KEY);
+  chrome.storage.local.get(KB_SETTINGS, (settings) => {
+    if (settings.NUCLIA_KB && settings.NUCLIA_ZONE && settings.NUCLIA_KEY) {
+      let labels = [];
+      if (info.parentMenuItemId && info.parentMenuItemId.startsWith(MENU_LABELSET_PREFIX)) {
+        labels.push({
+          labelset: info.parentMenuItemId.split(MENU_LABELSET_PREFIX)[1],
+          label: info.menuItemId.split(info.parentMenuItemId + '_')[1],
+        });
+      }
+      uploadLink(settings, info.linkUrl, labels);
+      createMenu(); // Keep menu in sync with actual labelsets
     } else {
       chrome.runtime.openOptionsPage();
     }
   });
 });
 
-function uploadLink(url, kb, key) {
-  const nuclia = new NucliaSDK.Nuclia({
-    backend: 'https://stashify.cloud/api',
-    knowledgeBox: kb,
-    zone: 'europe-1',
-    apiKey: key,
-  });
-  nuclia.db
-    .getKnowledgeBox()
-    .pipe(rxjs.switchMap((kb) => kb.createLinkResource({ uri: url })))
+function getLabels(settings) {
+  return getSDK(settings)
+    .knowledgeBox.getLabels()
+    .pipe(
+      rxjs.map((labels) =>
+        Object.entries(labels)
+          .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+          .filter(([key, labelset]) => labelset.kind.length === 0 || labelset.kind.includes('RESOURCES'))
+          .filter(([key, labelset]) => labelset.labels.length > 0),
+      ),
+    );
+}
+
+function uploadLink(settings, url, labels) {
+  getSDK(settings)
+    .db.getKnowledgeBox()
+    .pipe(rxjs.switchMap((kb) => kb.createLinkResource({ uri: url }, { classifications: labels })))
     .subscribe();
+}
+
+function getSDK(settings) {
+  return new NucliaSDK.Nuclia({
+    backend: 'https://nuclia.cloud/api',
+    knowledgeBox: settings.NUCLIA_KB,
+    zone: settings.NUCLIA_ZONE,
+    apiKey: settings.NUCLIA_KEY,
+  });
 }
--- libs/chrome-ext/src/lib/options/options.js
+++ libs/chrome-ext/src/lib/options/options.js
@@ -1,9 +1,13 @@
+const DEFAULT_ZONE = 'europe-1';
+
 function saveOptions() {
   const kb = document.getElementById('kb').value;
+  const zone = document.getElementById('zone').value;
   const key = document.getElementById('key').value;
-  chrome.storage.local.set({ 'NUCLIA_KB': kb, 'NUCLIA_KEY': key }, () => {
+  chrome.storage.local.set({ NUCLIA_KB: kb, NUCLIA_ZONE: zone, NUCLIA_KEY: key }, () => {
     const message = document.querySelector('.message');
     message.textContent = 'Options saved';
+    chrome.runtime.sendMessage({ action: 'UPDATE_MENU' });
     setTimeout(() => {
       message.textContent = '';
     }, 1500);
@@ -11,10 +15,14 @@ function saveOptions() {
 }
 
 function restoreOptions() {
-  chrome.storage.local.get({ 'NUCLIA_KB': '', 'NUCLIA_KEY': '' }, ({ NUCLIA_KB, NUCLIA_KEY }) => {
-    document.getElementById('kb').value = NUCLIA_KB;
-    document.getElementById('key').value = NUCLIA_KEY;
-  });
+  chrome.storage.local.get(
+    { NUCLIA_KB: '', NUCLIA_ZONE: DEFAULT_ZONE, NUCLIA_KEY: '' },
+    ({ NUCLIA_KB, NUCLIA_ZONE, NUCLIA_KEY }) => {
+      document.getElementById('kb').value = NUCLIA_KB;
+      document.getElementById('zone').value = NUCLIA_ZONE;
+      document.getElementById('key').value = NUCLIA_KEY;
+    },
+  );
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
