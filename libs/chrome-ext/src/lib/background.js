ea74ee3475220b3445e2e04222bdde1cfbe8a26e
Eric BREHAULT <ebrehault@gmail.com>
2022-08-31 13:55:48 +0200

    allow to import youtube channels/playlists

--- libs/chrome-ext/src/lib/background.js
+++ libs/chrome-ext/src/lib/background.js
@@ -5,11 +5,11 @@ try {
 }
 
 const MENU_LABELSET_PREFIX = `NUCLIA_LABELSET_`;
-const KB_SETTINGS = ['NUCLIA_KB', 'NUCLIA_ZONE', 'NUCLIA_KEY'];
+const SETTINGS = ['NUCLIA_KB', 'NUCLIA_ZONE', 'NUCLIA_KEY', 'YOUTUBE_KEY'];
 
 const baseMenuOptions = {
   targetUrlPatterns: ['https://*/*'],
-  contexts: ['link'],
+  contexts: ['link', 'page'],
 };
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -20,16 +20,19 @@ chrome.runtime.onMessage.addListener((request) => {
   if (request.action === 'UPDATE_MENU') {
     createMenu();
   }
+  if (request.action === 'UPLOAD_LIST') {
+    uploadLinksList(request.selection, request.labels);
+  }
 });
 
 function createMenu() {
   chrome.contextMenus.removeAll(() => {
     chrome.contextMenus.create({
       id: `NUCLIA_UPLOAD`,
-      title: 'Upload link to Nuclia',
+      title: 'Upload to Nuclia',
       ...baseMenuOptions,
     });
-    chrome.storage.local.get(KB_SETTINGS, (settings) => {
+    chrome.storage.local.get(SETTINGS, (settings) => {
       if (settings.NUCLIA_KB && settings.NUCLIA_ZONE && settings.NUCLIA_KEY) {
         getLabels(settings).subscribe((labelsets) => {
           if (labelsets.length > 0) {
@@ -67,7 +70,7 @@ function createSubmenus(labelsets) {
 }
 
 chrome.contextMenus.onClicked.addListener((info) => {
-  chrome.storage.local.get(KB_SETTINGS, (settings) => {
+  chrome.storage.local.get(SETTINGS, (settings) => {
     if (settings.NUCLIA_KB && settings.NUCLIA_ZONE && settings.NUCLIA_KEY) {
       let labels = [];
       if (info.parentMenuItemId && info.parentMenuItemId.startsWith(MENU_LABELSET_PREFIX)) {
@@ -76,8 +79,24 @@ chrome.contextMenus.onClicked.addListener((info) => {
           label: info.menuItemId.split(info.parentMenuItemId + '_')[1],
         });
       }
-      uploadLink(settings, info.linkUrl, labels);
-      createMenu(); // Keep menu in sync with actual labelsets
+      if (info.linkUrl) {
+        uploadLink(settings, info.linkUrl, labels);
+        createMenu(); // Keep menu in sync with actual labelsets
+      } else if (info.pageUrl) {
+        const url = info.pageUrl;
+        if (url.includes('youtube.com/') && settings.YOUTUBE_KEY) {
+          if (url.includes('youtube.com/channel/')) {
+            const channelId = url.split('/channel/')[1].split('/')[0];
+            getChannelVideos(settings, channelId, labels);
+          }
+          if (url.includes('youtube.com/playlist?list=')) {
+            const playlistId = url.split('/playlist?list=')[1].split('&')[0];
+            getPlaylistVideos(settings, playlistId, labels);
+          }
+        } else {
+          chrome.runtime.openOptionsPage();
+        }
+      }
     } else {
       chrome.runtime.openOptionsPage();
     }
@@ -104,6 +123,52 @@ function uploadLink(settings, url, labels) {
     .subscribe();
 }
 
+function getChannelVideos(settings, channelId, labels) {
+  loadPaginated(
+    `https://www.googleapis.com/youtube/v3/search?key=${settings.YOUTUBE_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=50`,
+  )
+    .then((items) =>
+      items
+        .filter((video) => video.id.kind === 'youtube#video')
+        .map((video) => ({
+          id: video.id.videoId,
+          title: video.snippet.title,
+          date: video.snippet.publishedAt,
+        }))
+        .sort((a, b) => b.date.localeCompare(a.date)),
+    )
+    .then((videos) => selectVideos(videos, labels));
+}
+
+function getPlaylistVideos(settings, playlistId, labels) {
+  loadPaginated(
+    `https://www.googleapis.com/youtube/v3/playlistItems?key=${settings.YOUTUBE_KEY}&playlistId=${playlistId}&part=snippet,id&order=date&maxResults=50`,
+  )
+    .then((items) =>
+      items
+        .filter((video) => video.snippet.resourceId.kind === 'youtube#video')
+        .map((video) => ({
+          id: video.snippet.resourceId.videoId,
+          title: video.snippet.title,
+          date: video.snippet.publishedAt,
+        }))
+        .sort((a, b) => b.date.localeCompare(a.date)),
+    )
+    .then((videos) => selectVideos(videos, labels));
+}
+
+function selectVideos(videos, labels) {
+  chrome.storage.local.set({ videos });
+  chrome.tabs.create({ url: `youtube/selection.html?labels=${JSON.stringify(labels)}` });
+}
+
+function uploadLinksList(list, labels) {
+  if (!(list && list.length && list.length > 0)) {
+    return;
+  }
+  chrome.storage.local.get(SETTINGS, (settings) => list.forEach((url) => uploadLink(settings, url, labels)));
+}
+
 function getSDK(settings) {
   return new NucliaSDK.Nuclia({
     backend: 'https://nuclia.cloud/api',
@@ -111,4 +176,28 @@ function getSDK(settings) {
     zone: settings.NUCLIA_ZONE,
     apiKey: settings.NUCLIA_KEY,
   });
+}
+
+function loadPaginated(url, items = [], pageToken = '') {
+  return new Promise((resolve, reject) =>
+    fetch(pageToken ? `${url}&pageToken=${pageToken}` : url)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+        response
+          .json()
+          .then((data) => {
+            items = items.concat(data.items);
+
+            if (data.nextPageToken) {
+              loadPaginated(url, items, data.nextPageToken).then(resolve).catch(reject);
+            } else {
+              resolve(items);
+            }
+          })
+          .catch(reject);
+      })
+      .catch(reject),
+  );
 }
--- libs/chrome-ext/src/lib/options/options.js
+++ libs/chrome-ext/src/lib/options/options.js
@@ -4,7 +4,8 @@ function saveOptions() {
   const kb = document.getElementById('kb').value;
   const zone = document.getElementById('zone').value;
   const key = document.getElementById('key').value;
-  chrome.storage.local.set({ NUCLIA_KB: kb, NUCLIA_ZONE: zone, NUCLIA_KEY: key }, () => {
+  const youTubeKey = document.getElementById('youTubeKey').value;
+  chrome.storage.local.set({ NUCLIA_KB: kb, NUCLIA_ZONE: zone, NUCLIA_KEY: key, YOUTUBE_KEY: youTubeKey }, () => {
     const message = document.querySelector('.message');
     message.textContent = 'Options saved';
     chrome.runtime.sendMessage({ action: 'UPDATE_MENU' });
@@ -16,11 +17,12 @@ function saveOptions() {
 
 function restoreOptions() {
   chrome.storage.local.get(
-    { NUCLIA_KB: '', NUCLIA_ZONE: DEFAULT_ZONE, NUCLIA_KEY: '' },
-    ({ NUCLIA_KB, NUCLIA_ZONE, NUCLIA_KEY }) => {
+    { NUCLIA_KB: '', NUCLIA_ZONE: DEFAULT_ZONE, NUCLIA_KEY: '', YOUTUBE_KEY: '' },
+    ({ NUCLIA_KB, NUCLIA_ZONE, NUCLIA_KEY, YOUTUBE_KEY }) => {
       document.getElementById('kb').value = NUCLIA_KB;
       document.getElementById('zone').value = NUCLIA_ZONE;
       document.getElementById('key').value = NUCLIA_KEY;
+      document.getElementById('youTubeKey').value = YOUTUBE_KEY;
     },
   );
 }
