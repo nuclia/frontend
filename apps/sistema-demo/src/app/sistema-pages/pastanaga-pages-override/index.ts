754895b2a5c358de2f3e5257f4bdc45127c20e65
mpellerin42 <mpellerin42@gmail.com>
2022-09-12 14:29:55 +0200

    Remove custom palette demo page

--- apps/sistema-demo/src/app/sistema-pages/pastanaga-pages-override/index.ts
+++ apps/sistema-demo/src/app/sistema-pages/pastanaga-pages-override/index.ts
@@ -2,7 +2,6 @@ export * from './sistema-buttons/sistema-buttons.component';
 export * from './sistema-confirmation-dialog/sistema-confirmation-dialog.component';
 export * from './sistema-icons/sistema-icons.component';
 export * from './sistema-modal';
-export * from './sistema-palette/sistema-palette.component';
 export * from './sistema-scrollbar/sistema-scrollbar.component';
 export * from './sistema-tables/sistema-table.component';
 export * from './sistema-toasts/sistema-toast.component';
