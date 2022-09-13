8b15c5adba5eddeff7da515754f5b3eacf67475d
oriol <o.perramon@iskra.cat>
2022-09-13 16:57:19 +0200

    [sc-2184] Allow to authenticate to Nuclia from Chrome extension - Replace api key field with a login button - Replace knowledge box Id field with an account and knowledge box selectors

---
+++ libs/chrome-ext/src/lib/api.js
@@ -0,0 +1,36 @@
+
+const API_URL = 'https://nuclia.cloud/api';
+const DASHBOARD_URL = 'https://nuclia.cloud';
+
+const SETTINGS = { NUCLIA_ACCOUNT: '', NUCLIA_KB: '', NUCLIA_TOKEN: '', YOUTUBE_KEY: '' };
+
+// Service workers doesn't support localStorage, so a custom one is created.
+localStorage = getStorage();
+
+function getSDK(token) {
+  const sdk = new NucliaSDK.Nuclia({ backend: API_URL });
+  sdk.auth.authenticate({
+    access_token: token,
+    refresh_token: '', // At the moment we don't mind about token refresh
+  })
+  return sdk;
+}
+
+function getLoginUrl() {
+  const settingsUrl = chrome.runtime.getURL('options/options.html');
+  return `${DASHBOARD_URL}/redirect?redirect=${settingsUrl}&fromExtension=true`;
+}
+
+function getStorage() {
+  let _storage = {};
+  return {
+    getItem: (key) => _storage[key],
+    setItem: (key, value) => { _storage[key] = value },
+    removeItem: (key) => { _storage[key] = undefined },
+    clear: () => { _storage = {} },
+  }
+}
+
+function getSettings() {
+  return chrome.storage.local.get(SETTINGS);
+}
--- libs/chrome-ext/src/lib/options/options.js
+++ libs/chrome-ext/src/lib/options/options.js
@@ -1,13 +1,128 @@
-const DEFAULT_ZONE = 'europe-1';
+let nuclia;
 
-function saveOptions() {
+let data = {
+  logged: false,
+  showWelcome: false,
+  accounts: [],
+  kbs: {},
+  validKb: false,
+};
+
+function init() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const token = urlParams.get('access_token');
+  data.showWelcome = urlParams.has('access_token');
+  if (token) {
+    chrome.storage.local.set({ NUCLIA_TOKEN: token }).then(() => fetchData());
+  } else {
+    fetchData();
+  }
+}
+
+function fetchData() {
+  rxjs
+    .from(getSettings())
+    .pipe(
+      rxjs.tap((settings) => {
+        nuclia = getSDK(settings.NUCLIA_TOKEN);
+      }),
+      rxjs.switchMap(() =>
+        nuclia.db.getWelcome().pipe(
+          rxjs.tap(() => {
+            data.logged = true;
+          }),
+          rxjs.catchError((e) => {
+            data.logged = false;
+            data.showWelcome = false;
+            chrome.storage.local.set({ NUCLIA_TOKEN: '' });
+            throw e;
+          }),
+        ),
+      ),
+      rxjs.switchMap(() => nuclia.db.getAccounts()),
+      rxjs.switchMap((accountsData) => {
+        data.accounts = accountsData;
+        return rxjs.forkJoin(
+          accountsData.map((account) =>
+            nuclia.db.getKnowledgeBoxes(account.slug).pipe(rxjs.map((kbs) => [account.slug, kbs])),
+          ),
+        );
+      }),
+      rxjs.switchMap((kbsData) => {
+        kbsData.forEach(([account, kbs]) => {
+          data.kbs[account] = kbs.filter((kb) => ['SOWNER', 'SCONTRIBUTOR'].includes(kb.role_on_kb));
+        });
+        return rxjs.from(getSettings());
+      }),
+    )
+    .subscribe({
+      next: (settings) => {
+        const validAccount = data.accounts.some((account) => account.slug === settings.NUCLIA_ACCOUNT);
+        data.validKb = validAccount && data.kbs[settings.NUCLIA_ACCOUNT].some((kb) => kb.slug === settings.NUCLIA_KB);
+        initUI();
+      },
+      error: () => initUI(),
+    });
+}
+
+function initUI() {
+  if (data.showWelcome && data.validKb) {
+    document.getElementById('welcome').classList.remove('hidden');
+  } else if (data.logged) {
+    setAccountOptions();
+    getSettings().then((settings) => {
+      document.getElementById('youTubeKey').value = settings.YOUTUBE_KEY;
+    });
+    document.getElementById('logged').classList.remove('hidden');
+  } else {
+    document.getElementById('auth').classList.remove('hidden');
+  }
+}
+
+function setAccountOptions() {
+  const accountSelect = document.getElementById('account');
+  accountSelect.innerHTML = '<option value="">Select account</option>';
+  data.accounts.forEach((account) => {
+    const option = document.createElement('option');
+    option.setAttribute('value', account.slug);
+    option.textContent = account.title;
+    accountSelect.append(option);
+  });
+  accountSelect.addEventListener('change', (event) => {
+    setKbOptions(event.target.value);
+  });
+  getSettings().then((settings) => {
+    if (data.validKb) {
+      accountSelect.value = settings.NUCLIA_ACCOUNT;
+    }
+    setKbOptions(accountSelect.value);
+  });
+}
+
+function setKbOptions(account) {
+  const kbSelect = document.getElementById('kb');
+  kbSelect.innerHTML = '<option value="">Select knowledge box</option>';
+  account ? kbSelect.removeAttribute('disabled') : kbSelect.setAttribute('disabled', '');
+  (data.kbs[account] || []).forEach((kb) => {
+    const option = document.createElement('option');
+    option.setAttribute('value', kb.slug);
+    option.textContent = kb.title;
+    kbSelect.append(option);
+  });
+  getSettings().then((settings) => {
+    if (data.validKb) {
+      kbSelect.value = settings.NUCLIA_KB;
+    }
+  });
+}
+
+function saveForm() {
+  const account = document.getElementById('account').value;
   const kb = document.getElementById('kb').value;
-  const zone = document.getElementById('zone').value;
-  const key = document.getElementById('key').value;
   const youTubeKey = document.getElementById('youTubeKey').value;
-  chrome.storage.local.set({ NUCLIA_KB: kb, NUCLIA_ZONE: zone, NUCLIA_KEY: key, YOUTUBE_KEY: youTubeKey }, () => {
-    const message = document.querySelector('.message');
-    message.textContent = 'Options saved';
+  chrome.storage.local.set({ NUCLIA_ACCOUNT: account, NUCLIA_KB: kb, YOUTUBE_KEY: youTubeKey }, () => {
+    const message = document.querySelector('.submit-message');
+    message.textContent = 'Settings saved';
     chrome.runtime.sendMessage({ action: 'UPDATE_MENU' });
     setTimeout(() => {
       message.textContent = '';
@@ -15,17 +130,15 @@ function saveOptions() {
   });
 }
 
-function restoreOptions() {
-  chrome.storage.local.get(
-    { NUCLIA_KB: '', NUCLIA_ZONE: DEFAULT_ZONE, NUCLIA_KEY: '', YOUTUBE_KEY: '' },
-    ({ NUCLIA_KB, NUCLIA_ZONE, NUCLIA_KEY, YOUTUBE_KEY }) => {
-      document.getElementById('kb').value = NUCLIA_KB;
-      document.getElementById('zone').value = NUCLIA_ZONE;
-      document.getElementById('key').value = NUCLIA_KEY;
-      document.getElementById('youTubeKey').value = YOUTUBE_KEY;
-    },
-  );
+function login() {
+  window.location.href = getLoginUrl();
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save').addEventListener('click', saveOptions);
+function close() {
+  chrome.tabs.getCurrent((tab) => chrome.tabs.remove(tab.id, () => {}));
+}
+
+document.addEventListener('DOMContentLoaded', init);
+document.getElementById('save').addEventListener('click', saveForm);
+document.getElementById('login').addEventListener('click', login);
+document.getElementById('close').addEventListener('click', close);
