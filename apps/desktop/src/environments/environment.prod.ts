2f586214661b06d8e140698cb0a18c69e2fc59a8
Eric BREHAULT <ebrehault@gmail.com>
2022-09-14 08:40:14 +0200

    use prod endpoint

--- apps/desktop/src/environments/environment.prod.ts
+++ apps/desktop/src/environments/environment.prod.ts
@@ -12,7 +12,7 @@ export const environment = {
       CLIENT_ID: '__DROPBOX_KEY__',
     },
     gdrive: {
-      endpoint: 'http://127.0.0.1:5000/gdrive/authorize',
+      endpoint: 'https://nuclia.cloud/api/external_auth/gdrive/authorize',
       API_KEY: '__GOOGLE_API_KEY__',
     },
     nucliacloud: {
