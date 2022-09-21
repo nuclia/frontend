61b1e4e06b2ffdc6bc8055affb94b519ca1c7df5
oriol <o.perramon@iskra.cat>
2022-09-21 17:37:06 +0200

    Fix format

--- libs/search-widget/src/_video-widget/SearchBar.svelte
+++ libs/search-widget/src/_video-widget/SearchBar.svelte
@@ -2,7 +2,7 @@
 
 <script lang="ts">
   import type { KBStates } from '@nuclia/core';
-  import { resetStore, nucliaStore } from "../core/store";
+  import { resetStore, nucliaStore } from '../core/store';
   import { initNuclia, resetNuclia } from '../core/api';
   import { onMount } from 'svelte';
   import { setCDN, coerceBooleanProperty, loadFonts, loadSvgSprite } from '../core/utils';
