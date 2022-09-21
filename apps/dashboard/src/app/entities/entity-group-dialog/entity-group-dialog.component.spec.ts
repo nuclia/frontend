09b6419a4a2490403a4688e3b71363976e5bff16
mpellerin42 <mpellerin42@gmail.com>
2022-09-21 11:25:57 +0200

    Fix unit tests

--- apps/dashboard/src/app/entities/entity-group-dialog/entity-group-dialog.component.spec.ts
+++ apps/dashboard/src/app/entities/entity-group-dialog/entity-group-dialog.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EntityGroupDialogComponent } from './entity-group-dialog.component';
+import { MockModule, MockProvider } from 'ng-mocks';
+import { SDKService } from '@flaps/core';
+import {
+  ModalRef,
+  PaButtonModule,
+  PaModalModule,
+  PaTextFieldModule,
+  PaTranslateModule,
+} from '@guillotinaweb/pastanaga-angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('EntityGroupDialogComponent', () => {
   let component: EntityGroupDialogComponent;
@@ -8,7 +18,21 @@ describe('EntityGroupDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MockModule(PaTranslateModule),
+        MockModule(PaModalModule),
+        MockModule(PaTextFieldModule),
+        MockModule(PaButtonModule),
+        MockModule(ReactiveFormsModule),
+      ],
       declarations: [EntityGroupDialogComponent],
+      providers: [
+        {
+          provide: ModalRef,
+          useValue: new ModalRef({ id: 1 }),
+        },
+        MockProvider(SDKService),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EntityGroupDialogComponent);
