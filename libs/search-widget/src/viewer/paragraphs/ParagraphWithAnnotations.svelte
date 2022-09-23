18355a3f7d6097a3d515a6d4b86bbd29156c112e
mpellerin42 <mpellerin42@gmail.com>
2022-09-23 15:55:20 +0200

    Update and remove entities

--- libs/search-widget/src/viewer/paragraphs/ParagraphWithAnnotations.svelte
+++ libs/search-widget/src/viewer/paragraphs/ParagraphWithAnnotations.svelte
@@ -2,9 +2,10 @@
   import Paragraph from './Paragraph.svelte';
   import { onDestroy, onMount } from 'svelte';
   import EntityFamilyMenu from '../menus/EntityFamilyMenu.svelte';
-  import { WidgetParagraph } from '../../core/models';
+  import { EntityGroup, WidgetParagraph } from '../../core/models';
   import { viewerStore } from '../viewer.store';
   import { map } from 'rxjs';
+  import { addCustomEntity, CustomEntity, removeCustomEntity, updateCustomEntity } from '../stores/annotation.store';
 
   export let paragraph: WidgetParagraph;
   export let paragraphId: string;
@@ -15,10 +16,15 @@
     let textWithMarks = '';
     let currentIndex = 0;
     entities.filter(entity => entity.paragraphId === paragraphId).forEach((entity) => {
-      textWithMarks += `${paragraph.text.slice(currentIndex, entity.start)}<mark family="${entity.entityFamilyId}">${paragraph.text.slice(entity.start, entity.end)}</mark>`;
+      textWithMarks += `${paragraph.text.slice(currentIndex, entity.start)}<mark family="${entity.entityFamilyId}" start="${entity.start}" end="${entity.end}" entity="${entity.entity}" paragraphId="${entity.paragraphId}">${paragraph.text.slice(entity.start, entity.end)}</mark>`;
       currentIndex = entity.end;
     });
     textWithMarks += paragraph.text.slice(currentIndex);
+
+    setTimeout(() => {
+      cleanUpMarkListener();
+      setupMarkListener();
+    });
     return textWithMarks;
   }));
   let contentContainer: HTMLElement;
@@ -27,24 +33,37 @@
   let menuPosition;
   let menuHeight;
   let selectedText;
-  let currentEntityFamily;
+  let selectedEntity: CustomEntity | undefined;
 
-  onMount(() => {
-    contentContainer.addEventListener('contextmenu', contextMenu);
+  function setupMarkListener() {
     contentContainer.querySelectorAll('mark[family]').forEach(mark => {
       mark.addEventListener('click', clickOnEntity);
     });
-  });
+  }
 
-  onDestroy(() => {
+  function cleanUpMarkListener() {
     contentContainer.querySelectorAll('mark[family]').forEach(mark => {
       mark.removeEventListener('click', clickOnEntity);
     });
+  }
+
+  onMount(() => {
+    contentContainer.addEventListener('contextmenu', contextMenu);
+  });
+
+  onDestroy(() => {
+    cleanUpMarkListener();
     contentContainer.removeEventListener('contextmenu', contextMenu);
   });
 
   const clickOnEntity = (event) => {
-    currentEntityFamily = event.target.getAttribute('family');
+    selectedEntity = {
+      entityFamilyId: event.target.getAttribute('family'),
+      entity: event.target.getAttribute('entity'),
+      paragraphId,
+      start: parseInt(event.target.getAttribute('start')),
+      end: parseInt(event.target.getAttribute('end'))
+    };
     openMenu(event);
   }
 
@@ -64,30 +83,49 @@
   const contextMenu = (event: MouseEvent) => {
     const selection = document.getSelection();
     if (selection.type === 'Range') {
-      selectedText = {
-        cleanedText: getCleanedUpSelectedText(selection),
-        start: selection.anchorOffset,
-        end: selection.focusOffset,
-      };
       event.preventDefault();
+      const range = selection.getRangeAt(0);
+      const highlightedText = selection.anchorNode?.textContent.slice(range.startOffset, range.endOffset);
+      let start = range.startOffset;
+      let end = range.endOffset;
+      if (paragraph.text.slice(start, end) !== highlightedText) {
+        // document.getSelection() returns text from the last tag, so if users selects text after a marked entity in the paragraph,
+        // the range is relative to the last mark and not to the beginning of the paragraph.
+        start = paragraph.text.indexOf(highlightedText);
+        end = start + (range.endOffset - range.startOffset);
+      }
+      selectedText = {
+        trimmedText: highlightedText.replaceAll(/\s+/ig, ' ').trim(),
+        start,
+        end,
+      };
       openMenu(event);
     }
   }
 
-  const selectFamily = (family: string) => {
-    // TODO
-    console.log('selectFamily', family, selectedText);
+  const selectFamily = (family: EntityGroup) => {
+    if (!selectedEntity) {
+      addCustomEntity({
+        entityFamilyId: family.id,
+        entity: selectedText.trimmedText,
+        paragraphId,
+        start: selectedText.start,
+        end: selectedText.end
+      });
+    } else if (selectedEntity.entityFamilyId === family.id){
+      removeCustomEntity(selectedEntity);
+    } else {
+      updateCustomEntity(selectedEntity, {...selectedEntity, entityFamilyId: family.id});
+    }
+    closeMenu();
   }
 
   const closeMenu = () => {
     isMenuVisible = false;
+    selectedEntity = undefined;
     setTimeout(() => {
       isMenuOpen = false;
     }, 240);
-  }
-
-  function getCleanedUpSelectedText(selection: Selection) {
-    return selection.anchorNode?.textContent.substring(selection.anchorOffset, selection.focusOffset).replaceAll(/\s+/ig, ' ').trim();
   }
 </script>
 
@@ -102,7 +140,7 @@
   <div class="menu-container"
        style:opacity={isMenuVisible ? 1 : 0}>
     <EntityFamilyMenu position={menuPosition}
-                      selectedFamily={currentEntityFamily}
+                      selectedFamily={selectedEntity?.entityFamilyId}
                       on:menuHeight={(event) => menuHeight = event.detail.height}
                       on:familySelection={(event) => selectFamily(event.detail.family)}
                       on:close={closeMenu}></EntityFamilyMenu>
--- libs/search-widget/src/viewer/stores/annotation.store.ts
+++ libs/search-widget/src/viewer/stores/annotation.store.ts
@@ -4,6 +4,7 @@ import type { EntityGroup } from '../../core/models';
 export type CustomEntity = {
   paragraphId: string;
   entityFamilyId: string;
+  entity: string;
   start: number;
   end: number;
 };
@@ -21,20 +22,56 @@ export const annotationStore: AnnotationStore = {
     {
       paragraphId: 'fe5cc983ded4330f65ae992f58d85fcf/f/fe5cc983ded4330f65ae992f58d85fcf/0-169',
       entityFamilyId: 'idea',
+      entity: 'idea',
       start: 19,
       end: 25,
     },
     {
       paragraphId: 'fe5cc983ded4330f65ae992f58d85fcf/f/fe5cc983ded4330f65ae992f58d85fcf/0-169',
       entityFamilyId: 'idea',
+      entity: 'idea',
       start: 54,
       end: 59,
     },
     {
       paragraphId: 'fe5cc983ded4330f65ae992f58d85fcf/f/fe5cc983ded4330f65ae992f58d85fcf/1318-1421',
       entityFamilyId: 'idea',
+      entity: 'idea',
       start: 19,
       end: 23,
     },
   ]),
 };
+
+export function addCustomEntity(entity: CustomEntity) {
+  const entities = annotationStore.customEntities.getValue();
+  annotationStore.customEntities.next(entities.concat([entity]));
+}
+
+export function removeCustomEntity(entityToRemove: CustomEntity) {
+  const entities = annotationStore.customEntities.getValue();
+  const indexToRemove = entities.findIndex((entity) => isEqual(entity, entityToRemove));
+  if (indexToRemove > -1) {
+    entities.splice(indexToRemove, 1);
+    annotationStore.customEntities.next(entities);
+  }
+}
+
+export function updateCustomEntity(previousEntity: CustomEntity, newState: CustomEntity) {
+  const entities = annotationStore.customEntities.getValue();
+  const indexToUpdate = entities.findIndex((entity) => isEqual(entity, previousEntity));
+  if (indexToUpdate > -1) {
+    entities[indexToUpdate] = newState;
+    annotationStore.customEntities.next(entities);
+  }
+}
+
+function isEqual(a: CustomEntity, b: CustomEntity): boolean {
+  return (
+    a.paragraphId === b.paragraphId &&
+    a.entityFamilyId === b.entityFamilyId &&
+    a.entity === b.entity &&
+    a.start === b.start &&
+    a.end === b.end
+  );
+}
