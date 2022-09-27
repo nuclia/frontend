51ff006c2ea20bf4a8464ee5d95dae1c8a924f40
mpellerin42 <mpellerin42@gmail.com>
2022-09-27 16:46:50 +0200

    Fix close button style

--- libs/search-widget/src/components/button/CloseButton.svelte
+++ libs/search-widget/src/components/button/CloseButton.svelte
@@ -2,18 +2,18 @@
   import Icon from '../../common/Icon.svelte';
 
   export let aspect = 'solid'; // solid or basic
+  export let kind = 'secondary'; // primary | secondary | inverted
   export let size = 'medium'; // medium or small
   export let disabled = false;
 </script>
 
-<button
-  class="sw-button icon {aspect}"
-  class:small={size === 'small'}
-  type="button"
-  aria-label="Close"
-  tabindex="0"
-  {disabled}
-  on:click
+<button class="sw-button icon {aspect} {kind}"
+        class:small={size === 'small'}
+        type="button"
+        aria-label="Close"
+        tabindex="0"
+        {disabled}
+        on:click
 >
   <Icon name="cross" />
 </button>
