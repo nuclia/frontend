030ac878aa118313b2d592c0fe9baeb29a622355
Eric BREHAULT <ebrehault@gmail.com>
2022-10-25 20:04:17 +0200

    clean up + increment version

--- apps/desktop/src/app/sync/sources/gcs.ts
+++ apps/desktop/src/app/sync/sources/gcs.ts
@@ -1,6 +1,3 @@
-/// <reference path="../../../../../../node_modules/@types/gapi/index.d.ts" />
-/// <reference path="../../../../../../node_modules/@types/gapi.auth2/index.d.ts" />
-
 import {
   ConnectorSettings,
   ISourceConnector,
