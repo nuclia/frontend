d51e6e32ab16e96e92bde3cd3ed18e122bc96f5b
oriol <o.perramon@iskra.cat>
2022-10-29 21:04:41 +0200

    Upgrade d3 version

--- apps/dashboard/src/app/components/charts/bar-chart/bar-chart.component.ts
+++ apps/dashboard/src/app/components/charts/bar-chart/bar-chart.component.ts
@@ -98,9 +98,9 @@ export class BarChartComponent implements AfterViewInit, OnDestroy {
     }
 
     // Tooltip event handlers
-    const mouseover = (data: [string, number], index: number, group: any) => {
+    const mouseover = (event: any, data: [string, number]) => {
       if (!this.tooltipsEnabled) return;
-      const { bottom, left, height, width } = group[index].getBoundingClientRect();
+      const { bottom, left, height, width } = event.target.getBoundingClientRect();
       this.tooltipBottom = this.container!.nativeElement.getBoundingClientRect().bottom - bottom + height + 8;
       this.tooltipLeft = left - this.container!.nativeElement.getBoundingClientRect().left + width / 2;
       this.showTooltip = true;
