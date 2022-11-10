76bc5516f7f7d376cad65f620985390b89f1113f
mpellerin42 <mpellerin42@gmail.com>
2022-11-10 14:52:09 +0100

    Reformat Sistema-demo app with prettier

--- apps/sistema-demo/src/app/app-routing.module.ts
+++ apps/sistema-demo/src/app/app-routing.module.ts
@@ -6,9 +6,7 @@ import { WelcomePageModule } from './welcome-page/welcome-page.module';
 
 function buildRoutesFromMenu() {
   const routes: Routes = [{ path: '', pathMatch: 'full', component: WelcomePageComponent }];
-  menu.forEach((section) =>
-    section.pages.forEach((page) => routes.push({path: page.view, component: page.type})),
-  );
+  menu.forEach((section) => section.pages.forEach((page) => routes.push({ path: page.view, component: page.type })));
   return routes;
 }
 
@@ -16,6 +14,6 @@ const appRoutes: Routes = buildRoutesFromMenu();
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes), WelcomePageModule],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- apps/sistema-demo/src/app/sistema-pages/pastanaga-pages-override/sistema-modal/modal-example/modal-example.component.ts
+++ apps/sistema-demo/src/app/sistema-pages/pastanaga-pages-override/sistema-modal/modal-example/modal-example.component.ts
@@ -2,10 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ModalRef } from '@guillotinaweb/pastanaga-angular';
 
 @Component({
-    templateUrl: './modal-example.component.html',
-    styleUrls: ['./modal-example.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './modal-example.component.html',
+  styleUrls: ['./modal-example.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalExampleComponent {
-    constructor(public modal: ModalRef) {}
+  constructor(public modal: ModalRef) {}
 }
