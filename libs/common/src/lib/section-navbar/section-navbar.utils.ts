95548252688e0fc010f4505abf545ab1b7ced7aa
mpellerin42 <mpellerin42@gmail.com>
2022-11-10 14:54:21 +0100

    Reformat common lib with prettier

--- libs/common/src/lib/section-navbar/section-navbar.utils.ts
+++ libs/common/src/lib/section-navbar/section-navbar.utils.ts
@@ -1,35 +1,33 @@
-
-
 export function scrollTo(to: number, duration: number) {
   const start = document.documentElement.scrollTop;
-  const change = to - start
-  const increment = 20
-  let currentTime = 0
-  duration = (typeof (duration) === 'undefined') ? 500 : duration
-  var animateScroll = function() {
+  const change = to - start;
+  const increment = 20;
+  let currentTime = 0;
+  duration = typeof duration === 'undefined' ? 500 : duration;
+  var animateScroll = function () {
     // increment the time
-    currentTime += increment
+    currentTime += increment;
     // find the value with the quadratic in-out easing function
-    var val = easeInOutQuad(currentTime, start, change, duration)
+    var val = easeInOutQuad(currentTime, start, change, duration);
     // move the document.body
-    move(val)
+    move(val);
     // do the animation unless its over
     if (currentTime < duration) {
-      window.requestAnimationFrame(animateScroll)
+      window.requestAnimationFrame(animateScroll);
     }
-  }
-  animateScroll()
+  };
+  animateScroll();
 }
 
 function move(amount: number) {
-  document.documentElement.scrollTop = amount
+  document.documentElement.scrollTop = amount;
 }
 
 function easeInOutQuad(t: number, b: number, c: number, d: number) {
   t /= d / 2;
   if (t < 1) {
-    return c / 2 * t * t + b;
+    return (c / 2) * t * t + b;
   }
   t--;
-  return -c / 2 * (t * (t - 2) - 1) + b;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
 }
--- libs/common/src/lib/user/user-container/user-container.component.ts
+++ libs/common/src/lib/user/user-container/user-container.component.ts
@@ -3,15 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'stf-user-container',
   templateUrl: './user-container.component.html',
-  styleUrls: ['./user-container.component.scss']
+  styleUrls: ['./user-container.component.scss'],
 })
 export class UserContainerComponent implements OnInit {
   @Input() widthSm: string = '450px';
   @Input() showLink: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
