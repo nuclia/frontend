229be6b0a0e8af85e4f0cd405a8935001d331293
mpellerin42 <mpellerin42@gmail.com>
2022-12-01 17:55:01 +0100

    Remove mediaQueryList event listener on destroy

--- apps/dashboard/src/app/account/account.component.ts
+++ apps/dashboard/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnDestroy } from '@angular/core';
 import { WINDOW } from '@ng-web-apis/common';
 
 // Height from which the menu get smaller (prevents scrollbar to appear)
@@ -10,12 +10,21 @@ const heightMediaQuery = '(max-height: 730px)';
   styleUrls: ['./account.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountComponent {
+export class AccountComponent implements OnDestroy {
   smallNavbar: boolean = this.window.matchMedia(heightMediaQuery).matches;
+  private mediaQueryList;
+
   constructor(@Inject(WINDOW) private window: Window, private cdr: ChangeDetectorRef) {
-    this.window.matchMedia(heightMediaQuery).addEventListener('change', (event) => {
-      this.smallNavbar = event.matches;
-      this.cdr.detectChanges();
-    });
+    this.mediaQueryList = this.window.matchMedia(heightMediaQuery);
+    this.mediaQueryList.addEventListener('change', this.onMatchMediaChange);
+  }
+
+  ngOnDestroy() {
+    this.mediaQueryList.removeEventListener('change', this.onMatchMediaChange);
+  }
+
+  private onMatchMediaChange(event: MediaQueryListEvent) {
+    this.smallNavbar = event.matches;
+    this.cdr.detectChanges();
   }
 }
