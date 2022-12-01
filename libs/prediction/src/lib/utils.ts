ad89164725337166adfcfa2fb903af179ee6e52e
Eric BREHAULT <ebrehault@gmail.com>
2022-12-01 17:37:22 +0100

    logger function

--- libs/prediction/src/lib/utils.ts
+++ libs/prediction/src/lib/utils.ts
@@ -1,3 +1,7 @@
 let CDN = 'https://cdn.nuclia.cloud/';
 export const setCDN = (cdn: string) => (CDN = cdn);
 export const getCDN = () => CDN;
+
+// TO ENABLE DEBUGGING, GO TO YOUR BROWSER CONSOLE AND TYPE:
+// localStorage.setItem('nuclia_debug', 'true');
+export const logger = localStorage.getItem('nuclia_debug') === 'true' ? console.log : () => {};
