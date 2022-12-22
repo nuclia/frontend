ba90914a404402a8d7cc1c10f3f5667dd5e1681e
mpellerin42 <mpellerin42@gmail.com>
2022-12-22 16:47:59 +0100

    Creating account and kb + redirecting to resource list

--- libs/common/src/lib/user/onboarding/onboarding.component.ts
+++ libs/common/src/lib/user/onboarding/onboarding.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { OnboardingService, OnboardingStatus } from './onboarding.service';
+import { OnboardingPayload } from './onboarding.models';
+import { Zone, ZoneService } from '@flaps/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'stf-onboarding',
@@ -7,7 +12,27 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OnboardingComponent implements OnInit {
-  constructor() {}
+  onboardingForm = new FormGroup({
+    industry: new FormControl<string>(''),
+    searchEngine: new FormControl<string>(''),
+    getUpdates: new FormControl<boolean>(false),
+  });
+
+  zones: Zone[] = [];
+  onboardingStatus: Observable<OnboardingStatus> = this.onboardingService.onboardingState;
+
+  constructor(private onboardingService: OnboardingService, private zoneService: ZoneService) {
+    this.zoneService.getZones().subscribe((zones) => (this.zones = zones));
+  }
 
   ngOnInit(): void {}
+
+  submitForm() {
+    const data: OnboardingPayload = {
+      industry: this.onboardingForm.value.industry || '',
+      other_search_engines: this.onboardingForm.value.searchEngine || '',
+      receive_updates: this.onboardingForm.value.getUpdates || false,
+    };
+    this.onboardingService.startOnboarding(data, this.zones[0].id);
+  }
 }
