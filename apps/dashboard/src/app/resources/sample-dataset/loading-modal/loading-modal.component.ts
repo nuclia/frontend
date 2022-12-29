9535dccf89e785259f2c6b5beaaaff31b27b070d
Mat Pellerin <mpellerin42@gmail.com>
2022-12-29 11:37:29 +0100

    Add loading dataset message (#460)

---
+++ apps/dashboard/src/app/resources/sample-dataset/loading-modal/loading-modal.component.ts
@@ -0,0 +1,17 @@
+import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
+import { BaseModalComponent } from '@guillotinaweb/pastanaga-angular';
+
+@Component({
+  selector: 'app-loading-modal',
+  templateUrl: './loading-modal.component.html',
+  styleUrls: ['./loading-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoadingModalComponent extends BaseModalComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    if (!!this.ref) {
+      this.id = this.ref.id;
+      this.config = this.ref.config;
+    }
+  }
+}
