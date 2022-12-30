cba464ea3130a1c570077f3d5fabe4f3b6d3514a
mpellerin42 <mpellerin42@gmail.com>
2022-12-30 11:28:35 +0100

    Fix unit tests

--- apps/dashboard/src/app/resources/sample-dataset/dataset-import.component.spec.ts
+++ apps/dashboard/src/app/resources/sample-dataset/dataset-import.component.spec.ts
@@ -1,18 +1,31 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DatasetImportComponent } from './dataset-import.component';
 import { MockModule, MockPipe, MockProvider } from 'ng-mocks';
 import { UploadService } from '../upload.service';
 import { SDKService } from '@flaps/core';
-import { PaButtonModule, PaTextFieldModule, PaTranslateModule, TranslatePipe } from '@guillotinaweb/pastanaga-angular';
-import { ActivatedRoute } from '@angular/router';
+import {
+  ModalConfig,
+  ModalRef,
+  PaButtonModule,
+  PaTextFieldModule,
+  PaTranslateModule,
+  TranslatePipe,
+} from '@guillotinaweb/pastanaga-angular';
+import { ActivatedRoute, Router } from '@angular/router';
 import { of } from 'rxjs';
+import { SampleDatasetService } from './sample-dataset.service';
+import { SisModalService, SisToastService } from '@nuclia/sistema';
+import { LoadingModalComponent } from './loading-modal/loading-modal.component';
 
 describe('DatasetImportComponent', () => {
   let component: DatasetImportComponent;
   let fixture: ComponentFixture<DatasetImportComponent>;
 
   let uploadService: UploadService;
+  const loadingModal: ModalRef = {
+    close: jest.fn(),
+  } as any as ModalRef;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -31,6 +44,13 @@ describe('DatasetImportComponent', () => {
         }),
         MockProvider(SDKService),
         MockProvider(ActivatedRoute),
+        MockProvider(SampleDatasetService, {
+          importDataset: jest.fn(() => of(null) as any),
+        }),
+        MockProvider(SisModalService, {
+          openModal: jest.fn(() => loadingModal),
+        }),
+        MockProvider(SisToastService),
       ],
     }).compileComponents();
 
@@ -61,5 +81,40 @@ describe('DatasetImportComponent', () => {
     expect(uploadService.upload).toHaveBeenCalledWith('csv');
   }));
 
-  it('should navigate to resource list after closing upload dialog', () => {});
+  describe('importDataset', () => {
+    let sampleService: SampleDatasetService;
+    let modalService: SisModalService;
+    let router: Router;
+
+    beforeEach(() => {
+      sampleService = TestBed.inject(SampleDatasetService);
+      modalService = TestBed.inject(SisModalService);
+      router = TestBed.inject(Router);
+      router.navigate = jest.fn();
+
+      component.selectDataset = 'universe';
+    });
+
+    it('should open loading modal and call importDataset', () => {
+      component.importDataset();
+      expect(modalService.openModal).toHaveBeenCalledWith(
+        LoadingModalComponent,
+        new ModalConfig({
+          dismissable: false,
+          data: {
+            title: 'onboarding.dataset.importing_title',
+            description: 'onboarding.dataset.importing_description',
+          },
+        }),
+      );
+      expect(sampleService.importDataset).toHaveBeenCalledWith('universe');
+    });
+
+    it('should navigate to resource list and close loading modal on success', fakeAsync(() => {
+      const route = TestBed.inject(ActivatedRoute);
+      component.importDataset();
+      expect(router.navigate).toHaveBeenCalledWith(['..'], { relativeTo: route });
+      expect(loadingModal.close).toHaveBeenCalled();
+    }));
+  });
 });
