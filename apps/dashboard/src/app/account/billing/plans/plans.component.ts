5a5be365bbf8690cf68ed9820d4959d5426c0a88
Eric BREHAULT <ebrehault@gmail.com>
2023-01-04 12:13:24 +0100

    Hide billing pages content when feature flag [sc-3433]

--- apps/dashboard/src/app/account/billing/plans/plans.component.ts
+++ apps/dashboard/src/app/account/billing/plans/plans.component.ts
@@ -1,5 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { STFTrackingService } from '@flaps/core';
+import { shareReplay } from 'rxjs';
 import { BASIC_PLAN, PLAN_PARAMETERS, TEAM_PLAN } from '../billing.service';
 
 @Component({
@@ -13,8 +15,9 @@ export class PlansComponent {
   teamPlan = TEAM_PLAN;
   basicPlan = BASIC_PLAN;
   parameters = PLAN_PARAMETERS;
+  isBillingEnabled = this.tracking.isFeatureEnabled('billing').pipe(shareReplay(1));
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private tracking: STFTrackingService) {}
 
   toggleMode() {
     this.mode = this.mode === 'annual' ? 'monthly' : 'annual';
