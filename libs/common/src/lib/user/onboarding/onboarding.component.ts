a2b1ff4e0aed73a73470f07e824df51a216ede7b
mpellerin42 <mpellerin42@gmail.com>
2023-01-05 10:33:45 +0100

    Onboarding improvements
      - Replacing industry text field by a mandatory select box
      - Changing search engine label

--- libs/common/src/lib/user/onboarding/onboarding.component.ts
+++ libs/common/src/lib/user/onboarding/onboarding.component.ts
@@ -1,9 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { OnboardingService, OnboardingStatus } from './onboarding.service';
 import { OnboardingPayload } from './onboarding.models';
 import { Zone, ZoneService } from '@flaps/core';
 import { Observable } from 'rxjs';
+import { OptionModel } from '@guillotinaweb/pastanaga-angular';
+import { TranslateService } from '@ngx-translate/core';
+
+const industries = [
+  'IT_software',
+  'marketing',
+  'finance',
+  'research',
+  'education',
+  'health',
+  'insurance',
+  'public_administration',
+  'sales',
+  'legal',
+  'telecommunications',
+  'engineering',
+  'pharma',
+  'startup',
+  'manufacturing',
+  'robotics',
+  'energy',
+  'chemical_industry',
+  'food_service',
+  'construction',
+  'retail',
+  'other',
+];
 
 @Component({
   selector: 'stf-onboarding',
@@ -13,15 +40,24 @@ import { Observable } from 'rxjs';
 })
 export class OnboardingComponent {
   onboardingForm = new FormGroup({
-    industry: new FormControl<string>(''),
+    industry: new FormControl<string>('', [Validators.required]),
     searchEngine: new FormControl<string>(''),
     getUpdates: new FormControl<boolean>(true),
   });
 
+  validationMessages = {
+    industry: { required: 'validation.required' },
+  };
+
   zones: Zone[] = [];
   onboardingStatus: Observable<OnboardingStatus> = this.onboardingService.onboardingState;
+  industryOptions: OptionModel[] = this.buildIndustryList();
 
-  constructor(private onboardingService: OnboardingService, private zoneService: ZoneService) {
+  constructor(
+    private onboardingService: OnboardingService,
+    private zoneService: ZoneService,
+    private translate: TranslateService,
+  ) {
     this.zoneService.getZones().subscribe((zones) => (this.zones = zones));
   }
 
@@ -31,6 +67,28 @@ export class OnboardingComponent {
       other_search_engines: this.onboardingForm.value.searchEngine || '',
       receive_updates: this.onboardingForm.value.getUpdates || false,
     };
-    this.onboardingService.startOnboarding(data, this.zones[0].id);
+    console.log(data);
+    // this.onboardingService.startOnboarding(data, this.zones[0].id);
+  }
+
+  private buildIndustryList(): OptionModel[] {
+    const list = industries
+      .map(
+        (industry) =>
+          new OptionModel({
+            id: industry,
+            value: industry,
+            label: this.translate.instant(`onboarding.industry.${industry}`),
+          }),
+      )
+      .sort((a, b) => a.label.toLocaleLowerCase().localeCompare(b.label.toLocaleLowerCase()));
+    list.push(
+      new OptionModel({
+        id: 'other',
+        value: 'other',
+        label: this.translate.instant('onboarding.industry.other'),
+      }),
+    );
+    return list;
   }
 }
