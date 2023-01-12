82ab3edf386b8e1b87c1dd9388cf1506911f2e86
operramon <52449656+operramon@users.noreply.github.com>
2023-01-12 16:46:55 +0100

    [sc-3571] Avoid unnecessary search calls in dashboard (#504)

--- libs/search-widget/src/core/search-bar.ts
+++ libs/search-widget/src/core/search-bar.ts
@@ -3,7 +3,7 @@ import { PENDING_RESULTS } from './models';
 import { search } from './api';
 import { navigateToLink } from './stores/widget.store';
 import { ResourceProperties, Search } from '@nuclia/core';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
 import {
   isEmptySearchQuery,
   searchFilters,
@@ -13,33 +13,41 @@ import {
   triggerSearch,
 } from './stores/search.store';
 
+const subscriptions: Subscription[] = [];
+
+export function unsubscribeTriggerSearch() {
+  subscriptions.forEach((subscription) => subscription.unsubscribe());
+}
+
 export const setupTriggerSearch = (
   dispatch: (event: string, details: string | Search.Results) => void | undefined,
 ): void => {
-  triggerSearch
-    .pipe(
-      tap(() => searchResults.set(PENDING_RESULTS)),
-      switchMap(() => isEmptySearchQuery.pipe(take(1))),
-      filter((isEmptySearchQuery) => !isEmptySearchQuery),
-      switchMap(() => searchQuery.pipe(take(1))),
-      tap((query) => (dispatch ? dispatch('search', query) : undefined)),
-      switchMap((query) =>
-        forkJoin([searchOptions.pipe(take(1)), searchFilters.pipe(take(1)), navigateToLink.pipe(take(1))]).pipe(
-          map(([options, filters, navigateToLink]) => {
-            const show = navigateToLink
-              ? [ResourceProperties.BASIC, ResourceProperties.VALUES]
-              : [ResourceProperties.BASIC];
-            const currentOptions = { ...options, show, filters };
-            return { query, options: currentOptions };
-          }),
+  subscriptions.push(
+    triggerSearch
+      .pipe(
+        tap(() => searchResults.set(PENDING_RESULTS)),
+        switchMap(() => isEmptySearchQuery.pipe(take(1))),
+        filter((isEmptySearchQuery) => !isEmptySearchQuery),
+        switchMap(() => searchQuery.pipe(take(1))),
+        tap((query) => (dispatch ? dispatch('search', query) : undefined)),
+        switchMap((query) =>
+          forkJoin([searchOptions.pipe(take(1)), searchFilters.pipe(take(1)), navigateToLink.pipe(take(1))]).pipe(
+            map(([options, filters, navigateToLink]) => {
+              const show = navigateToLink
+                ? [ResourceProperties.BASIC, ResourceProperties.VALUES]
+                : [ResourceProperties.BASIC];
+              const currentOptions = { ...options, show, filters };
+              return { query, options: currentOptions };
+            }),
+          ),
         ),
-      ),
-      switchMap(({ query, options }) => search(query, options)),
-      tap((results) => {
-        if (typeof dispatch === 'function') {
-          dispatch('results', results);
-        }
-      }),
-    )
-    .subscribe((results) => searchResults.set(results));
+        switchMap(({ query, options }) => search(query, options)),
+        tap((results) => {
+          if (typeof dispatch === 'function') {
+            dispatch('results', results);
+          }
+        }),
+      )
+      .subscribe((results) => searchResults.set(results)),
+  );
 };
