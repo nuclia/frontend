be13566cbbc7e77ba204dcc389882ca5647c15a6
Eric BREHAULT <ebrehault@gmail.com>
2023-03-02 18:18:28 +0100

    fix npm publish

--- libs/sdk-core/src/index.ts
+++ libs/sdk-core/src/index.ts
@@ -1,5 +1,5 @@
+export * from './lib/auth';
 export * from './lib/core';
 export * from './lib/models';
-export * from './lib/auth';
 export * from './lib/db';
 export * from './lib/rest';
--- libs/search-widget/src/widgets/search-widget/index.ts
+++ libs/search-widget/src/widgets/search-widget/index.ts
@@ -1,2 +1,2 @@
-export { default as NucliaSearchBar } from './SearchBar.svelte';
 export { default as NucliaSearchResults } from './SearchResults.svelte';
+export { default as NucliaSearchBar } from './SearchBar.svelte';
