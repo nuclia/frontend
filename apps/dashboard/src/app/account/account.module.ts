9b87ef47f9efadebc5b1b008e1669b3306a5e6df
mpellerin42 <mpellerin42@gmail.com>
2023-03-03 17:41:50 +0100

    Remove FlexLayoutModule from account module

--- apps/dashboard/src/app/account/account.module.ts
+++ apps/dashboard/src/app/account/account.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { OverlayModule } from '@angular/cdk/overlay';
@@ -62,7 +61,6 @@ const Components = [
   imports: [
     CommonModule,
     AngularSvgIconModule,
-    FlexLayoutModule,
     TranslateModule.forChild(),
     ReactiveFormsModule,
     FormsModule,
