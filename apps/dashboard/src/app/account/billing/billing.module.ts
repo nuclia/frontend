f87bda8a0c62ee4934bcba7663ac286af3cce768
oriol <o.perramon@iskra.cat>
2023-03-08 20:52:41 +0100

    Remove old billing pages

--- apps/dashboard/src/app/account/billing/billing.module.ts
+++ apps/dashboard/src/app/account/billing/billing.module.ts
@@ -4,33 +4,14 @@ import { TranslateModule } from '@ngx-translate/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MatDialogModule } from '@angular/material/dialog';
-import { PaButtonModule, PaIconModule, PaTextFieldModule, PaTooltipModule } from '@guillotinaweb/pastanaga-angular';
+import { PaButtonModule, PaIconModule, PaTextFieldModule } from '@guillotinaweb/pastanaga-angular';
 import { BillingComponent } from './billing.component';
-import { PlansComponent } from './plans/plans.component';
-import { PlansSettingsComponent } from './settings/plan-settings.component';
-import { PaymentComponent } from './payment/payment.component';
-import { CardComponent } from './card/card.component';
-import { STFSectionNavbarModule } from '../../components/section-navbar';
 
 const routes = [
   {
     path: '',
     component: BillingComponent,
-    children: [
-      {
-        path: 'plans',
-        component: PlansComponent,
-      },
-      {
-        path: 'settings',
-        component: PlansSettingsComponent,
-      },
-      {
-        path: 'payment',
-        component: PaymentComponent,
-      },
-    ],
+    children: [],
   },
 ];
 
@@ -41,14 +22,11 @@ const routes = [
     AngularSvgIconModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
-    MatDialogModule,
-    STFSectionNavbarModule,
     PaButtonModule,
     PaTextFieldModule,
-    PaTooltipModule,
     PaIconModule,
   ],
-  declarations: [BillingComponent, PlansComponent, PlansSettingsComponent, PaymentComponent, CardComponent],
+  declarations: [BillingComponent],
   exports: [],
 })
 export class BillingModule {}
