848b22f70e6075be0e3b13a00b09c9f7c83e7f38
Eric BREHAULT <ebrehault@gmail.com>
2023-03-16 16:36:28 +0100

    show sources

--- libs/search-widget/src/components/answer/Answer.svelte
+++ libs/search-widget/src/components/answer/Answer.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
   import type { Answer } from '../../core/answer.models';
   import { _ } from '../../core/i18n';
+  import Expander from '../../common/expander/Expander.svelte';
   export let answer: Partial<Answer>;
 </script>
 
 <div>
-  {answer.text}
+  <div>{answer.text}</div>
+  {#if answer.sources}
+    <Expander>
+      <h3
+        slot="header"
+        class="title-xs">
+        {$_('answer.sources')}
+      </h3>
+      {#each answer.sources as source}
+        <div>{source.paragraph}</div>
+      {/each}
+    </Expander>
+  {/if}
 </div>
 
 <style
