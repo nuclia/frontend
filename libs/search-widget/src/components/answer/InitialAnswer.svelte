c664eb2dae353f3c683af9d34c026d3e259f0f01
Eric BREHAULT <ebrehault@gmail.com>
2023-03-16 18:02:52 +0100

    Dialog modal

--- libs/search-widget/src/components/answer/InitialAnswer.svelte
+++ libs/search-widget/src/components/answer/InitialAnswer.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
   import Icon from '../../common/icons/Icon.svelte';
+  import Modal from '../../common/modal/Modal.svelte';
   import { _ } from '../../core/i18n';
   import { firstAnswer, isStreaming } from '../../core/stores/answers.store';
   import Answer from './Answer.svelte';
+  import Dialog from './Dialog.svelte';
   import Feedback from './Feedback.svelte';
+
+  let showDialog = false;
 </script>
 
 {#if $firstAnswer.text}
   <div class="container">
     <div class="answer"><Answer answer={$firstAnswer} /></div>
     <div class="actions">
-      <div>
+      <div
+        class="go-to-dialog"
+        on:click={() => (showDialog = true)}>
         <Icon name="chat" />
         {$_('answer.chat-action')}
       </div>
@@ -22,6 +28,12 @@
     </div>
   </div>
 {/if}
+
+<Modal
+  show={showDialog}
+  closeButton={true}>
+  <Dialog />
+</Modal>
 
 <style
   lang="scss"
