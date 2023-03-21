daa144ab065f4fbcadc7a86db07bf4a389a17648
mpellerin42 <mpellerin42@gmail.com>
2023-03-21 16:25:28 +0100

    Fix unit tests

--- apps/dashboard/src/app/synonyms/synonyms.component.spec.ts
+++ apps/dashboard/src/app/synonyms/synonyms.component.spec.ts
@@ -3,9 +3,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SynonymsComponent } from './synonyms.component';
 import { MockModule, MockProvider } from 'ng-mocks';
 import { SynonymsService } from './synonyms.service';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PaButtonModule, PaTextFieldModule, PaTogglesModule } from '@guillotinaweb/pastanaga-angular';
+import { of } from 'rxjs';
 
 describe('SynonymsComponent', () => {
   let component: SynonymsComponent;
@@ -21,7 +22,12 @@ describe('SynonymsComponent', () => {
         MockModule(ReactiveFormsModule),
       ],
       declarations: [SynonymsComponent],
-      providers: [MockProvider(SynonymsService)],
+      providers: [
+        MockProvider(SynonymsService, {
+          synonyms: of({}),
+        }),
+        MockProvider(TranslateService),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SynonymsComponent);
