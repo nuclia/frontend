81210338e7156f7a37a0e21c18aa464f747ecf2e
Eric BREHAULT <ebrehault@gmail.com>
2023-03-30 11:14:52 +0200

    use hash-based routing in contributor app

--- apps/contributor/src/app/app-routing.ts
+++ apps/contributor/src/app/app-routing.ts
@@ -14,6 +14,7 @@ import { MainContainerComponent } from './home/main-container/main-container.com
 export const routerOptions: ExtraOptions = {
   onSameUrlNavigation: 'reload',
   scrollPositionRestoration: 'enabled',
+  useHash: true,
 };
 
 export const routes: Routes = [
