12440b5aa985329cb8f6c8230ace2e388b5ee581
oriol <o.perramon@iskra.cat>
2023-04-04 09:19:17 +0200

    [sc-4781] Implement billing navigation

--- apps/dashboard/src/app/account/billing/billing.component.ts
+++ apps/dashboard/src/app/account/billing/billing.component.ts
@@ -1,4 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-billing',
@@ -7,5 +9,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BillingComponent {
-  constructor() {}
+  isCheckoutActive = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    map(() => this.router.url.includes('checkout')),
+  );
+
+  constructor(private router: Router) {}
 }
