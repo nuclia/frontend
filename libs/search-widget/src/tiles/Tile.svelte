51813c59ba5b7469d8f196a980d3d9fab1f17484
Eric BREHAULT <ebrehault@gmail.com>
2023-04-11 12:02:14 +0200

    fix tile type mapping (#677)

--- libs/search-widget/src/tiles/Tile.svelte
+++ libs/search-widget/src/tiles/Tile.svelte
@@ -25,18 +25,25 @@
       tileType = url?.includes('youtube.com') || url?.includes('youtu.be') ? 'video' : 'text';
     } else if (result?.field?.field_type === FIELD_TYPE.file && !!result?.fieldData?.value) {
       const file = (result.fieldData as FileFieldData).value?.file;
+      // for audio, video, image or text, we have a corresponding tile
+      // for mimetype starying with 'application/', it is more complex:
+      // - anything like a spreadsheet is a spreadsheet
+      // - 'applicatuion/octet-stream' is the default generic mimetype, its means we have no idea what it is, so we use text as that's the most reliable
+      // - anything else is a pdf ('application/pdf' of course, but also any MSWord, OpenOffice, etc., are converted to pdf by the backend)
       if (file?.content_type?.startsWith('audio')) {
         tileType = 'audio';
       } else if (file?.content_type?.startsWith('video')) {
         tileType = 'video';
       } else if (file?.content_type?.startsWith('image')) {
         tileType = 'image';
-      } else if (file?.content_type?.startsWith('text/plain')) {
+      } else if (file?.content_type?.startsWith('text')) {
         tileType = 'text';
-      } else if (file?.content_type?.startsWith('application/pdf')) {
-        tileType = 'pdf';
       } else if (SpreadsheetContentTypes.includes(file?.content_type || '')) {
         tileType = 'spreadsheet';
+      } else if (file?.content_type?.startsWith('application/octet-stream')) {
+        tileType = 'text';
+      } else if (file?.content_type?.startsWith('application')) {
+        tileType = 'pdf';
       } else {
         tileType = 'text';
       }
