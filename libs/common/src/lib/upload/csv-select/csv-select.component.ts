2cbfc9fd0262c6987be88a263bf3dfe5f6c28fb5
mpellerin42 <mpellerin42@gmail.com>
2023-04-17 12:02:10 +0200

    Improve csv-select component so we can reuse it for entities
      - update style (spacing) and element positioning (CSV format details after the button)
      - adapt upload dialog components using it accordingly
      - add `confirmData` input in csv-select: when present, we open a confirmation dialog before uploading (needed for entity management dialog)
      - add `buttonKind` input allowing to change the kind of select csv button

--- libs/common/src/lib/upload/csv-select/csv-select.component.ts
+++ libs/common/src/lib/upload/csv-select/csv-select.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { coerceNumberProperty } from '@angular/cdk/coercion';
 import { parseCsv } from '../utils';
-import { SisToastService } from '@nuclia/sistema';
+import { SisModalService, SisToastService } from '@nuclia/sistema';
+import { ConfirmationData, Kind } from '@guillotinaweb/pastanaga-angular';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-csv-select',
@@ -17,22 +19,56 @@ export class CsvSelectComponent<T> {
   get fields() {
     return this._fields;
   }
-  private _fields: number = 0;
+
+  @Input()
+  set buttonKind(value: Kind | undefined) {
+    if (value) {
+      this._buttonKind = value;
+    }
+  }
+  get buttonKind() {
+    return this._buttonKind;
+  }
+  private _buttonKind: Kind = 'primary';
+
+  @Input()
+  set confirmData(value: ConfirmationData | undefined) {
+    this._confirmData = value;
+  }
+  get confirmData(): ConfirmationData | undefined {
+    return this._confirmData;
+  }
 
   @Input() help: string[] = [];
   @Output() select = new EventEmitter<string[][]>();
 
-  constructor(private toaster: SisToastService) {}
+  @ViewChild('fileUpload') fileUpload?: ElementRef;
+
+  private _confirmData?: ConfirmationData;
+  private _fields = 0;
+
+  constructor(private toaster: SisToastService, private modalService: SisModalService) {}
+
+  onUpload() {
+    if (this.confirmData) {
+      this.modalService
+        .openConfirm(this.confirmData)
+        .onClose.pipe(filter((confirm) => confirm))
+        .subscribe(() => this.openUploadDialog());
+    } else {
+      this.openUploadDialog();
+    }
+  }
 
   readCsv(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        const cvs = parseCsv(reader.result as string);
-        const isValid = cvs.every((row) => row.length === this.fields);
+        const csv = parseCsv(reader.result as string);
+        const isValid = csv.every((row) => row.length === this.fields);
         if (isValid) {
-          this.select.emit(cvs);
+          this.select.emit(csv);
         } else {
           this.toaster.error('upload.invalid_csv');
         }
@@ -42,5 +78,9 @@ export class CsvSelectComponent<T> {
       };
       reader.readAsText(file, 'UTF-8');
     }
+  }
+
+  private openUploadDialog() {
+    this.fileUpload?.nativeElement.click();
   }
 }
