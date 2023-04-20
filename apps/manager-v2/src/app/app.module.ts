79718bf09577d6a02d26d7afd8d410fc37458b25
mpellerin42 <mpellerin42@gmail.com>
2023-04-20 15:27:41 +0200

    Setup config, Pastanaga, Sistema, Translations

--- apps/manager-v2/src/app/app.module.ts
+++ apps/manager-v2/src/app/app.module.ts
@@ -3,12 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { BackendConfigurationService, STFConfigModule } from '@flaps/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
+import { TranslateLoader, TranslateModule, TranslatePipe } from '@ngx-translate/core';
+
+// Load locales
+import localeEn from '@angular/common/locales/en';
+import localeEs from '@angular/common/locales/es';
+import localeCa from '@angular/common/locales/ca';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+registerLocaleData(localeEn);
+registerLocaleData(localeEs);
+registerLocaleData(localeCa);
+
+export function createTranslateLoader(http: HttpClient, config: BackendConfigurationService) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', `.json?version=${config.getVersion()}`);
+}
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    STFConfigModule.forRoot(environment),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient, BackendConfigurationService],
+      },
+    }),
+    AngularSvgIconModule.forRoot(),
+  ],
+  providers: [TranslatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
