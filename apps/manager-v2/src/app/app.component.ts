912d15db661857e5b6dfc48935ae19e40d9f4f21
mpellerin42 <mpellerin42@gmail.com>
2023-04-20 15:00:58 +0200

    Create manager-v2 app

---
+++ apps/manager-v2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nuclia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'manager-v2';
+}
