0fec2b242b49bafeae08e5d8220910b0baaef6f4
mpellerin42 <mpellerin42@gmail.com>
2023-04-21 17:56:00 +0200

    Knowledge box list + more details in left panel

--- apps/manager-v2/src/app/manage-accounts/account-details/knowledge-boxes/knowledge-boxes.component.ts
+++ apps/manager-v2/src/app/manage-accounts/account-details/knowledge-boxes/knowledge-boxes.component.ts
@@ -1,8 +1,68 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AccountDetailsStore } from '../account-details.store';
+import { filter, forkJoin, map, Observable, of, switchMap, take } from 'rxjs';
+import { Stash } from '../../account.models';
+import { Counters, Nuclia } from '@nuclia/core';
+import { SDKService } from '@flaps/core';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   templateUrl: './knowledge-boxes.component.html',
   styleUrls: ['./knowledge-boxes.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class KnowledgeBoxesComponent {}
+export class KnowledgeBoxesComponent implements OnInit {
+  kbList: Observable<Stash[]> = this.accountStore.accountDetails.pipe(
+    map((account) => (account ? account.stashes.items || [] : [])),
+  );
+  counters: { [kbId: string]: number } = {};
+
+  constructor(private accountStore: AccountDetailsStore, private sdk: SDKService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.accountStore.zones
+      .pipe(
+        filter((zones) => zones.length > 0),
+        take(1),
+        switchMap((zones) =>
+          this.kbList.pipe(
+            filter((list) => list.length > 0),
+            take(1),
+            switchMap((kbs) => {
+              const requests: Observable<{ kbId: string; counters: Counters } | null>[] = [];
+              kbs.forEach((kb) => {
+                const zone = zones.find((z) => z.id === kb.zone);
+                if (zone) {
+                  const specificNuclia = new Nuclia({
+                    ...this.sdk.nuclia.options,
+                    zone: zone.slug,
+                    knowledgeBox: kb.id,
+                  });
+                  requests.push(
+                    specificNuclia.knowledgeBox.counters().pipe(
+                      map((counters) => ({ kbId: kb.id, counters })),
+                      catchError((error) => {
+                        console.error(`Loading counters for ${kb.slug} failed`, error);
+                        return of(null);
+                      }),
+                    ),
+                  );
+                } else {
+                  console.error(`No zone found for KB ${kb.slug}`, kb, zones);
+                }
+              });
+              return requests.length > 0 ? forkJoin(requests) : of(null);
+            }),
+          ),
+        ),
+      )
+      .subscribe((responses) => {
+        responses?.forEach((response) => {
+          if (response) {
+            this.counters[response.kbId] = response.counters.resources;
+          }
+        });
+        this.cdr.markForCheck();
+      });
+  }
+}
