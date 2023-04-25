8d0eff2e3376d81b3a7e9f01dea23faaeb779ab9
oriol <o.perramon@iskra.cat>
2023-04-25 12:56:16 +0200

    Add option to user menu

--- libs/common/src/lib/user-menu/user-menu.component.ts
+++ libs/common/src/lib/user-menu/user-menu.component.ts
@@ -8,10 +8,9 @@ import {
   Output,
 } from '@angular/core';
 import { Router } from '@angular/router';
-import { StateService } from '@flaps/core';
+import { SDKService, StateService, STFTrackingService } from '@flaps/core';
 import { Account, Welcome } from '@nuclia/core';
-import { stfAnimations } from '@flaps/pastanaga';
-import { Subject, takeUntil } from 'rxjs';
+import { map, shareReplay, Subject, takeUntil } from 'rxjs';
 import { AvatarModel } from '@guillotinaweb/pastanaga-angular';
 import { NavigationService } from '../services';
 
@@ -20,7 +19,6 @@ import { NavigationService } from '../services';
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: stfAnimations,
 })
 export class UserMenuComponent implements OnDestroy {
   @Input() set userInfo(userInfo: Welcome | undefined | null) {
@@ -40,12 +38,16 @@ export class UserMenuComponent implements OnDestroy {
   avatar: AvatarModel = {};
   accounts: string[] = [];
   account: Account | null = null;
+  isAccountManager = this.sdk.currentAccount.pipe(map((account) => account!.can_manage_account));
+  isBillingEnabled = this.tracking.isFeatureEnabled('billing').pipe(shareReplay(1));
   private unsubscribeAll = new Subject<void>();
 
   constructor(
     private router: Router,
     private stateService: StateService,
     private navigation: NavigationService,
+    private sdk: SDKService,
+    private tracking: STFTrackingService,
     private cdr: ChangeDetectorRef,
   ) {
     this.stateService.account.pipe(takeUntil(this.unsubscribeAll)).subscribe((account) => {
@@ -62,6 +64,11 @@ export class UserMenuComponent implements OnDestroy {
   goProfile() {
     this.close.emit();
     this.router.navigate(['/user/profile']);
+  }
+
+  goToBilling() {
+    this.close.emit();
+    this.router.navigate([`${this.navigation.getAccountManageUrl(this.account?.slug || '')}/billing`]);
   }
 
   switchAccount() {
