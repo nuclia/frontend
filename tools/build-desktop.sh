749217c22bb25fa20083632b1fbd4fb7ff40baef
Eric BREHAULT <ebrehault@gmail.com>
2023-04-28 14:24:43 +0200

    fix desktop build

--- tools/build-desktop.sh
+++ tools/build-desktop.sh
@@ -9,8 +9,4 @@ echo "Build desktop electron for production"
 ./node_modules/.bin/nx build desktop-electron -c production
 ./node_modules/.bin/tsc -p apps/desktop/electron/tsconfig.server.json
 cp -r dist/service ./dist/apps/desktop-electron/assets
-if [ -e dist/apps/desktop-electron/main.preload.ts.js ]
-then
-  mv dist/apps/desktop-electron/main.preload.ts.js dist/apps/desktop-electron/main.preload.js
-fi
 ./node_modules/.bin/nx run desktop-electron:make
