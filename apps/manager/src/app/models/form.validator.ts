53ce3c594b73b1c8e39fa96deb4f24378fcf1a62
mpellerin42 <mpellerin42@gmail.com>
2023-05-12 10:47:34 +0200

    Delete old manager app

--- apps/manager/src/app/models/form.validator.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
-
-export function validSlug(): ValidatorFn {
-  return (control: AbstractControl) => {
-    const slug = control.value;
-    const regex = /[A-Z_&@-\s/\\]/;
-    return regex.test(slug) ? { invalidCharacters: true } : null;
-  };
-}
--- apps/manager/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  backend: {
-    social_login: false,
-    new_api: true,
-  },
-};
