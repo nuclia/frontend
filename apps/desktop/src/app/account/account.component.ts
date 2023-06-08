19e4326f5b4aa6d91e1ce7831e973f080edf5bc3
Mat Pellerin <mpellerin42@gmail.com>
2023-06-08 17:37:21 +0200

    Fix [sc-5861]: Prevent bad account selection (#818)
    * Fix [sc-5861]: Prevent bad account selection
    
    In some corner cases happening only for people having several accounts on several environment (ie Stage and Prod), the Desktop ended up in a bad state where no account was selected. This PR is adding more checks and redirections to prevent this from happening.
    
    * Update Desktop version

--- apps/desktop/src/app/account/account.component.ts
+++ apps/desktop/src/app/account/account.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { SyncService } from '../sync/sync.service';
+import { ACCOUNT_KEY, SyncService } from '../sync/sync.service';
 import { SDKService } from '@flaps/core';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'nde-select-account',
@@ -10,11 +11,28 @@ import { SDKService } from '@flaps/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectAccountComponent {
-  accounts = this.sdk.nuclia.db.getAccounts();
+  accounts = this.sdk.nuclia.db.getAccounts().pipe(
+    tap((accounts) => {
+      if (accounts.length === 0) {
+        this.logout();
+      }
+    }),
+    catchError(() => {
+      this.logout();
+      return of([]);
+    }),
+  );
+
   constructor(private sdk: SDKService, private sync: SyncService, private router: Router) {}
 
   selectAccount(account: string) {
     this.sync.selectAccount(account);
+    this.router.navigate(['/']);
+  }
+
+  logout() {
+    localStorage.removeItem(ACCOUNT_KEY);
+    this.sdk.nuclia.auth.logout();
     this.router.navigate(['/']);
   }
 }
