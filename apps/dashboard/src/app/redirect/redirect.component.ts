108a028e29bda965eda5d72e7b673ea994ed3933
oriol <o.perramon@iskra.cat>
2023-06-08 19:41:23 +0200

    [sc-5890] Allow to show an authentication token

--- apps/dashboard/src/app/redirect/redirect.component.ts
+++ apps/dashboard/src/app/redirect/redirect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SDKService, UserService } from '@flaps/core';
 import { take, map, filter, switchMap, combineLatest } from 'rxjs';
@@ -7,13 +7,17 @@ const AUTHORIZED_REDIRECTS = ['nuclia-desktop://', 'http://localhost:4200'];
 const AUTHORIZED_REDIRECTS_REGEX = [/^chrome\-extension\:\/\/[a-z]+\/options\/options\.html$/];
 
 @Component({
-  template: '{{ fromChromeExtension ?  "Redirecting to Nuclia Chrome extension…" : "Redirecting to Nuclia Desktop…" }}',
+  templateUrl: './redirect.component.html',
+  styleUrls: ['./redirect.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RedirectComponent {
   fromChromeExtension = false;
   isValidToken = combineLatest([this.sdk.nuclia.auth.isAuthenticated(), this.userService.userInfo]).pipe(
     map(([authenticated, userInfo]) => !!(authenticated && userInfo)),
   );
+  token = this.sdk.nuclia.auth.getToken();
+  displayToken = this.route.queryParamMap.pipe(map((params) => params.has('display')));
 
   constructor(private sdk: SDKService, private userService: UserService, private route: ActivatedRoute) {
     this.isValidToken
@@ -27,14 +31,20 @@ export class RedirectComponent {
           this.fromChromeExtension = true;
         }
         let redirectUrl: string = params['redirect'] || '';
-        const matchRegex = AUTHORIZED_REDIRECTS_REGEX.some((regex) => regex.test(redirectUrl));
-        if (!matchRegex && !AUTHORIZED_REDIRECTS.includes(redirectUrl)) {
-          console.warn('Redirect URL not authorized');
-          return;
+        if (redirectUrl) {
+          const matchRegex = AUTHORIZED_REDIRECTS_REGEX.some((regex) => regex.test(redirectUrl));
+          if (!matchRegex && !AUTHORIZED_REDIRECTS.includes(redirectUrl)) {
+            console.warn('Redirect URL not authorized');
+            return;
+          }
+          redirectUrl += redirectUrl.includes('?') ? '&' : '?';
+          const tokens = `access_token=${this.sdk.nuclia.auth.getToken()}&refresh_token=${this.sdk.nuclia.auth.getRefreshToken()}`;
+          location.href = redirectUrl + tokens;
         }
-        redirectUrl += redirectUrl.includes('?') ? '&' : '?';
-        const tokens = `access_token=${this.sdk.nuclia.auth.getToken()}&refresh_token=${this.sdk.nuclia.auth.getRefreshToken()}`;
-        location.href = redirectUrl + tokens;
       });
+  }
+
+  copy() {
+    navigator.clipboard.writeText(this.token);
   }
 }
--- apps/dashboard/src/app/redirect/redirect.module.ts
+++ apps/dashboard/src/app/redirect/redirect.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { PaButtonModule } from '@guillotinaweb/pastanaga-angular';
 
 import { RedirectComponent } from './redirect.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, PaButtonModule, TranslateModule.forChild()],
   exports: [],
   declarations: [RedirectComponent],
   providers: [],
