956214ef76ba45a1f1436bce169e9caee01c82d0
mpellerin42 <mpellerin42@gmail.com>
2023-06-20 16:53:43 +0200

    Add indexes and remove unused function from resource.store
    `getFieldUrl` from `resource.store.ts` has been replaced by `getFieldUrl` from `viewer.store.ts` and wasn't used anymore.

--- libs/search-widget/src/index.js
+++ libs/search-widget/src/index.js
@@ -1,3 +1,3 @@
-export { default as NucliaSearchBar } from './widgets/search-widget/SearchBar.svelte';
-export { default as NucliaSearchResults } from './widgets/search-widget/SearchResults.svelte';
-export { default as NucliaViewerWidget } from './widgets/viewer-widget/ViewerWidget.svelte';
+export * from './core';
+
+export * from './widgets';
