7111105c277a382e8907f9b389e18920076a1787
Mat Pellerin <mpellerin42@gmail.com>
2023-06-23 14:34:21 +0200

    [sc-6030] Refactor onboarding form (#855)

--- libs/common/src/lib/user/onboarding/onboarding.component.ts
+++ libs/common/src/lib/user/onboarding/onboarding.component.ts
@@ -2,35 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { OnboardingService, OnboardingStatus } from './onboarding.service';
 import { OnboardingPayload } from './onboarding.models';
-import { STFTrackingService, Zone, ZoneService } from '@flaps/core';
+import { Zone, ZoneService } from '@flaps/core';
 import { Observable } from 'rxjs';
-import { OptionModel } from '@guillotinaweb/pastanaga-angular';
-import { TranslateService } from '@ngx-translate/core';
-
-const industries = [
-  'IT_software',
-  'marketing',
-  'finance',
-  'research',
-  'education',
-  'health',
-  'insurance',
-  'public_administration',
-  'sales',
-  'legal',
-  'telecommunications',
-  'engineering',
-  'pharma',
-  'startup',
-  'manufacturing',
-  'robotics',
-  'energy',
-  'chemical_industry',
-  'food_service',
-  'construction',
-  'retail',
-  'other',
-];
 
 const PHONE_INTERNATIONAL_CODE = new RegExp(/^[+][0-9s]+$/);
 const PHONE_NUMBER = new RegExp(/^[0-9\s]+$/);
@@ -44,48 +17,34 @@ const PHONE_NUMBER = new RegExp(/^[0-9\s]+$/);
 export class OnboardingComponent {
   onboardingForm = new FormGroup({
     company: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
-    industry: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
-    jobTitle: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+    use_case: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+    hosted_nucliadb: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+    organization_size: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
     phoneInternationalCode: new FormControl<string>('', {
       nonNullable: true,
-      validators: [Validators.pattern(PHONE_INTERNATIONAL_CODE)],
+      validators: [Validators.required, Validators.pattern(PHONE_INTERNATIONAL_CODE)],
     }),
     phoneNumber: new FormControl<string>('', {
       nonNullable: true,
-      validators: [Validators.pattern(PHONE_NUMBER)],
+      validators: [Validators.required, Validators.pattern(PHONE_NUMBER)],
     }),
-    searchEngine: new FormControl<string>('', { nonNullable: true }),
     getUpdates: new FormControl<boolean>(true, { nonNullable: true }),
   });
 
   validationMessages = {
-    industry: { required: 'validation.required' },
     company: { required: 'validation.required' },
-    jobTitle: { required: 'validation.required' },
-    phoneInternationalCode: { required: 'validation.required', pattern: 'onboarding.phone.invalid_code' },
+    use_case: { required: 'validation.required' },
+    hosted_nucliadb: { required: 'validation.required' },
+    organization_size: { required: 'validation.required' },
+    phoneInternationalCode: { required: 'validation.required_short', pattern: 'onboarding.phone.invalid_code' },
     phoneNumber: { required: 'validation.required', pattern: 'onboarding.phone.invalid_number' },
   };
 
   zones: Zone[] = [];
   onboardingStatus: Observable<OnboardingStatus> = this.onboardingService.onboardingState;
-  industryOptions: OptionModel[] = this.buildIndustryList();
 
-  constructor(
-    private onboardingService: OnboardingService,
-    private zoneService: ZoneService,
-    private translate: TranslateService,
-    private tracking: STFTrackingService,
-  ) {
+  constructor(private onboardingService: OnboardingService, private zoneService: ZoneService) {
     this.zoneService.getZones().subscribe((zones) => (this.zones = zones));
-    this.tracking.isFeatureEnabled('mandatory-phone').subscribe((enabled) => {
-      if (enabled) {
-        this.onboardingForm.get('phoneInternationalCode')?.addValidators([Validators.required]);
-        this.onboardingForm.get('phoneNumber')?.addValidators([Validators.required]);
-        // force validation to be refreshed
-        this.onboardingForm.patchValue({ phoneNumber: '' });
-        this.onboardingForm.updateValueAndValidity();
-      }
-    });
   }
 
   submitForm() {
@@ -95,33 +54,12 @@ export class OnboardingComponent {
     const formValue = this.onboardingForm.getRawValue();
     const data: OnboardingPayload = {
       company: formValue.company,
-      industry: formValue.industry,
-      job_title: formValue.jobTitle,
+      use_case: formValue.use_case,
+      hosted_nucliadb: formValue.hosted_nucliadb === 'yes',
+      organization_size: formValue.organization_size,
       phone: formValue.phoneNumber ? `${formValue.phoneInternationalCode} ${formValue.phoneNumber}` : undefined,
-      other_search_engines: formValue.searchEngine,
       receive_updates: formValue.getUpdates,
     };
     this.onboardingService.startOnboarding(data, this.zones[0].id);
-  }
-
-  private buildIndustryList(): OptionModel[] {
-    const list = industries
-      .map(
-        (industry) =>
-          new OptionModel({
-            id: industry,
-            value: industry,
-            label: this.translate.instant(`onboarding.industry.${industry}`),
-          }),
-      )
-      .sort((a, b) => a.label.toLocaleLowerCase().localeCompare(b.label.toLocaleLowerCase()));
-    list.push(
-      new OptionModel({
-        id: 'other',
-        value: 'other',
-        label: this.translate.instant('onboarding.industry.other'),
-      }),
-    );
-    return list;
   }
 }
