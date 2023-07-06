c486fe7e937b7f5699abf564f0f1ffb012c09d16
mpellerin42 <mpellerin42@gmail.com>
2023-07-06 11:57:50 +0200

    Implement image renderer

--- libs/search-widget/src/components/viewer/renderers/ImageRenderer.svelte
+++ libs/search-widget/src/components/viewer/renderers/ImageRenderer.svelte
@@ -1,3 +1,14 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { fieldData, getFieldUrl } from '../../../core';
+  import type { Observable } from 'rxjs';
 
-TODO: Image renderer
+  const imageUrl: Observable<string> = getFieldUrl();
+</script>
+
+<div class="sw-image-renderer">
+  <img src={$imageUrl} alt={$fieldData?.value?.file?.filename || ''} />
+</div>
+
+<style
+  lang="scss"
+  src="./ImageRenderer.scss"></style>
