b7ebeb6113f016392834a365ecd224a91a3a9dce
mpellerin42 <mpellerin42@gmail.com>
2023-07-17 12:50:29 +0200

    Spreadsheet renderer

--- apps/search-widget-demo/src/App.svelte
+++ apps/search-widget-demo/src/App.svelte
@@ -104,7 +104,7 @@
         knowledgebox={kb}
         lang="en"
         placeholder="Search"
-        features="filter,suggestions,permalink,relations,knowledgeGraph,zanswers,zonlyAnswers" />
+        features="filter,suggestions,permalink,relations,knowledgeGraph,answers,zonlyAnswers" />
       <NucliaSearchResults bind:this={resultsWidget} />
     </div>
   {/if}
--- libs/search-widget/src/components/viewer/renderers/SpreadsheetRenderer.svelte
+++ libs/search-widget/src/components/viewer/renderers/SpreadsheetRenderer.svelte
@@ -1,3 +1,71 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import Tabs from '../../../common/tabs/TabsList.svelte';
+  import Tab from '../../../common/tabs/TabItem.svelte';
+  import { combineLatest, filter, map, Subject, take } from 'rxjs';
+  import { fieldData } from '../../../core';
+  import { FileFieldExtractedData } from '@nuclia/core';
+  import { onMount } from 'svelte';
 
-TODO: Spreadsheet renderer
+  const selectedSheetId = new Subject<string>();
+  const sheets = fieldData.pipe(
+    map(
+      (field) => Object.values((field?.extracted as FileFieldExtractedData)?.file?.file_rows_previews || {})[0]?.sheets,
+    ),
+  );
+  const sheet = combineLatest([sheets, selectedSheetId]).pipe(map(([list, selected]) => list?.[selected]));
+
+  onMount(() => {
+    sheets
+      .pipe(
+        filter((list) => !!list),
+        take(1),
+      )
+      .subscribe((list) => {
+        select(Object.keys(list)[0] || '');
+      });
+  });
+
+  function select(id: string) {
+    selectedSheetId.next(id);
+  }
+</script>
+
+
+<div class="sw-spreadsheet-renderer">
+  <div class="tabs">
+    {#if $sheets && Object.keys($sheets).length > 1}
+      <Tabs>
+        {#each Object.entries($sheets) as [id]}
+          <Tab
+            on:click={select(id)}
+            active={$selectedSheetId === id}>
+            {id}
+          </Tab>
+        {/each}
+      </Tabs>
+    {/if}
+  </div>
+  <div class="table-container">
+    {#if $sheet}
+      <table>
+        {#each $sheet.rows || [] as row, index}
+          <tr>
+            {#each row.cell || [] as cell}
+              <td
+                class="ellipsis"
+                title={cell}>
+                {cell}
+              </td>
+            {/each}
+          </tr>
+        {/each}
+      </table>
+    {/if}
+  </div>
+</div>
+
+
+
+<style
+  lang="scss"
+  src="./SpreadsheetRenderer.scss"></style>
