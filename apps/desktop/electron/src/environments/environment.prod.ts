b83a14255927262aa701bb925c973d6f9f6dab31
Eric BREHAULT <ebrehault@gmail.com>
2023-08-30 15:50:28 +0200

    make sure we collect server errors in Sentry

--- apps/desktop/electron/src/environments/environment.prod.ts
+++ apps/desktop/electron/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   sentry_url: '__SENTRY_URL__',
-  sentry_environment: 'prod',
+  sentry_environment: 'desktop-prod',
   sentry_release: '__SENTRY_RELEASE__',
 };
