b776f2793501d63cb25e48e3780a989068e5d1a6
mpellerin42 <mpellerin42@gmail.com>
2023-09-19 09:42:08 +0200

    Finish upload data page
      - Redirect to resources page after upload
      - Display explicit download and open options for Nuclia desktop
      - Add missing translations

--- libs/common/src/lib/upload/upload-data/upload-data.component.ts
+++ libs/common/src/lib/upload/upload-data/upload-data.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { getDesktopAppUrl, getDesktopPlatform, RELEASE_URL } from '../../utils';
 import { UploadDialogService, UploadType } from '../../resources';
+import { filter } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'stf-upload-data',
@@ -9,13 +11,20 @@ import { UploadDialogService, UploadType } from '../../resources';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UploadDataComponent {
-  constructor(private uploadService: UploadDialogService) {}
+  constructor(
+    private uploadService: UploadDialogService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
 
   selectUpload(type: UploadType) {
-    this.uploadService.upload(type).onClose.pipe().subscribe(console.log);
+    this.uploadService
+      .upload(type)
+      .onClose.pipe(filter((data) => !data || !data.cancel))
+      .subscribe(() => this.router.navigate(['../resources'], { relativeTo: this.route }));
   }
 
-  openDesktop() {
+  downloadDesktop() {
     const platform = getDesktopPlatform();
     if (platform) {
       getDesktopAppUrl(platform).subscribe((url) => {
@@ -24,5 +33,9 @@ export class UploadDataComponent {
     } else {
       window.open(RELEASE_URL);
     }
+  }
+
+  openDesktop() {
+    window.open('nuclia-desktop://index.html');
   }
 }
