0a35cee76dd7d6412542a6ee7a5e80d6d4a51536
oriol <o.perramon@iskra.cat>
2023-09-25 14:22:30 +0200

    [sc-7203] add feedback when clicking on the copy button

--- apps/dashboard/src/app/redirect/redirect.module.ts
+++ apps/dashboard/src/app/redirect/redirect.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { PaButtonModule } from '@guillotinaweb/pastanaga-angular';
+import { PaButtonModule, PaIconModule } from '@guillotinaweb/pastanaga-angular';
 
 import { RedirectComponent } from './redirect.component';
 
 @NgModule({
-  imports: [CommonModule, PaButtonModule, TranslateModule.forChild()],
+  imports: [CommonModule, PaButtonModule, PaIconModule, TranslateModule.forChild()],
   exports: [],
   declarations: [RedirectComponent],
   providers: [],
