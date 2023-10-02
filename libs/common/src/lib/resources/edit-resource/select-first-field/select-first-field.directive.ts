2f7d9396ee2a8186a31bd07bd6df5d06cdf261bf
mpellerin42 <mpellerin42@gmail.com>
2023-10-02 17:55:39 +0200

    Fix select first field to select oldest field when possible

--- libs/common/src/lib/resources/edit-resource/select-first-field/select-first-field.directive.ts
+++ libs/common/src/lib/resources/edit-resource/select-first-field/select-first-field.directive.ts
@@ -2,13 +2,15 @@ import { Directive, inject, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EditResourceService } from '../edit-resource.service';
 import { filter, map, Observable, Subject, switchMap } from 'rxjs';
-import { FieldId, Resource } from '@nuclia/core';
+import { FIELD_TYPE, FieldId, Resource, ResourceField } from '@nuclia/core';
 import { takeUntil } from 'rxjs/operators';
+import { ResourceNavigationService } from '../resource-navigation.service';
 
 @Directive({
   selector: '[appSelectFirstField]',
 })
 export class SelectFirstFieldDirective implements OnDestroy {
+  private navigationService = inject(ResourceNavigationService);
   route: ActivatedRoute = inject(ActivatedRoute);
   router: Router = inject(Router);
   editResource: EditResourceService = inject(EditResourceService);
@@ -37,9 +39,22 @@ export class SelectFirstFieldDirective implements OnDestroy {
         takeUntil(this.unsubscribeAll),
       )
       .subscribe((fields) => {
-        const field = fields[0];
-        this.router.navigate([`./${field.field_type}/${field.field_id}`], { relativeTo: this.route, replaceUrl: true });
+        // Take the oldest field
+        const notGenericFields = fields.filter((field) => field.field_type !== FIELD_TYPE.generic);
+        notGenericFields.sort((a, b) => {
+          if (a.value && b.value && 'added' in a.value && 'added' in b.value) {
+            return (a.value.added || '').localeCompare(b.value?.added || '');
+          } else {
+            return 0;
+          }
+        });
+        const field: ResourceField = notGenericFields[0];
+        this.router.navigate([`./${field.field_type}/${field.field_id}`], {
+          relativeTo: this.route,
+          replaceUrl: true,
+        });
       });
+    this.navigationService.currentRoute = this.route;
   }
 
   ngOnDestroy(): void {
