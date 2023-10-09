1d00a2e6c3210a12d36c4c8ca60b0b1d64a9f4f9
mpellerin42 <mpellerin42@gmail.com>
2023-10-09 14:59:47 +0200

    [sc-7470]: add book a demo button in header
    Also update left and right spaces on the layout to match the latest designs.

--- libs/common/src/lib/base/base.module.ts
+++ libs/common/src/lib/base/base.module.ts
@@ -5,10 +5,11 @@ import { TopbarModule } from '../topbar';
 import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
 import { STFSidebarModule } from '../sidebar';
 import { NavbarModule } from '../navbar';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [BaseComponent, DashboardLayoutComponent],
   exports: [BaseComponent, DashboardLayoutComponent],
-  imports: [RouterModule, TopbarModule, STFSidebarModule, NavbarModule],
+  imports: [CommonModule, NavbarModule, RouterModule, STFSidebarModule, TopbarModule],
 })
 export class BaseModule {}
