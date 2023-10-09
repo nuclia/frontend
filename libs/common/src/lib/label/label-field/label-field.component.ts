a337f0676a2b219158d04a18874552dd30ed442b
oriol <o.perramon@iskra.cat>
2023-10-09 11:59:38 +0200

    Fixes

--- libs/common/src/lib/label/label-field/label-field.component.ts
+++ libs/common/src/lib/label/label-field/label-field.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Classification, LabelSetKind, LabelSets } from '@nuclia/core';
-import { BehaviorSubject, map, merge, ReplaySubject, tap } from 'rxjs';
+import { BehaviorSubject, map, ReplaySubject, Subject, takeUntil, tap } from 'rxjs';
 import { LabelsService } from '../labels.service';
 import { Size } from '@guillotinaweb/pastanaga-angular';
 import { switchMap } from 'rxjs/operators';
@@ -11,7 +11,7 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./label-field.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LabelFieldComponent {
+export class LabelFieldComponent implements OnDestroy, OnInit {
   @Input()
   set selection(value: Classification[]) {
     this._selection = value || [];
@@ -25,10 +25,11 @@ export class LabelFieldComponent {
   @Input() set kind(kind: LabelSetKind) {
     this._kind.next(kind);
   }
-  private _labelSets = new ReplaySubject<LabelSets>();
+
+  private _labelSets = new BehaviorSubject<LabelSets | undefined>(undefined);
   @Input() set labelSets(value: LabelSets | undefined) {
     if (value) {
-      this._labelSets.next(value);
+      this.labelSets$.next(value);
     }
   }
   @Input() size: Size | undefined;
@@ -37,23 +38,45 @@ export class LabelFieldComponent {
   @Output() selectionChange = new EventEmitter<Classification[]>();
   @Output() hasLabels = new EventEmitter<boolean>();
 
-  labelSets$ = merge(
-    this._labelSets,
-    this._kind.pipe(
-      switchMap((kind) =>
-        kind === LabelSetKind.RESOURCES ? this.labelsService.resourceLabelSets : this.labelsService.paragraphLabelSets,
-      ),
-    ),
-  );
+  labelSets$ = new ReplaySubject<LabelSets>();
   hasLabels$ = this.labelSets$.pipe(
     map((labels) => !!labels && Object.keys(labels).length > 0),
     tap((hasLabel) => this.hasLabels.emit(hasLabel)),
   );
+  unsubscribeAll = new Subject<void>();
 
   constructor(private labelsService: LabelsService) {}
+
+  ngOnInit() {
+    if (this._labelSets.getValue()) {
+      this._labelSets
+        .pipe(
+          map((labelsets) => labelsets as LabelSets),
+          takeUntil(this.unsubscribeAll),
+        )
+        .subscribe(this.labelSets$);
+    } else {
+      this._kind
+        .pipe(
+          switchMap((kind) =>
+            kind === LabelSetKind.RESOURCES
+              ? this.labelsService.resourceLabelSets
+              : this.labelsService.paragraphLabelSets,
+          ),
+          map((labelsets) => labelsets || {}),
+          takeUntil(this.unsubscribeAll),
+        )
+        .subscribe(this.labelSets$);
+    }
+  }
 
   updateSelection($event: Classification[]) {
     this.selection = [...$event];
     this.selectionChange.emit(this.selection);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeAll.next();
+    this.unsubscribeAll.complete();
   }
 }
