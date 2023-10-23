4515ea40f996c36cd91ca51bfb639e9a064517c5
Mat Pellerin <mpellerin42@gmail.com>
2023-10-23 13:44:14 +0200

    [sc-7503] Immediate retry+backoff on network errors (#1082)
    On Desktop:
    - retry 3 times when `createResource` fails (in `upload`) and when `createOrUpdateResource` fails (in `uploadLink`)
    - remove deprecated algolia destination and dependency
    - remove deprecated methods in destinations/nuclia-cloud

--- apps/desktop/src/app/sync/destinations/nuclia-cloud.ts
+++ apps/desktop/src/app/sync/destinations/nuclia-cloud.ts
@@ -1,15 +1,12 @@
 import { Account, IKnowledgeBoxItem, INuclia, Nuclia, NucliaOptions, WritableKnowledgeBox } from '@nuclia/core';
-import { catchError, from, map, Observable, of, switchMap, tap } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import {
   baseLogoPath,
-  ConnectorParameters,
   ConnectorSettings,
   DestinationConnectorDefinition,
   Field,
   IDestinationConnector,
 } from '../models';
-import { lookup } from 'mime-types';
-import { sha256 } from '../../utils';
 import { ACCOUNT_KEY } from '../sync.service';
 
 export const NucliaCloudKB: DestinationConnectorDefinition = {
@@ -49,70 +46,6 @@ class NucliaCloudKBImpl implements IDestinationConnector {
     return of(true);
   }
 
-  upload(
-    originalId: string,
-    filename: string,
-    params: ConnectorParameters,
-    data: { blob?: Blob; metadata?: any },
-  ): Observable<void> {
-    if (params && params['kb'] && data.blob) {
-      const blob = data.blob;
-      const mimetype = lookup(filename) || 'application/octet-stream';
-      return this.getAccount().pipe(
-        tap((account) => {
-          const applicableLimit = this.isMedia(mimetype)
-            ? account.limits?.upload.upload_limit_max_media_file_size
-            : account.limits?.upload.upload_limit_max_non_media_file_size;
-          if (applicableLimit && blob.size > applicableLimit) {
-            console.error(`File too large. Size=${blob.size}, limit=${applicableLimit}`);
-            throw new Error(`File "${filename}" is too large.`);
-          }
-        }),
-        switchMap(() => this.getKb(params['kb'])),
-        switchMap((kb) =>
-          from(sha256(originalId)).pipe(
-            switchMap((slug) =>
-              kb.getResourceBySlug(slug, [], []).pipe(
-                catchError((error) => {
-                  if (error.status === 404) {
-                    return kb
-                      .createResource({ slug, title: filename }, true)
-                      .pipe(map((data) => kb.getResourceFromData({ id: data.uuid })));
-                  } else {
-                    throw error;
-                  }
-                }),
-              ),
-            ),
-            switchMap((resource) => {
-              return resource.upload('file', new File([blob], filename), false, {
-                contentType: mimetype,
-              });
-            }),
-          ),
-        ),
-        map(() => undefined),
-      );
-    } else {
-      return of(undefined);
-    }
-  }
-
-  uploadLink(
-    filename: string,
-    params: ConnectorParameters,
-    data: { uri: string; extra_headers: { [key: string]: string } },
-  ): Observable<void> {
-    if (params && params['kb']) {
-      return this.getKb(params['kb']).pipe(
-        switchMap((kb) => kb.createResource({ title: filename, files: { [filename]: { file: data } } })),
-        map(() => undefined),
-      );
-    } else {
-      return of(undefined);
-    }
-  }
-
   private getKbField(): Observable<Field> {
     const request: Observable<IKnowledgeBoxItem[]> = this.nuclia.options.standalone
       ? this.nuclia.db.getStandaloneKbs().pipe(
@@ -146,29 +79,5 @@ class NucliaCloudKBImpl implements IDestinationConnector {
           }),
       })),
     );
-  }
-
-  private getKb(slug: string): Observable<WritableKnowledgeBox> {
-    if (!this.kbs[slug]) {
-      return this.nuclia.db
-        .getKnowledgeBox(localStorage.getItem(ACCOUNT_KEY) || '', slug)
-        .pipe(tap((kb) => (this.kbs[slug] = kb)));
-    } else {
-      return of(this.kbs[slug]);
-    }
-  }
-
-  private getAccount(): Observable<Account> {
-    if (!this.account) {
-      return this.nuclia.db
-        .getAccount(localStorage.getItem(ACCOUNT_KEY) || '')
-        .pipe(tap((account) => (this.account = account)));
-    } else {
-      return of(this.account);
-    }
-  }
-
-  private isMedia(mimetype: string): boolean {
-    return mimetype.startsWith('image/') || mimetype.startsWith('video/') || mimetype.startsWith('audio/');
   }
 }
