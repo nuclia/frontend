ca908b2c20c63e89f0b5bc0f36fed94d608bcd65
Eric BREHAULT <ebrehault@gmail.com>
2023-10-25 14:46:19 +0200

    Hixe account type button when not relevant

--- libs/common/src/lib/topbar/topbar.component.ts
+++ libs/common/src/lib/topbar/topbar.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SDKService, UserService } from '@flaps/core';
-import { map, take } from 'rxjs';
+import { combineLatest, map, shareReplay, take } from 'rxjs';
 import { NavigationService } from '../services';
 import { StandaloneService } from '../services/standalone.service';
 
@@ -15,8 +15,18 @@ export class TopbarComponent {
   userInfo = this.userService.userInfo;
   kb = this.sdk.currentKb;
   isStage = location.hostname === 'stashify.cloud';
-  accountType = this.sdk.currentAccount.pipe(map((account) => account.type));
-  isAccountManager = this.sdk.currentAccount.pipe(map((account) => account.can_manage_account));
+  private _accountType = this.sdk.currentAccount.pipe(shareReplay());
+  accountType = this._accountType.pipe(map((account) => account.type));
+  isAccountManager = this._accountType.pipe(map((account) => account.can_manage_account));
+  isTypeEnforced = combineLatest([this.accountType, this.route.queryParams.pipe(map((params) => params['type']))]).pipe(
+    map(([currentType, nextType]) => nextType && currentType !== nextType),
+  );
+  shouldAccountTypeBeVisible = combineLatest([this.accountType, this.isTypeEnforced, this.isAccountManager]).pipe(
+    map(
+      ([accountType, isTypeEnforced, isAccountManager]) =>
+        !!accountType && accountType !== 'stash-enterprise' && !isTypeEnforced && isAccountManager,
+    ),
+  );
   billingUrl = this.sdk.currentAccount.pipe(
     map((account) => this.navigationService.getAccountManageUrl(account.slug) + '/billing'),
   );
@@ -32,6 +42,7 @@ export class TopbarComponent {
     private userService: UserService,
     private navigationService: NavigationService,
     private sdk: SDKService,
+    private route: ActivatedRoute,
     private standaloneService: StandaloneService,
   ) {}
 
