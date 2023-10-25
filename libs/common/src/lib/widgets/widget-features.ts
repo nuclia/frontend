4a9ef3429abae2eafec49091836a3c9537eb689e
Eric BREHAULT <ebrehault@gmail.com>
2023-10-25 17:48:10 +0200

    add knowledgeGraph in editor (#1086)

--- libs/common/src/lib/widgets/widget-features.ts
+++ libs/common/src/lib/widgets/widget-features.ts
@@ -15,6 +15,7 @@ export const DEFAULT_FEATURES: WidgetFeatures = {
   autofilter: false,
   noBM25forChat: false,
   hideLogo: false,
+  knowledgeGraph: false,
 };
 
 export const DEFAULT_FEATURES_LIST: string[] = Object.entries(DEFAULT_FEATURES)
