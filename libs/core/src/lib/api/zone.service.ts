f929110f2cdc7048d0c9082d8542bea97346b2a1
Eric BREHAULT <ebrehault@gmail.com>
2023-10-30 17:00:13 +0100

    manage a blocklist for zones (#1092)

--- libs/core/src/lib/api/zone.service.ts
+++ libs/core/src/lib/api/zone.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { DeprecatedApiService } from './deprecated-api.service';
 import { Zone } from '../models';
-import { Observable } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
+import { FeatureFlagService } from '../analytics/feature-flag.service';
 
 const VERSION = 'v1';
 const ZONES = 'zones';
@@ -10,9 +11,20 @@ const ZONES = 'zones';
   providedIn: 'root',
 })
 export class ZoneService {
-  constructor(private api: DeprecatedApiService) {}
+  constructor(
+    private api: DeprecatedApiService,
+    private featureFlagService: FeatureFlagService,
+  ) {}
 
   getZones(): Observable<Zone[]> {
-    return this.api.get(`/${VERSION}/${ZONES}`, true, undefined, true);
+    return this.api
+      .get(`/${VERSION}/${ZONES}`, true, undefined, true)
+      .pipe(
+        switchMap((zones) =>
+          this.featureFlagService
+            .getFeatureBlocklist('zones')
+            .pipe(map((blocklist) => (zones as Zone[]).filter((zone) => !blocklist.includes(zone.slug)))),
+        ),
+      );
   }
 }
