e7203248a562c5d20b1384000fb9fef815232353
operramon <52449656+operramon@users.noreply.github.com>
2023-11-02 14:24:41 +0100

    [sc-7711] Fix Infinite loop when invited to an account (#1094)

--- libs/common/src/lib/guards/select-account-kb.guard.ts
+++ libs/common/src/lib/guards/select-account-kb.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { SDKService } from '@flaps/core';
 import { SelectAccountKbService } from '../select-account-kb/select-account-kb.service';
 import { NavigationService } from '../services';
@@ -14,10 +14,10 @@ export const selectAccountKbGuard = (route: ActivatedRouteSnapshot) => {
   const sdk: SDKService = inject(SDKService);
 
   return selectService.refresh().pipe(
-    map(({ accounts, kbs }) => {
+    switchMap(({ accounts, kbs }) => {
       // No accounts
       if (accounts.length === 0) {
-        return router.createUrlTree(['/user/onboarding']);
+        return of(router.createUrlTree(['/user/onboarding']));
       }
 
       if (accounts.length === 1) {
@@ -27,29 +27,33 @@ export const selectAccountKbGuard = (route: ActivatedRouteSnapshot) => {
 
         // One account without kbs
         if (numKbs === 0) {
-          return router.createUrlTree([navigation.getAccountUrl(accountSlug)]);
+          return sdk.currentAccount.pipe(
+            map((account) =>
+              account.can_manage_account ? router.createUrlTree([navigation.getAccountUrl(accountSlug)]) : true,
+            ),
+          );
         }
 
         // One account with one kb
         else if (numKbs === 1) {
           const stashSlug = kbs[accountSlug][0].slug!;
-          return router.createUrlTree([navigation.getKbUrl(accountSlug, stashSlug)]);
+          return of(router.createUrlTree([navigation.getKbUrl(accountSlug, stashSlug)]));
         }
 
         // One account with multiple kbs
         else {
           if (hasInvalidParams(route, accounts)) {
-            return router.createUrlTree(['/select']);
+            return of(router.createUrlTree(['/select']));
           } else {
-            return true;
+            return of(true);
           }
         }
       }
 
       if (hasInvalidParams(route, accounts)) {
-        return router.createUrlTree(['/select']);
+        return of(router.createUrlTree(['/select']));
       }
-      return true;
+      return of(true);
     }),
     catchError(() => {
       return of(router.createUrlTree(['/user/logout']));
