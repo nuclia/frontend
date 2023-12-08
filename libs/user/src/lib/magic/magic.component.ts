080ff6a7cbeac11a483a24e1b277e473389236b4
Mat Pellerin <mpellerin42@gmail.com>
2023-12-08 11:13:57 +0100

    [sc-8028] new onboarding form in hubspot (#1179)
    * [sc-8028]: onboarding form updates
    
      - rewording and new options on use case question
      - new learn about us question
      - no more hosting question
    
    * Remove join button from magic component
    
    This magic component is used for both account creation and invitations. Having a `join` button was not a problem for invitation, but it was not an option for regular account creation.
    So we improved the error management flow of this step, and we put it back automatic.

--- libs/user/src/lib/magic/magic.component.ts
+++ libs/user/src/lib/magic/magic.component.ts
@@ -1,63 +1,59 @@
 import { MagicActionError, TokenService } from '@flaps/core';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { MagicService } from './magic.service';
-import { catchError, map, of, Subject, takeUntil, tap } from 'rxjs';
-import { SisToastService } from '@nuclia/sistema';
+import { catchError, filter, map, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'stf-magic',
   templateUrl: './magic.component.html',
-  styleUrls: ['./magic.component.scss'],
 })
 export class MagicComponent implements OnInit, OnDestroy {
   private unsubscribeAll = new Subject<void>();
 
-  token = '';
+  error = '';
   constructor(
     private tokenService: TokenService,
     private magicService: MagicService,
     private route: ActivatedRoute,
     private router: Router,
-    private toaster: SisToastService,
+    private cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit() {
     this.route.queryParamMap
       .pipe(
         takeUntil(this.unsubscribeAll),
-        map((params: ParamMap) => params.get('token')),
+        map((params: ParamMap) => {
+          const token = params.get('token');
+          if (!token) {
+            this.error = 'Missing token in the URL';
+            this.cdr.markForCheck();
+          }
+          return token;
+        }),
+        filter((token) => !!token),
+        switchMap((token) => this.tokenService.validate(token as string)),
+        tap((data) => this.magicService.execute(data)),
+        catchError((error) => {
+          const cause = error.detail as MagicActionError;
+          if (cause === 'local_user_already_exists' || cause === 'user_registered_as_external_user') {
+            this.error = `login.${cause}`;
+          } else {
+            this.error = 'login.token_expired';
+          }
+          this.router.navigate(['/user/login'], {
+            queryParams: { message: this.error },
+          });
+          return of(null);
+        }),
       )
-      .subscribe((token) => {
-        if (!token) {
-          this.toaster.error('Missing token in the URL');
-        }
-        this.token = token || '';
+      .subscribe({
+        error: () => {
+          this.error = 'onboarding.failed';
+          this.cdr.markForCheck();
+        },
       });
-  }
-
-  join() {
-    if (this.token) {
-      this.tokenService
-        .validate(this.token)
-        .pipe(
-          tap((data) => this.magicService.execute(data)),
-          catchError((error) => {
-            let message: string;
-            const cause = error.detail as MagicActionError;
-            if (cause === 'local_user_already_exists' || cause === 'user_registered_as_external_user') {
-              message = `login.${cause}`;
-            } else {
-              message = 'login.token_expired';
-            }
-            this.router.navigate(['/user/login'], {
-              queryParams: { message: message },
-            });
-            return of(null);
-          }),
-        )
-        .subscribe();
-    }
   }
 
   ngOnDestroy() {
