a9ecd26b384cdcde082772078fa1b7ac863f5f99
Mat Pellerin <mpellerin42@gmail.com>
2023-12-11 14:52:51 +0100

    [sc-7250] Prevent NER management page to get frozen (#1185)
    Display no more than 1000 rows in the NER list, and display a message when some NERs are hidden.

--- libs/common/src/lib/entities/entity-list/entity-list.component.ts
+++ libs/common/src/lib/entities/entity-list/entity-list.component.ts
@@ -6,7 +6,6 @@ import {
   EventEmitter,
   Input,
   OnDestroy,
-  OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
@@ -14,13 +13,15 @@ import { Subject } from 'rxjs';
 import { Entity, NerFamily } from '../model';
 import { EntitiesService } from '../entities.service';
 
+const COUNT_ROWS_DISPLAYED = 1000;
+
 @Component({
   selector: 'app-entity-list',
   templateUrl: './entity-list.component.html',
   styleUrls: ['./entity-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EntityListComponent implements OnInit, OnDestroy {
+export class EntityListComponent implements OnDestroy {
   @Input()
   set family(value: NerFamily | undefined) {
     const oldValue = this._family;
@@ -33,14 +34,19 @@ export class EntityListComponent implements OnInit, OnDestroy {
     return this._family;
   }
 
+  totalNERs = 0;
+  displayedNERs = COUNT_ROWS_DISPLAYED;
+
   get entities(): Entity[] {
-    return Object.values(this.family?.entities || {})
+    const nerList = Object.values(this.family?.entities || {})
       .filter((entity) => {
         return !entity.merged && this.filterQuery.length >= 3
           ? entity.value.toLocaleLowerCase().includes(this.filterQuery.toLocaleLowerCase())
           : !entity.merged;
       })
       .sort((a, b) => a.value.localeCompare(b.value));
+    this.totalNERs = nerList.length;
+    return nerList.slice(0, COUNT_ROWS_DISPLAYED);
   }
 
   @Input() selection: string[] = [];
@@ -58,15 +64,9 @@ export class EntityListComponent implements OnInit, OnDestroy {
     private entitiesService: EntitiesService,
   ) {}
 
-  ngOnInit(): void {}
-
   ngOnDestroy(): void {
     this.unsubscribeAll.next();
     this.unsubscribeAll.complete();
-  }
-
-  trackByValue(index: number, ner: Entity) {
-    return ner.value;
   }
 
   removeDuplicate(ner: Entity, duplicate: string) {
