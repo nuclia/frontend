8b5a80fa9a400275c7ae121586c4531bffb4137d
mpellerin42 <mpellerin42@gmail.com>
2023-12-28 16:20:30 +0100

    [sc-8084] Better spacing management in select account/kb page

--- libs/common/src/lib/select-account-kb/utils.ts
+++ libs/common/src/lib/select-account-kb/utils.ts
@@ -5,31 +5,17 @@ const duration = '350ms ease-in-out';
 export const selectAnimations = [
   trigger('slideInOut', [
     transition('false => true', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-        }),
-      ]),
       query(':enter', [style({ transform: 'translateX(100%)', opacity: 0 })], { optional: true }),
       group([
-        query(':leave', [animate(duration, style({ transform: 'translateX(-100%)', opacity: 0 }))], { optional: true }),
-        query(':enter', [animate(duration, style({ transform: 'translateX(0)', opacity: 1 }))], { optional: true }),
+        query(':leave', [animate(duration, style({ transform: 'translateX(-100%)', opacity: 0 }))], {
+          optional: true,
+        }),
+        query(':enter', [animate(duration, style({ transform: 'translateX(0)', opacity: 1 }))], {
+          optional: true,
+        }),
       ]),
     ]),
     transition('true => false', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-        }),
-      ]),
       query(':enter', [style({ transform: 'translateX(-100%)', opacity: 0 })]),
       group([
         query(':leave', [animate(duration, style({ transform: 'translateX(100%)', opacity: 0 }))]),
