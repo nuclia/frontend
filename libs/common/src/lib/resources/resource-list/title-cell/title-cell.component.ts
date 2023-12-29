a02b5bc7805b6871a207bbb49561ffc9e8a25ef1
mpellerin42 <mpellerin42@gmail.com>
2023-12-29 14:06:57 +0100

    [sc-8106] Render the resource title as link in resource list
    In resource list, we created a new `title-cell` component because all the resource tables (processed, pending and error) are sharing the same code for the title cell.
    
    Now the resource title is a real link (but we kept the same style, except it is underlined on hover), so we can easily open it on a new tab. Clicking anywhere else on the title cell is still navigating to the resource on the same tab as it used to.

---
+++ libs/common/src/lib/resources/resource-list/title-cell/title-cell.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResourceWithLabels } from '@flaps/common';
+import { PaIconModule } from '@guillotinaweb/pastanaga-angular';
+import { SisIconsModule } from '@nuclia/sistema';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'stf-title-cell',
+  standalone: true,
+  imports: [CommonModule, RouterModule, PaIconModule, SisIconsModule],
+  templateUrl: './title-cell.component.html',
+  styleUrl: './title-cell.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TitleCellComponent {
+  @Input() row?: ResourceWithLabels;
+
+  onClickLink($event: MouseEvent) {
+    $event.stopPropagation();
+  }
+}
