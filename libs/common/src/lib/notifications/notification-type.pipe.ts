040330e909a5d44e98ac8ed6cb4617d75202994a
mpellerin42 <mpellerin42@gmail.com>
2024-02-06 12:08:26 +0100

    Fix unit tests

--- libs/common/src/lib/notifications/notification-type.pipe.ts
+++ libs/common/src/lib/notifications/notification-type.pipe.ts
@@ -6,17 +6,19 @@ import { NotificationUI } from './notification.model';
   standalone: true,
 })
 export class NotificationTypePipe implements PipeTransform {
-  transform(notification: NotificationUI, failure: boolean): string {
+  transform(notification: NotificationUI): string {
     if (notification.type !== 'resource') {
       return '';
     }
     switch (notification.operation) {
       case 'created':
-        return failure ? 'notification.type.creation-failed' : 'notification.type.creation-completed';
+        return notification.failure ? 'notification.type.creation-failed' : 'notification.type.creation-completed';
       case 'modified':
-        return failure ? 'notification.type.modification-failed' : 'notification.type.modification-completed';
+        return notification.failure
+          ? 'notification.type.modification-failed'
+          : 'notification.type.modification-completed';
       case 'deleted':
-        return failure ? 'notification.type.deletion-failed' : 'notification.type.deletion-completed';
+        return notification.failure ? 'notification.type.deletion-failed' : 'notification.type.deletion-completed';
     }
   }
 }
