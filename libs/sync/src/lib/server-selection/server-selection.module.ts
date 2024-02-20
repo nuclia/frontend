fa49afcf059ddf11d1e6d198b15ba8d69ab82dff
oriol <o.perramon@iskra.cat>
2024-02-20 12:46:59 +0100

    Modify sync server settings form

--- libs/sync/src/lib/server-selection/server-selection.module.ts
+++ libs/sync/src/lib/server-selection/server-selection.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ServerSelectionComponent } from './server-selection.component';
 import { PaButtonModule, PaTextFieldModule } from '@guillotinaweb/pastanaga-angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { ServerSetupComponent } from './server-setup.component';
 
 @NgModule({
-  declarations: [ServerSelectionComponent],
-  imports: [CommonModule, PaButtonModule, TranslateModule, PaTextFieldModule],
+  declarations: [ServerSelectionComponent, ServerSetupComponent],
+  imports: [CommonModule, PaButtonModule, ReactiveFormsModule, TranslateModule, PaTextFieldModule],
 })
 export class ServerSelectionModule {}
---
+++ libs/sync/src/lib/server-selection/server-setup.component.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { LOCAL_SYNC_SERVER, SYNC_SERVER_KEY } from '../sync/new-sync.service';
+import { SyncService } from '../sync/sync.service';
+import { NavigationService } from '@flaps/core';
+import { take } from 'rxjs';
+
+@Component({
+  selector: 'nsy-server-setup',
+  templateUrl: './server-setup.component.html',
+  styleUrls: ['./server-setup.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ServerSetupComponent {
+  localUrl = LOCAL_SYNC_SERVER;
+  serverUrl = new FormControl<string>(localStorage.getItem(SYNC_SERVER_KEY) || '', { nonNullable: true });
+
+  constructor(
+    private sync: SyncService,
+    private navigationService: NavigationService,
+    private router: Router,
+  ) {}
+
+  save() {
+    this.sync.setSyncServer({ url: this.serverUrl.value, local: false });
+    this.navigationService.kbUrl.pipe(take(1)).subscribe((url) => this.router.navigate([`${url}/upload`]));
+  }
+}
