aafd186fc853dd163c8fec417c6ab74b6a6bd8ec
oriol <o.perramon@iskra.cat>
2024-03-04 13:01:11 +0100

    Do not fetch all the entities in the dashboard

--- libs/common/src/lib/entities/entities.service.ts
+++ libs/common/src/lib/entities/entities.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
-import { catchError, filter, map, switchMap, take, tap } from 'rxjs/operators';
+import { catchError, filter, switchMap, take, tap } from 'rxjs/operators';
 import { SDKService, STFUtils } from '@flaps/core';
 import { Entities, EntitiesGroup, Entity, IKnowledgeBox, UpdateEntitiesGroupPayload } from '@nuclia/core';
 
@@ -20,12 +20,15 @@ export class EntitiesService {
           this.entitiesSubject.next(null);
         }),
         filter((kb) => !!kb),
-        switchMap((kb) => this._refreshEntities(kb)),
+        switchMap((kb) => kb.getEntities()),
       )
-      .subscribe();
+      .subscribe({
+        next: (entities) => this.entitiesSubject.next(entities),
+        error: () => this.entitiesSubject.next(null),
+      });
   }
 
-  createFamily(family: { color: string; entities: string; title: string }): Observable<Entities> {
+  createFamily(family: { color: string; entities: string; title: string }): Observable<EntitiesGroup> {
     const groupId = STFUtils.generateSlug(family.title);
     const group: EntitiesGroup = {
       title: family.title,
@@ -36,7 +39,7 @@ export class EntitiesService {
 
     return this.sdk.currentKb.pipe(
       take(1),
-      switchMap((kb) => kb.createEntitiesGroup(groupId, group).pipe(switchMap(() => this._refreshEntities(kb)))),
+      switchMap((kb) => kb.createEntitiesGroup(groupId, group).pipe(switchMap(() => this._refreshFamily(kb, groupId)))),
     );
   }
 
@@ -48,7 +51,7 @@ export class EntitiesService {
       title: string;
     },
     entitiesBackup: string | undefined,
-  ): Observable<Entities> {
+  ): Observable<EntitiesGroup> {
     const payload: UpdateEntitiesGroupPayload = {
       title: family.title,
       color: family.color,
@@ -66,7 +69,9 @@ export class EntitiesService {
     }
     return this.sdk.currentKb.pipe(
       take(1),
-      switchMap((kb) => kb.updateEntitiesGroup(groupId, payload).pipe(switchMap(() => this._refreshEntities(kb)))),
+      switchMap((kb) =>
+        kb.updateEntitiesGroup(groupId, payload).pipe(switchMap(() => this._refreshFamily(kb, groupId))),
+      ),
     );
   }
 
@@ -86,18 +91,18 @@ export class EntitiesService {
     );
   }
 
-  addEntitiesToFamily(groupId: string, entities: string[]): Observable<Entities> {
+  addEntitiesToFamily(groupId: string, entities: string[]): Observable<EntitiesGroup> {
     return this.sdk.currentKb.pipe(
       take(1),
       switchMap((kb) =>
         kb
           .updateEntitiesGroup(groupId, { add: this.formatEntitiesToAdd(entities), update: {}, delete: [] })
-          .pipe(switchMap(() => this._refreshEntities(kb))),
+          .pipe(switchMap(() => this._refreshFamily(kb, groupId))),
       ),
     );
   }
 
-  addDuplicate(groupId: string, duplicatedEntities: Entity[], mainEntity: Entity): Observable<Entities> {
+  addDuplicate(groupId: string, duplicatedEntities: Entity[], mainEntity: Entity): Observable<EntitiesGroup> {
     const allValues = duplicatedEntities.map((entity) => entity.value);
     if (mainEntity.represents) {
       allValues.push(...mainEntity.represents);
@@ -127,12 +132,12 @@ export class EntitiesService {
             update,
             delete: [],
           })
-          .pipe(switchMap(() => this._refreshEntities(kb))),
+          .pipe(switchMap(() => this._refreshFamily(kb, groupId))),
       ),
     );
   }
 
-  removeDuplicate(groupId: string, mainEntity: Entity, duplicate: string): Observable<Entities> {
+  removeDuplicate(groupId: string, mainEntity: Entity, duplicate: string): Observable<EntitiesGroup> {
     mainEntity.represents = mainEntity.represents?.filter((d) => d !== duplicate);
 
     const entities = this.entitiesSubject.getValue();
@@ -154,30 +159,39 @@ export class EntitiesService {
             },
             delete: [],
           })
-          .pipe(switchMap(() => this._refreshEntities(kb))),
+          .pipe(switchMap(() => this._refreshFamily(kb, groupId))),
       ),
     );
   }
 
-  deleteEntities(groupId: string, entities: Entity[]): Observable<Entities> {
+  deleteEntities(groupId: string, entities: Entity[]): Observable<EntitiesGroup> {
     return this.sdk.currentKb.pipe(
       take(1),
       switchMap((kb) =>
         kb
           .updateEntitiesGroup(groupId, { add: {}, update: {}, delete: entities.map((entity) => entity.value) })
-          .pipe(switchMap(() => this._refreshEntities(kb))),
+          .pipe(switchMap(() => this._refreshFamily(kb, groupId))),
       ),
     );
   }
 
-  private _refreshEntities(kb: IKnowledgeBox): Observable<Entities> {
-    return kb.getEntities(true).pipe(
+  refreshFamily(familyId: string): Observable<EntitiesGroup> {
+    return this.sdk.currentKb.pipe(
+      take(1),
+      switchMap((kb) => this._refreshFamily(kb, familyId)),
+    );
+  }
+
+  private _refreshFamily(kb: IKnowledgeBox, familyId: string): Observable<EntitiesGroup> {
+    return kb.getEntitiesGroup(familyId).pipe(
       catchError(() => {
-        this.entitiesSubject.next(null);
         return EMPTY;
       }),
-      tap((entities) => {
-        this.entitiesSubject.next(entities);
+      tap((family) => {
+        this.entitiesSubject.next({
+          ...this.entitiesSubject.getValue(),
+          [familyId]: family,
+        });
       }),
     );
   }
