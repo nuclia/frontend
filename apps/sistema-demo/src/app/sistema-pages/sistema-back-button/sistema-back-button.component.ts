85e614006f4d87f1c5cdd1eb889d33ab6aaabc44
mpellerin42 <mpellerin42@gmail.com>
2024-03-15 17:47:58 +0100

    Sistema improvements
      - update border-radius to be 4px everywhere
      - add solid border on basic buttons (except on icon buttons)
      - fix labels demo page + improve padding
      - Update back-button to be just a link

--- apps/sistema-demo/src/app/sistema-pages/sistema-back-button/sistema-back-button.component.ts
+++ apps/sistema-demo/src/app/sistema-pages/sistema-back-button/sistema-back-button.component.ts
@@ -5,5 +5,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SistemaBackButtonComponent {
-  code = `<nsi-back-button link=".."></nsi-back-button>`;
+  code = `<nsi-back-button link="..">Back</nsi-back-button>`;
 }
--- apps/sistema-demo/src/app/sistema-pages/sistema-label/sistema-label.component.ts
+++ apps/sistema-demo/src/app/sistema-pages/sistema-label/sistema-label.component.ts
@@ -1,36 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LABEL_COLORS } from '@nuclia/sistema';
 
 @Component({
   templateUrl: './sistema-label.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SistemaLabelComponent {
-  code = `<pa-chip-selectionable textColor="#1D00C2FF" borderColor="#1D00C2FF" backgroundColor="#C7BDFFFF"
-                       [selected]="selection === 1"
-                       (select)="onSelect(1)">Blue label
-</pa-chip-selectionable>
+  code = `<nsi-label [color]="LABEL_COLORS[0].mainColor">Blue label</nsi-label>`;
 
-<pa-chip-selectionable [selected]="selection === 2"
-                       (select)="onSelect(2)">Black label
-</pa-chip-selectionable>
-
-<pa-chip-selectionable textColor="#C7A900FF" borderColor="#C7A900FF" backgroundColor="#FFF8D1FF"
-                       [selected]="selection === 3"
-                       (select)="onSelect(3)">Yellow label
-</pa-chip-selectionable>
-
-<pa-chip-selectionable textColor="#0CAD55" borderColor="#0CAD55" backgroundColor="#DAF3E6"
-                       [selected]="selection === 4"
-                       (select)="onSelect(4)">Green label
-</pa-chip-selectionable>`;
-
-  selection?: number;
-
-  onSelect(id: number) {
-    if (this.selection !== id) {
-      this.selection = id;
-    } else {
-      this.selection = undefined;
-    }
-  }
+  colors = LABEL_COLORS;
 }
