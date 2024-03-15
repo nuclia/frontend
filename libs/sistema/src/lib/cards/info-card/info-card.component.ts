0a36c0ba4b6853c3d018bc2e13e859fa7245d6f9
mpellerin42 <mpellerin42@gmail.com>
2024-03-15 09:20:24 +0100

    [sc-9351] info-card: add and document icon variant

--- libs/sistema/src/lib/cards/info-card/info-card.component.ts
+++ libs/sistema/src/lib/cards/info-card/info-card.component.ts
@@ -1,16 +1,18 @@
 import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaIconModule } from '@guillotinaweb/pastanaga-angular';
 
 @Component({
   selector: 'nsi-info-card',
   standalone: true,
-  imports: [CommonModule],
-  template: '<ng-content></ng-content>',
+  imports: [CommonModule, PaIconModule],
+  templateUrl: './info-card.component.html',
   styleUrl: './info-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoCardComponent {
   @Input() type: 'default' | 'warning' | 'highlight' = 'default';
+  @Input() icon?: string;
 
   @HostBinding('class.warning') get warning() {
     return this.type === 'warning';
