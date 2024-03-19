c4952d3cfb70b637ff2e927a0167963be55f70a3
Mat Pellerin <mpellerin42@gmail.com>
2024-03-19 17:29:43 +0100

    [sc-9390] Use tables with or without border depending on the context (#1430)
    * [sc-9390] Use new pastanaga table style
    
    * Clean up old KB training pages
    
    * Use Pastanaga Table demo page on sistema documentation
    
    We did override the table page just for the look and feel of the button in the table, which is a bit of a nonsense. We just know we should use basic buttons on tables, no need for a special for this.
    
    * Increase spacing between User table and its header.

--- apps/sistema-demo/src/app/sistema-pages/pastanaga-pages-override/index.ts
+++ apps/sistema-demo/src/app/sistema-pages/pastanaga-pages-override/index.ts
@@ -2,5 +2,4 @@ export * from './sistema-confirmation-dialog/sistema-confirmation-dialog.compone
 export * from './sistema-icons/sistema-icons.component';
 export * from './sistema-modal';
 export * from './sistema-scrollbar/sistema-scrollbar.component';
-export * from './sistema-tables/sistema-table.component';
 export * from './sistema-toasts/sistema-toast.component';
