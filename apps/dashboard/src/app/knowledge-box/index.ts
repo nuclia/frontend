1dfa0fb6fc9d52966113657f3e9ba43c59e917a2
mpellerin42 <mpellerin42@gmail.com>
2024-03-19 15:55:45 +0100

    Clean up old KB training pages

--- apps/dashboard/src/app/knowledge-box/index.ts
+++ apps/dashboard/src/app/knowledge-box/index.ts
@@ -1,7 +1,6 @@
 export * from './knowledge-box/knowledge-box.component';
 export * from './knowledge-box-home/knowledge-box-home.component';
 export * from './knowledge-box-keys/knowledge-box-keys.component';
-export * from './knowledge-box-training';
 export * from './knowledge-box-users';
 export * from './service-access/service-access.component';
 export * from './knowledge-box.module';
--- apps/dashboard/src/app/knowledge-box/knowledge-box-training/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './knowledge-box-training.component';
-export * from './knowledge-box-training.module';
-export * from './training-history/training-history.component';
