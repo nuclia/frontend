6049a55cb73aa2bb40c2004ec392026e55bbce6c
mpellerin42 <mpellerin42@gmail.com>
2024-03-26 12:52:11 +0100

    Task list UI

---
+++ apps/dashboard/src/app/tasks-automation/task-list-item/task-list-item.component.ts
@@ -0,0 +1,71 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaButtonModule, PaDateTimeModule, PaTableModule, PaTogglesModule } from '@guillotinaweb/pastanaga-angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { BadgeComponent } from '@nuclia/sistema';
+
+interface BaseTask {
+  count: { total: number; processed: number };
+  creationDate: string;
+  filters: { label: string; count?: number }[];
+  hasPrompt?: boolean;
+  type: 'automated' | 'one-time';
+  fieldName?: string;
+  labelSet?: string;
+  nerFamily?: string;
+}
+
+interface AutomatedTask extends BaseTask {
+  running: boolean;
+  type: 'automated';
+}
+
+interface OneTimeTask extends BaseTask {
+  status: 'completed' | 'progress' | 'error';
+  type: 'one-time';
+}
+
+@Component({
+  selector: 'app-task-list-item',
+  standalone: true,
+  imports: [
+    CommonModule,
+    PaButtonModule,
+    TranslateModule,
+    PaTableModule,
+    PaButtonModule,
+    PaDateTimeModule,
+    BadgeComponent,
+    PaTogglesModule,
+    PaTogglesModule,
+  ],
+  templateUrl: './task-list-item.component.html',
+  styleUrl: './task-list-item.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TaskListItemComponent {
+  @Input() taskType:
+    | 'summarize'
+    | 'global-question'
+    | 'q-and-a'
+    | 'label-resources'
+    | 'label-text-blocks'
+    | 'label-ners' = 'summarize';
+  @Input() taskTitle = '';
+  @Input() taskDescription = '';
+  @Input() taskList: (AutomatedTask | OneTimeTask)[] = [];
+
+  get firstColumn(): { field: 'fieldName' | 'labelSet' | 'nerFamily'; header: string } {
+    switch (this.taskType) {
+      case 'summarize':
+      case 'global-question':
+      case 'q-and-a':
+        return { field: 'fieldName', header: 'tasks-automation.table.header.field-name' };
+      case 'label-resources':
+      case 'label-text-blocks':
+        return { field: 'labelSet', header: 'tasks-automation.table.header.label-set' };
+      case 'label-ners':
+        return { field: 'nerFamily', header: 'tasks-automation.table.header.ner-family' };
+    }
+  }
+}
--- apps/dashboard/src/app/tasks-automation/tasks-automation.component.ts
+++ apps/dashboard/src/app/tasks-automation/tasks-automation.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { TaskListItemComponent } from './task-list-item/task-list-item.component';
 
 @Component({
   selector: 'app-tasks-automation',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, TaskListItemComponent],
   templateUrl: './tasks-automation.component.html',
   styleUrl: './tasks-automation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
