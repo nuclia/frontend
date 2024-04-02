64cf8a029bb659e733fc66c3c8b60a3dffbb8681
mpellerin42 <mpellerin42@gmail.com>
2024-04-02 16:21:22 +0200

    Scroll top when navigating between task automation pages

--- libs/common/src/lib/tasks-automation/tasks-automation.component.ts
+++ libs/common/src/lib/tasks-automation/tasks-automation.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tasks-automation',
@@ -9,4 +11,23 @@ import { RouterOutlet } from '@angular/router';
   template: '<router-outlet></router-outlet>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TasksAutomationComponent {}
+export class TasksAutomationComponent implements OnDestroy {
+  private unsubscribeAll = new Subject<void>();
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.unsubscribeAll),
+      )
+      .subscribe(() => {
+        // Scroll back to top on navigation (don't know why but our scrollPositionRestoration configuration isn't working on those pages)
+        document.querySelector('.dashboard-content')?.scrollTo(0, 0);
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeAll.next();
+    this.unsubscribeAll.complete();
+  }
+}
