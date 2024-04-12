81b9a5fa10cba5c1a7aa21b9206b3d0d363248fe
mpellerin42 <mpellerin42@gmail.com>
2024-04-12 12:49:58 +0200

    [sc-9695] Remove libs/sdk-core/protobuf

--- libs/sdk-core/protobuf/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ./build/protobuf
-mkdir -p build/protobuf
-cp package.json *.md ../../../LICENSE.md ./build/protobuf
-node export_to_json.mjs
-sed "s/__MODEL__/$(cat build/model.json)/" index.mjs > build/protobuf/index.mjs
