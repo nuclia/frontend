1bd510dac01b126a8a1b076973042b2cde06d0c5
oriol <o.perramon@iskra.cat>
2024-05-07 16:24:13 +0200

    [sc-10092] Fix e2e when local storage is empty

--- apps/dashboard/src/app/knowledge-box/knowledge-box/knowledge-box.component.ts
+++ apps/dashboard/src/app/knowledge-box/knowledge-box/knowledge-box.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs';
+import { forkJoin, take } from 'rxjs';
 import { SisModalService } from '@nuclia/sistema';
-import { SDKService } from '@flaps/core';
+import { FeaturesService, SDKService } from '@flaps/core';
 import { GETTING_STARTED_DONE_KEY } from '@nuclia/user';
 import { GettingStartedComponent } from '../../onboarding/getting-started/getting-started.component';
 import { WelcomeInExistingKBComponent } from '../../onboarding/welcome-in-existing-kb/welcome-in-existing-kb.component';
@@ -12,19 +12,22 @@ import { WelcomeInExistingKBComponent } from '../../onboarding/welcome-in-existi
 export class KnowledgeBoxComponent implements OnInit {
   constructor(
     private sdk: SDKService,
+    private features: FeaturesService,
     private modalService: SisModalService,
   ) {}
 
   ngOnInit() {
     const gettingStartedDone = localStorage.getItem(GETTING_STARTED_DONE_KEY) === 'true';
     if (!gettingStartedDone) {
-      this.sdk.counters.pipe(take(1)).subscribe((counters) => {
-        if (counters.resources === 0) {
-          this.modalService.openModal(GettingStartedComponent);
-        } else {
-          this.modalService.openModal(WelcomeInExistingKBComponent);
-        }
-      });
+      forkJoin([this.sdk.counters.pipe(take(1)), this.features.isKbAdmin.pipe(take(1))]).subscribe(
+        ([counters, isKbAdmin]) => {
+          if (counters.resources === 0 && isKbAdmin) {
+            this.modalService.openModal(GettingStartedComponent);
+          } else if (!isKbAdmin) {
+            this.modalService.openModal(WelcomeInExistingKBComponent);
+          }
+        },
+      );
     }
   }
 }
