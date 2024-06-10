cdd3191d5c9784a29b5c3a8465e5a8908c869087
Eric BREHAULT <ebrehault@gmail.com>
2024-06-10 09:25:55 +0200

    more tests

--- libs/sync/src/lib/sync-details-page/sync-details-page.component.spec.ts
+++ libs/sync/src/lib/sync-details-page/sync-details-page.component.spec.ts
@@ -1,7 +1,7 @@
 import { createRoutingFactory, mockProvider, SpectatorRouting } from '@ngneat/spectator/jest';
 import { SyncDetailsPageComponent } from './sync-details-page.component';
 import { SyncService } from '../logic';
-import { SisToastService } from '@nuclia/sistema';
+import { SisModalService, SisToastService } from '@nuclia/sistema';
 import { of } from 'rxjs';
 import { SDKService } from '@flaps/core';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -10,6 +10,12 @@ import { SvgIconRegistryService } from 'angular-svg-icon';
 describe('SyncDetailsPageComponent', () => {
   let spectator: SpectatorRouting<SyncDetailsPageComponent>;
   let syncService: SyncService;
+
+  global.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
 
   const createService = createRoutingFactory({
     component: SyncDetailsPageComponent,
@@ -28,6 +34,7 @@ describe('SyncDetailsPageComponent', () => {
       mockProvider(SDKService, {
         currentKb: of({
           id: 'testKb',
+          getLabels: () => of({}),
         }),
       }),
       mockProvider(SyncService, {
@@ -39,6 +46,9 @@ describe('SyncDetailsPageComponent', () => {
             connector: {
               name: 'dropbox',
             },
+            foldersToSync: [
+              { uuid: 'folder1', title: 'Folder 1', originalId: 'folder1', metadata: { displayPath: '/folder1' } },
+            ],
           }),
         connectors: {
           dropbox: {
@@ -48,10 +58,34 @@ describe('SyncDetailsPageComponent', () => {
               logo: `/dropbox.svg`,
               description: 'File storage and synchronization service developed by Dropbox',
               permanentSyncOnly: true,
+              allowToSelectFolders: true,
             },
           },
         },
+        getConnector: jest.fn(() => ({
+          allowToSelectFolders: true,
+          getParametersSections: () => of([]),
+        })),
         updateSync: jest.fn(() => of(undefined)),
+        isSyncing: of(false),
+        getLogs: jest.fn(() =>
+          of([
+            {
+              message: 'Synchronization finished',
+              level: 'low',
+              createdAt: '2024-06-03T07:15:42.978Z',
+              action: 'finish-synchronization-sync-object',
+              payload: {
+                from: '9512775a-1a27-4c03-a8ea-8cddd3f38958-gdrive6-ajtiyi',
+                to: '9512775a-1a27-4c03-a8ea-8cddd3f38958',
+                date: '2024-06-03T07:15:42.978Z',
+                processed: [],
+                successCount: 0,
+                error: '',
+              },
+            },
+          ]),
+        ),
       }),
       mockProvider(SvgIconRegistryService, { loadSvg: () => {} }),
     ],
@@ -62,11 +96,12 @@ describe('SyncDetailsPageComponent', () => {
     syncService = spectator.inject(SyncService);
   });
 
-  it('should display sync', () => {
+  it('should display sync activity', () => {
     expect(spectator.query('h1.page-title')?.textContent).toContain('Test Sync 123');
+    expect(spectator.query('.activity-logs-container')?.textContent).toContain('Synchronization finished');
   });
 
-  it('should toggle the sync status', () => {
+  it('should toggle the sync activity status', () => {
     expect(spectator.query('[qa="active-toggle"]')?.textContent).toContain('sync.badge.active');
     spectator.click('[qa="active-toggle"] input');
     spectator.detectChanges();
@@ -74,5 +109,27 @@ describe('SyncDetailsPageComponent', () => {
     spectator.click('[qa="active-toggle"] input');
     spectator.detectChanges();
     expect(syncService.updateSync).toHaveBeenCalledWith('123', { disabled: false });
+  });
+
+  it('should open confirm dialog when triggering sync', () => {
+    const modalService = spectator.inject(SisModalService);
+    const openConfirm = jest.spyOn(modalService, 'openConfirm');
+    spectator.click('[qa="sync-now"]');
+    spectator.detectChanges();
+    expect(openConfirm).toHaveBeenCalled();
+  });
+
+  it('should open confirm dialog when deleting sync', () => {
+    const modalService = spectator.inject(SisModalService);
+    const openConfirm = jest.spyOn(modalService, 'openConfirm');
+    spectator.click('[qa="delete"]');
+    spectator.detectChanges();
+    expect(openConfirm).toHaveBeenCalled();
+  });
+
+  it('should display the selected folders', () => {
+    spectator.click('[qa="folders-tab"]');
+    spectator.detectChanges();
+    expect(spectator.query('nsy-folders-tab')?.textContent).toContain('/folder1');
   });
 });
