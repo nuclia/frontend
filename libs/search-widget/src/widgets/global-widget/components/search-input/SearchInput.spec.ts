7a0706d9d454c07f558b0361b64c314043aca07f
Eric BREHAULT <ebrehault@gmail.com>
2024-08-22 14:58:10 +0200

    fix test

--- libs/search-widget/src/widgets/global-widget/components/search-input/SearchInput.spec.ts
+++ libs/search-widget/src/widgets/global-widget/components/search-input/SearchInput.spec.ts
@@ -3,7 +3,7 @@ import { fireEvent, render } from '@testing-library/svelte';
 import { firstValueFrom } from 'rxjs';
 import { searchQuery } from '../../../../core/stores/search.store';
 
-export async function shouldEmitQuery(searchInput: HTMLInputElement | null) {
+export async function shouldEmitQuery(searchInput: HTMLTextAreaElement | null) {
   expect(searchInput).toBeTruthy();
   if (searchInput) {
     await fireEvent.input(searchInput, { target: { value: 'Who is Batman?' } });
@@ -16,7 +16,7 @@ export async function shouldEmitQuery(searchInput: HTMLInputElement | null) {
 describe('Search input', () => {
   it('should emit query', async () => {
     const { container } = render(SearchInput);
-    const searchInput = container.querySelector('input');
+    const searchInput = container.querySelector('textarea');
     await shouldEmitQuery(searchInput);
   });
 });
