7a86f6be133594d2d6322f6b95d5acb787199200
operramon <52449656+operramon@users.noreply.github.com>
2024-09-09 15:35:17 +0200

    [sc-10999] Add clear button in the chat (#1723)

--- libs/search-widget/src/components/answer/ChatInput.svelte
+++ libs/search-widget/src/components/answer/ChatInput.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Textarea from '../../common/textarea/Textarea.svelte';
-  import Icon from '../../common/icons/Icon.svelte';
+  import { Icon, IconButton } from '../../common';
   import { _ } from '../../core/i18n';
   import { ask } from '../../core/stores/effects';
 
@@ -26,15 +26,30 @@
       askQuestion();
     }
   };
+
+  const clear = () => {
+    question = '';
+  };
 </script>
 
 <div
   class="sw-chat-input"
   class:fullscreen
   class:highlight={isListening}>
-  <div class="icon">
-    <Icon name="chat" />
-  </div>
+  {#if question.length === 0}
+    <div class="chat-icon">
+      <Icon name="chat" />
+    </div>
+  {:else}
+    <div class="clear">
+      <IconButton
+        aspect="basic"
+        icon="cross"
+        ariaLabel={$_('input.clear')}
+        size="small"
+        on:click={clear} />
+    </div>
+  {/if}
   <Textarea
     name="nuclia-chat-field"
     bind:this={inputElement}
