4b436a2203ec638ce0f9e396a2ffb327e99cf4d7
operramon <52449656+operramon@users.noreply.github.com>
2024-10-04 16:22:04 +0200

    [sc-11086] Questions without answer in activity logs (#1758)

--- apps/dashboard/src/app/activity/activity-download.component.ts
+++ apps/dashboard/src/app/activity/activity-download.component.ts
@@ -6,7 +6,7 @@ import { format } from 'date-fns';
 import { WINDOW } from '@ng-web-apis/common';
 
 type Tab = 'resources' | 'searches';
-type ActivityTab = EventType | 'feedback';
+type ActivityTab = EventType | 'feedback' | 'no-answer';
 
 @Component({
   selector: 'app-activity-download',
@@ -16,7 +16,7 @@ type ActivityTab = EventType | 'feedback';
 })
 export class ActivityDownloadComponent {
   activityTabs: { [tab in Tab]: ActivityTab[] } = {
-    searches: [EventType.CHAT, EventType.SEARCH, 'feedback'],
+    searches: [EventType.CHAT, EventType.SEARCH, 'feedback', 'no-answer'],
     resources: [EventType.PROCESSED, EventType.NEW, EventType.MODIFIED],
   };
   selectedTab: Tab = 'resources';
@@ -26,9 +26,15 @@ export class ActivityDownloadComponent {
     (acc, tab) => {
       acc[tab] = this.sdk.currentKb.pipe(
         take(1),
-        switchMap((kb) =>
-          tab === 'feedback' ? kb.listFeedback() : kb.listActivityDownloads(tab).pipe(map((res) => res.downloads)),
-        ),
+        switchMap((kb) => {
+          if (tab === 'feedback') {
+            return kb.listFeedback();
+          } else if (tab === 'no-answer') {
+            return kb.listActivityDownloads(EventType.CHAT).pipe(map((res) => res.downloads));
+          } else {
+            return kb.listActivityDownloads(tab).pipe(map((res) => res.downloads));
+          }
+        }),
         map((res) =>
           res.sort((a, b) => a.localeCompare(b) * -1).map((month) => ({ month, formatted: this.formatDate(month) })),
         ),
@@ -47,6 +53,10 @@ export class ActivityDownloadComponent {
   ) {}
 
   download(month: string) {
+    if (this.selectedActivityTab === 'no-answer') {
+      this.downloadQuestionsWithoutAnswer(month);
+      return;
+    }
     this.sdk.currentKb
       .pipe(
         take(1),
@@ -79,5 +89,40 @@ export class ActivityDownloadComponent {
     const [year, month] = value.split('-');
     const date = new Date(parseInt(year), parseInt(month) - 1, 2);
     return format(date, 'MMMM yyyy');
+  }
+
+  downloadQuestionsWithoutAnswer(month: string) {
+    this.sdk.currentKb
+      .pipe(
+        take(1),
+        switchMap((kb) =>
+          kb.activityMonitor
+            .queryRemiScores({
+              status: 'NO_CONTEXT',
+              month,
+            })
+            .pipe(map((data) => ({ data, kb }))),
+        ),
+      )
+      .subscribe(({ data, kb }) => {
+        const filename = `activity_${kb.id}_no-answer_${month}.csv`;
+        const header = 'Question without answer\n';
+        const rows = data.map((item) => this.formatCsvRow(item.question)).join('\n');
+        this.downloadFile(filename, header + rows, 'text/csv');
+      });
+  }
+
+  private downloadFile(filename: string, content: string, mime: string) {
+    const blob = new Blob([content], { type: `${mime};charset=utf-8` });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', filename);
+    link.click();
+    URL.revokeObjectURL(url);
+  }
+
+  private formatCsvRow(text: string) {
+    return `"${text.replace(/"/g, '""')}"`;
   }
 }
