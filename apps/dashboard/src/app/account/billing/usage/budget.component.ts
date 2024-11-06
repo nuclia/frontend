f4c2e3cf19b5da8b7020bfd0fd78ba322c3d5333
operramon <52449656+operramon@users.noreply.github.com>
2024-11-06 15:22:15 +0100

    [sc-11211] Remove the option to set unlimited budget (#1827)

--- apps/dashboard/src/app/account/billing/usage/budget.component.ts
+++ apps/dashboard/src/app/account/billing/usage/budget.component.ts
@@ -10,10 +10,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BudgetComponent implements OnDestroy {
-  budgetValidators = [Validators.required, Validators.min(1)];
   form = new FormGroup({
-    budget: new FormControl<number | null>(null, { validators: this.budgetValidators }),
-    type: new FormControl<'unlimited' | 'limited' | null>(null, { validators: [Validators.required] }),
+    budget: new FormControl<number | null>(null, { validators: [Validators.required, Validators.min(1)] }),
     action: new FormControl<'BLOCK_ACCOUNT' | 'WARN_ACCOUNT_OWNER'>('BLOCK_ACCOUNT', {
       validators: [Validators.required],
     }),
@@ -27,23 +25,12 @@ export class BudgetComponent implements OnDestroy {
   constructor(private billing: BillingService) {
     this.billing
       .getSubscription()
-      .pipe(
-        filter((subscription) => !!subscription),
-        map((subscription) => {
-          const budget = subscription?.subscription?.on_demand_budget;
-          return {
-            budget: typeof budget === 'number' ? budget : null,
-            action: subscription?.subscription?.action_on_budget_exhausted || 'BLOCK_ACCOUNT',
-          };
-        }),
-      )
-      .subscribe(({ budget, action }) => {
-        const type = typeof budget === 'number' ? 'limited' : 'unlimited';
-        this.form.patchValue({ budget, action, type });
-        this.updateValidation(type);
-        this.budgetChange.emit({
-          on_demand_budget: budget,
-          action_on_budget_exhausted: this.showActions ? action : undefined,
+      .pipe(filter((subscription) => !!subscription))
+      .subscribe((subscription) => {
+        const budget = subscription?.subscription?.on_demand_budget;
+        this.form.patchValue({
+          budget: typeof budget === 'number' ? budget : null,
+          action: subscription?.subscription?.action_on_budget_exhausted || 'BLOCK_ACCOUNT',
         });
       });
 
@@ -54,8 +41,8 @@ export class BudgetComponent implements OnDestroy {
           this.budgetChange.emit(
             this.form.valid
               ? {
-                  on_demand_budget: values.type === 'limited' ? (values.budget as number) : null,
-                  action_on_budget_exhausted: this.showActions && values.type === 'limited' ? values.action : undefined,
+                  on_demand_budget: values.budget as number,
+                  action_on_budget_exhausted: this.showActions ? values.action : undefined,
                 }
               : undefined,
           );
@@ -68,12 +55,5 @@ export class BudgetComponent implements OnDestroy {
   ngOnDestroy() {
     this.unsubscribeAll.next();
     this.unsubscribeAll.complete();
-  }
-
-  updateValidation(type: 'limited' | 'unlimited') {
-    type === 'limited'
-      ? this.form.controls.budget.addValidators(this.budgetValidators)
-      : this.form.controls.budget.removeValidators(this.budgetValidators);
-    this.form.controls.budget.updateValueAndValidity();
   }
 }
