e619d3114699df79413ab4d27ff5776a66c24981
Eric BREHAULT <ebrehault@gmail.com>
2024-11-08 17:13:36 +0100

    support JSON

--- libs/common/src/lib/tasks-automation/task-forms/ask/ask.component.ts
+++ libs/common/src/lib/tasks-automation/task-forms/ask/ask.component.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BackButtonComponent, InfoCardComponent, TwoColumnsConfigurationItemComponent } from '@nuclia/sistema';
+import {
+  BackButtonComponent,
+  InfoCardComponent,
+  SisToastService,
+  TwoColumnsConfigurationItemComponent,
+} from '@nuclia/sistema';
 import { TranslateModule } from '@ngx-translate/core';
 import { TaskFormCommonConfig, TaskFormComponent } from '../task-form.component';
-import { PaTextFieldModule } from '@guillotinaweb/pastanaga-angular';
+import { PaTextFieldModule, PaTogglesModule } from '@guillotinaweb/pastanaga-angular';
 import { TaskRouteDirective } from '../task-route.directive';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TasksAutomationService } from '../../tasks-automation.service';
 import { TaskApplyTo } from '@nuclia/core';
+import { json } from 'd3';
 
 @Component({
   standalone: true,
@@ -20,6 +26,7 @@ import { TaskApplyTo } from '@nuclia/core';
     TaskFormComponent,
     TranslateModule,
     TwoColumnsConfigurationItemComponent,
+    PaTogglesModule,
   ],
   templateUrl: './ask.component.html',
   styleUrl: '../_task-form.common.scss',
@@ -27,12 +34,45 @@ import { TaskApplyTo } from '@nuclia/core';
 })
 export class AskComponent extends TaskRouteDirective {
   taskAutomation = inject(TasksAutomationService);
+  toaster = inject(SisToastService);
   askForm = new FormGroup({
+    isJSON: new FormControl<boolean>(false),
     question: new FormControl<string>('', [Validators.required]),
     fieldName: new FormControl<string>('', [Validators.required]),
   });
 
+  jsonExample = {
+    name: 'book',
+    description: 'Information about the book',
+    parameters: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'string',
+          description: 'The title of the book',
+        },
+        author: {
+          type: 'string',
+          description: 'The author of the book',
+        },
+        ref_num: {
+          type: 'string',
+          description: 'The ISBN of the book',
+        },
+      },
+      required: ['title', 'author', 'ref_num'],
+    },
+  };
+
   activateTask(commonConfig: TaskFormCommonConfig) {
+    if (this.askForm.get('isJSON')?.value) {
+      try {
+        JSON.parse(this.askForm.get('question')?.value || '');
+      } catch (e) {
+        this.toaster.error('tasks-automation.generator.your-question.invalid-json');
+        return;
+      }
+    }
     this.taskAutomation
       .startTask(
         'ask',
@@ -44,6 +84,7 @@ export class AskComponent extends TaskRouteDirective {
           operations: [
             {
               ask: {
+                json: this.askForm.get('isJSON')?.value,
                 question: this.askForm.get('question')?.value,
                 destination: this.askForm.get('fieldName')?.value,
               },
