c768bd0ce65a2296d3b29e0b7f1ffb3b22609861
Carlos Lopez <9074620+rastut@users.noreply.github.com>
2025-02-05 10:25:10 +0100

    Change build args

--- apps/manager-v2/src/app/app.component.ts
+++ apps/manager-v2/src/app/app.component.ts
@@ -22,8 +22,8 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor(
     private ngxTranslate: TranslateService,
-    private config: BackendConfigurationService,
     private paTranslate: PaTranslateService,
+    private config: BackendConfigurationService,
   ) {
     const userLocale = localStorage.getItem(userLocaleKey);
     this.initTranslate(userLocale);
