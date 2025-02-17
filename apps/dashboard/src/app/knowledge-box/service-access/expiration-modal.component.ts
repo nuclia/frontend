0c6d29be826684aa736571e52ba53588bfab9673
oriol <o.perramon@iskra.cat>
2025-02-17 18:56:01 +0100

    [sc-11711] Validate the expiration date of keys

--- apps/dashboard/src/app/knowledge-box/service-access/expiration-modal.component.ts
+++ apps/dashboard/src/app/knowledge-box/service-access/expiration-modal.component.ts
@@ -1,22 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DateAfter } from '@flaps/common';
 import { ModalRef, PaButtonModule, PaDatePickerModule, PaModalModule } from '@guillotinaweb/pastanaga-angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  imports: [CommonModule, PaDatePickerModule, PaModalModule, TranslateModule, PaButtonModule],
+  imports: [CommonModule, PaDatePickerModule, PaModalModule, TranslateModule, PaButtonModule, ReactiveFormsModule],
   templateUrl: './expiration-modal.component.html',
   styleUrl: './expiration-modal.component.scss',
 })
 export class ExpirationModalComponent {
-  expiration = new Date();
+  expiration = new FormControl<Date>(new Date(), {
+    nonNullable: true,
+    validators: [DateAfter(new Date().toISOString()), Validators.required],
+  });
 
   constructor(public modal: ModalRef<void, { expiration: Date }>) {
-    this.expiration.setFullYear(this.expiration.getFullYear() + 1);
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 1);
+    this.expiration.setValue(date);
   }
 
   add() {
-    this.modal.close({ expiration: this.expiration });
+    this.modal.close({ expiration: this.expiration.getRawValue() });
   }
 
   close() {
