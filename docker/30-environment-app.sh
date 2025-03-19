8c68ba7d5bd116d96e42cb78745f17ab435a8378
Eric BREHAULT <ebrehault@gmail.com>
2025-03-19 17:26:49 +0100

    re-brand page title

--- docker/30-environment-app.sh
+++ docker/30-environment-app.sh
@@ -56,6 +56,7 @@ function apply_path {
     else
       echo "Re-branding to '$BRAND_NAME'";
       sed -i "s/Nuclia/$BRAND_NAME/" /dist/assets/i18n/**/*.json
+      sed -i "s/<title>Nuclia<\/title>/<title>$BRAND_NAME<\/title>/" /dist/index.html
     fi
 
     if [ "$BRAND_DOMAIN" == nuclia.cloud ]; then
