df2e555aa2899352dfdb08ddb8930ee484e513fa
mpellerin42 <mpellerin42@gmail.com>
2025-04-04 12:43:24 +0200

    Add link component between agent boxes when needed
    Improve workspace layout with proper overflow

--- apps/dashboard/src/app/retrieval-agent/agent-dashboard/agent-dashboard.component.ts
+++ apps/dashboard/src/app/retrieval-agent/agent-dashboard/agent-dashboard.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaButtonModule } from '@guillotinaweb/pastanaga-angular';
 import { AgentBoxComponent } from './agent-box/agent-box.component';
 import { ConnectableEntryComponent } from './connectable-entry/connectable-entry.component';
 import { ArrowDownComponent } from './arrow-down.component';
+import { LinkService } from './link/link.service';
 
 @Component({
   imports: [CommonModule, ArrowDownComponent, PaButtonModule, AgentBoxComponent, ConnectableEntryComponent],
@@ -11,4 +20,14 @@ import { ArrowDownComponent } from './arrow-down.component';
   styleUrl: './agent-dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AgentDashboardComponent {}
+export class AgentDashboardComponent implements AfterViewInit {
+  private linkService = inject(LinkService);
+
+  @ViewChild('linkContainer') linkContainer?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (this.linkContainer) {
+      this.linkService.container = this.linkContainer;
+    }
+  }
+}
