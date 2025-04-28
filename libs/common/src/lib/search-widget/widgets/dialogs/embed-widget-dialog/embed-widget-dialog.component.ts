4b1c988d4c4b9fc66fad2dc0aae7af6cc85756b8
oriol <o.perramon@iskra.cat>
2025-04-28 11:24:14 +0200

    Fix nucliadb admin

--- libs/common/src/lib/search-widget/widgets/dialogs/embed-widget-dialog/embed-widget-dialog.component.ts
+++ libs/common/src/lib/search-widget/widgets/dialogs/embed-widget-dialog/embed-widget-dialog.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ModalRef, PaButtonModule, PaModalModule, PaTogglesModule } from '@guillotinaweb/pastanaga-angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { InfoCardComponent } from '@nuclia/sistema';
+import { StandaloneService } from 'libs/common/src/lib/services';
 
 @Component({
   selector: 'stf-embed-widget-dialog',
@@ -15,8 +16,12 @@ export class EmbedWidgetDialogComponent {
   synchronize = false;
   snippet = this.modal.config?.data?.code.snippet;
   synchSnippet = this.modal.config?.data?.code.synchSnippet;
+  standalone = this.standaloneService.standalone;
 
-  constructor(public modal: ModalRef<{ code: { snippet: string; synchSnippet?: string } }>) {}
+  constructor(
+    public modal: ModalRef<{ code: { snippet: string; synchSnippet?: string } }>,
+    private standaloneService: StandaloneService,
+  ) {}
 
   copyAndClose() {
     navigator.clipboard.writeText(this.synchronize ? this.synchSnippet || '' : this.snippet || '');
