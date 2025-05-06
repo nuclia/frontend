487b61ae3beca11c9aa9ee7b58a00ac21233e261
mpellerin42 <mpellerin42@gmail.com>
2025-05-06 14:45:01 +0200

    Update deepEqual function to support deepInclude mode
    Fix jest warning on test setup in all projects

---
+++ libs/core/src/lib/utils/deep-equal.spec.ts
@@ -0,0 +1,25 @@
+import { deepEqual } from './deep-equal';
+
+describe('deepEqual', () => {
+  const object2 = { a: 1, b: '2', c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' } };
+
+  describe('with deepOnlcuded true', () => {
+    it('should return true if two objects are equivalent', () => {
+      expect(deepEqual({ a: 1, b: '2', c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' } }, object2, true)).toBe(true);
+    });
+    it('should return true if object1 is fully included in object 2', () => {
+      expect(deepEqual({ c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' } }, object2, true)).toBe(true);
+    });
+    it('should return false if object1 is not fully included in object 2', () => {
+      expect(deepEqual({ c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' }, f: 2 }, object2, true)).toBe(false);
+      expect(deepEqual({ a: 1, b: '2', c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' }, f: 2 }, object2, true)).toBe(false);
+    });
+    it('should return false if object1 has the same properties than object2 but not the same values', () => {
+      expect(deepEqual({ c: [2, 2], d: [{ a: 1 }], e: { a: 'toto' } }, object2, true)).toBe(false);
+      expect(deepEqual({ c: [1, 2], d: [{ a: 2 }], e: { a: 'toto' } }, object2, true)).toBe(false);
+      expect(deepEqual({ c: [1, 2], d: [{ a: 1 }], e: { a: 'titi' } }, object2, true)).toBe(false);
+      expect(deepEqual({ a: 2, c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' } }, object2, true)).toBe(false);
+      expect(deepEqual({ b: 2, c: [1, 2], d: [{ a: 1 }], e: { a: 'toto' } }, object2, true)).toBe(false);
+    });
+  });
+});
--- libs/core/src/lib/utils/deep-equal.ts
+++ libs/core/src/lib/utils/deep-equal.ts
@@ -4,15 +4,23 @@
  *
  * @param object1
  * @param object2
+ * @param deepIncluded: when true, check only if properties of object1 are all included and equivalent as the ones in object2. This way, object1 can have less properties than object2
  */
-export function deepEqual(object1: object, object2: object): boolean {
+export function deepEqual(object1: object, object2: object, deepIncluded?: boolean): boolean {
   // Get the objects keys
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
 
-  // Compares if the number of keys of the objects is different
-  if (keys1.length !== keys2.length) {
-    return false;
+  if (deepIncluded) {
+    // Check if the number of keys of object1 is smaller than the number of keys of object2
+    if (keys1.length > keys2.length) {
+      return false;
+    }
+  } else {
+    // Compares if the number of keys of the objects is different
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
   }
 
   // loop for-of: To loop through the values of an iterable object
@@ -32,7 +40,50 @@ export function deepEqual(object1: object, object2: object): boolean {
   return true;
 }
 
+/**
+ * Perform a deep comparison between two objects to determine if all the properties of the first object are equivalent to the ones included on the second object (which may have more properties).
+ * @param object1 object which should be fully included in the second one
+ * @param object2 object which should include all the properties of the first one
+ * @returns true if object1 is fully included in object2
+ */
+export function deepIncluded(object1: object, object2: object): boolean {
+  // Get the objects keys
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  // Compares if the number of keys of the objects is different
+  if (keys1.length >= keys2.length) {
+    return false;
+  }
+
+  // loop for-of: To loop through the values of an iterable object
+  for (const key of keys1) {
+    const value1 = (object1 as any)[key];
+    const value2 = (object2 as any)[key];
+
+    if (isObject(value1) && isObject(value2)) {
+      // Both are objects, so compare them using deepEqual
+      if (!deepEqual(value1, value2)) return false;
+    } else if (Array.isArray(value1) && Array.isArray(value2)) {
+      // Both are arrays, so compare length and each items using deepEqual
+      if (value1.length !== value2.length) {
+        return false;
+      } else {
+        return value1.every((prop, index) => deepEqual(prop, value2[index]));
+      }
+    } else {
+      // Both aren't objects nor array, so compare them using equality operator
+      if (value1 !== value2) return false;
+    }
+  }
+
+  return true;
+}
+
 // Helper function to check if the parameter is an object
 export function isObject(object: unknown): boolean {
   return object != null && typeof object === 'object';
+}
+export function isArray(object: unknown): boolean {
+  return object != null && Array.isArray(object);
 }
