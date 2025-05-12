277f0fefc633dedb242c53a1314d8bea1949f4cd
operramon <52449656+operramon@users.noreply.github.com>
2025-05-12 16:49:59 +0200

    [sc-11274] Manage models in the manager app (#2093)

---
+++ apps/manager-v2/src/app/manage-accounts/account-details/models/models.component.ts
@@ -0,0 +1,56 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SisModalService } from '@nuclia/sistema';
+import { filter, map, switchMap, take } from 'rxjs';
+import { ManagerStore } from '../../../manager.store';
+import { CommonModule } from '@angular/common';
+import { PaButtonModule, PaDropdownModule, PaPopupModule, PaTableModule } from '@guillotinaweb/pastanaga-angular';
+import { RegionalAccountService } from '../../regional-account.service';
+import { AccountModelSummary } from '../../regional-account.models';
+import { AccountService } from '../../account.service';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [CommonModule, PaButtonModule, PaDropdownModule, PaPopupModule, PaTableModule, RouterModule],
+  templateUrl: './models.component.html',
+  styleUrls: ['./models.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModelsComponent {
+  models = this.store.accountModels.pipe(map((models) => models.filter((zone) => zone.models.length > 0)));
+
+  constructor(
+    private store: ManagerStore,
+    private accountService: AccountService,
+    private regionalService: RegionalAccountService,
+    private modalService: SisModalService,
+  ) {
+    this.store.accountDetails
+      .pipe(
+        filter((details) => !!details),
+        take(1),
+        switchMap((details) => this.accountService.loadAccountModels(details.id)),
+      )
+      .subscribe();
+  }
+
+  delete(event: Event, model: AccountModelSummary, zone: string) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.modalService
+      .openConfirm({
+        title: `Delete "${model.title}"`,
+        description: `Are you sure you want to delete "${model.title}"?`,
+        isDestructive: true,
+        confirmLabel: 'Delete',
+      })
+      .onClose.pipe(
+        filter((confirm) => !!confirm),
+        switchMap(() => {
+          const accountId = this.store.getAccountId() || '';
+          return this.regionalService.deleteModel(model.model_id || '', accountId, zone);
+        }),
+        switchMap(() => this.accountService.loadAccountModels(this.store.getAccountId() || '')),
+      )
+      .subscribe();
+  }
+}
