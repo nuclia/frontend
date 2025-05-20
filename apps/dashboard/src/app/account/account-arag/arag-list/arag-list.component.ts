08e0765a7077c938947a04b91dd68f8adbd1b07e
Mat Pellerin <mpellerin42@gmail.com>
2025-05-20 16:57:24 +0200

    [sc-12323] ability to create agent from kb select (#2120)
    * Add arag creation button in KB select page
    
    * Manage arag creation limit

--- apps/dashboard/src/app/account/account-arag/arag-list/arag-list.component.ts
+++ apps/dashboard/src/app/account/account-arag/arag-list/arag-list.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FeaturesService, NavigationService, SDKService, STFUtils } from '@flaps/core';
 import {
   ModalService,
@@ -12,7 +12,13 @@ import {
 } from '@guillotinaweb/pastanaga-angular';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Account, IRetrievalAgentItem, RetrievalAgent, RetrievalAgentCreation } from '@nuclia/core';
-import { BadgeComponent, InfoCardComponent, SisProgressModule, SisToastService } from '@nuclia/sistema';
+import {
+  BadgeComponent,
+  InfoCardComponent,
+  SisProgressModule,
+  SisToastService,
+  StickyFooterComponent,
+} from '@nuclia/sistema';
 import { Subject, throwError } from 'rxjs';
 import { catchError, filter, map, switchMap, take } from 'rxjs/operators';
 import { CreateAragComponent } from '../create-arag/create-arag.component';
@@ -29,6 +35,7 @@ import { CreateAragComponent } from '../create-arag/create-arag.component';
     TranslateModule,
     PaTooltipModule,
     BadgeComponent,
+    StickyFooterComponent,
     InfoCardComponent,
   ],
   templateUrl: './arag-list.component.html',
@@ -44,6 +51,7 @@ export class AragListComponent implements OnInit, OnDestroy {
   private translate = inject(TranslateService);
   private navigation = inject(NavigationService);
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
   unsubscribeAll = new Subject<void>();
   isLoading = false;
@@ -54,11 +62,22 @@ export class AragListComponent implements OnInit, OnDestroy {
   aragUrl?: string;
 
   ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        take(1),
+        filter((params) => params['create']),
+      )
+      .subscribe(() => {
+        this.createArag();
+        this.router.navigate(['./'], { relativeTo: this.route, queryParams: {} });
+      });
+
     this.sdk.currentAccount
       .pipe(take(1))
       .pipe(
         switchMap((account) => {
           this.account = account;
+          // TODO: replace by max_arags once implemented in the backend
           this.maxRetrievalAgents = account.max_kbs;
           return this.sdk.aragList;
         }),
@@ -138,8 +157,12 @@ export class AragListComponent implements OnInit, OnDestroy {
           this.sdk.refreshAragList();
           this.setLoading(false);
         },
-        error: () => {
-          this.toaster.error('account.retrieval-agents.error.create');
+        error: (error) => {
+          const message =
+            error.status === 402
+              ? 'account.retrieval-agents.error.creation-limit'
+              : 'account.retrieval-agents.error.create';
+          this.toaster.error(message);
           this.setLoading(false);
         },
       });
