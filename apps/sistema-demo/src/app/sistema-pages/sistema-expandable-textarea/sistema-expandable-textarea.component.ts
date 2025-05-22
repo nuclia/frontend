d16ac232b81dc67f1c255c74635435956a1443d1
Mat Pellerin <mpellerin42@gmail.com>
2025-05-22 14:16:07 +0200

    Expandable textarea documentation (#2124)

---
+++ apps/sistema-demo/src/app/sistema-pages/sistema-expandable-textarea/sistema-expandable-textarea.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ExpandableTextareaComponent } from '@nuclia/sistema';
+import { PaDemoModule } from '../../../../../../libs/pastanaga-angular/projects/demo/src';
+
+@Component({
+  selector: 'nsd-sistema-expandable-textarea',
+  imports: [CommonModule, PaDemoModule, ExpandableTextareaComponent],
+  templateUrl: './sistema-expandable-textarea.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SistemaExpandableTextareaComponent {
+  codeExample = `<nsi-expandable-textarea
+  id="prompt"
+  formControlName="prompt"
+  rows="2"
+  resizable
+  placeholder="If…"
+  help="Provides more clues to extract parameters"
+  modalTitle="Extra prompt"
+  externalLabel>
+  Extra prompt
+</nsi-expandable-textarea>`;
+}
