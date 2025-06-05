b6e488c5d6850705fa2c3c040a055f6fe0a709a1
mpellerin42 <mpellerin42@gmail.com>
2025-06-05 15:41:59 +0200

    Improve how answers steps and context are displayed

--- libs/common/src/lib/retrieval-agent/agent-dashboard/workflow/sidebar/test-agent/elements/agent-context/agent-context.component.ts
+++ libs/common/src/lib/retrieval-agent/agent-dashboard/workflow/sidebar/test-agent/elements/agent-context/agent-context.component.ts
@@ -3,11 +3,11 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AragAnswerContext } from '@nuclia/core';
 import { LineBreakFormatterPipe } from 'libs/common/src/lib/pipes';
-import { ChipComponent } from '../chip';
+import { BlockquoteComponent } from '../blockquote';
 
 @Component({
   selector: 'stf-agent-context',
-  imports: [CommonModule, ChipComponent, LineBreakFormatterPipe, TranslateModule],
+  imports: [CommonModule, LineBreakFormatterPipe, TranslateModule, BlockquoteComponent],
   templateUrl: './agent-context.component.html',
   styleUrl: './agent-context.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
