a9bf4ae3a5e96406d5cc554aa1468bacc7705f77
mpellerin42 <mpellerin42@gmail.com>
2025-06-06 12:34:44 +0200

    Fix rephrase source display

--- libs/common/src/lib/retrieval-agent/agent-dashboard/workflow/nodes/rephrase/rephrase-node.component.ts
+++ libs/common/src/lib/retrieval-agent/agent-dashboard/workflow/nodes/rephrase/rephrase-node.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
 import { SDKService } from '@flaps/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { IKnowledgeBoxItem } from '@nuclia/core';
+import { NucliaDBDriver } from '@nuclia/core';
 import { switchMap, take } from 'rxjs';
 import { ConfigBlockComponent, ConfigBlockItem, NodeBoxComponent, NodeDirective } from '../../basic-elements';
 import { RephraseAgentUI } from '../../workflow.models';
@@ -16,30 +16,32 @@ import { RephraseAgentUI } from '../../workflow.models';
 export class RephraseNodeComponent extends NodeDirective implements OnInit {
   private sdk = inject(SDKService);
   private translate = inject(TranslateService);
-  private kbList = signal<IKnowledgeBoxItem[]>([]);
+  private driverList = signal<NucliaDBDriver[]>([]);
 
   rephraseConfig = computed<ConfigBlockItem[]>(() => {
     if (this.config()) {
       const config = this.config() as RephraseAgentUI;
-      const items: ConfigBlockItem[] = [];
-      if (config.kb) {
-        const kb = this.kbList().find((kb) => kb.id === config.kb);
-        items.push({
+      const driver = this.driverList().find((driver) => driver.identifier === config.kb);
+      const items: ConfigBlockItem[] = [
+        {
           title: this.translate.instant('retrieval-agents.workflow.node-types.rephrase.source'),
-          content: kb?.title || config.kb,
-        });
-      }
+          content: driver?.name || config.kb,
+        },
+      ];
       return items;
     }
     return [];
   });
 
   ngOnInit(): void {
-    this.sdk.currentAccount
+    this.sdk.currentArag
       .pipe(
         take(1),
-        switchMap((account) => this.sdk.nuclia.db.getKnowledgeBoxes(account.slug, account.id)),
+        switchMap((arag) => arag.getDrivers('nucliadb')),
       )
-      .subscribe((kbList) => this.kbList.set(kbList));
+      .subscribe((drivers) => {
+        this.driverList.set(drivers as NucliaDBDriver[]);
+        this.configUpdated.emit();
+      });
   }
 }
