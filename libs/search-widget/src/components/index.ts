78c0b8b02e0e2b24334fe0942f378d05657615c1
Eric BREHAULT <ebrehault@gmail.com>
2025-06-06 13:48:54 +0200

    rename into ResultsOrderButton to avoid naming conflict with type ResultsOrder

--- libs/search-widget/src/components/index.ts
+++ libs/search-widget/src/components/index.ts
@@ -2,7 +2,7 @@ export * from './answer';
 export { default as InfoCard } from './info-card/InfoCard.svelte';
 export * from './knowledge-graph';
 export * from './result-row';
-export { default as ResultsOrder } from './results-order/ResultsOrder.svelte';
+export { default as ResultsOrderButton } from './results-order/ResultsOrderButton.svelte';
 export { default as SearchInput } from './search-input/SearchInput.svelte';
 export { default as Suggestions } from './suggestions/Suggestions.svelte';
 export * from './viewer';
