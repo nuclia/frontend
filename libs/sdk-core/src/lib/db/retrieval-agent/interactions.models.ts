c043340b394322006b9203e9914207965cd4eb52
mpellerin42 <mpellerin42@gmail.com>
2025-06-23 17:36:14 +0200

    Update models

--- libs/sdk-core/src/lib/db/retrieval-agent/interactions.models.ts
+++ libs/sdk-core/src/lib/db/retrieval-agent/interactions.models.ts
@@ -23,9 +23,12 @@ export interface AragAnswer {
   answer: string | null;
   generated_text: string | null;
   step: AragAnswerStep | null;
+  possible_answer: string | null;
   context: AragAnswerContext | null;
   operation: AnswerOperation;
   seqid: number | null;
+  original_question_uuid: string | null;
+  actual_question_uuid: string | null;
 }
 
 export interface AragAnswerStep {
