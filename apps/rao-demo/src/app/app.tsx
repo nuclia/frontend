0bcdd1b0d297b7b95ce0fabe64c447e25c376d97
Eric BREHAULT <ebrehault@gmail.com>
2025-12-09 18:34:46 +0100

    use .env to store API KEY

--- apps/rao-demo/src/app/app.tsx
+++ apps/rao-demo/src/app/app.tsx
@@ -6,7 +6,7 @@ export function App() {
       <RaoWrapper
         aragid="b4fe9f16-d6b9-444c-a60a-6c4ae8a29d45"
         account="6787bcbe-cc1c-4134-85f6-324104d9e1a3"
-        apikey="eyJhbGciOiJSUzI1NiIsImtpZCI6InNhIiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwczovL2V1cm9wZS0xLnN0YXNoaWZ5LmNsb3VkLyIsImlhdCI6MTc2NDM3OTE1OSwic3ViIjoiMjBiYTA1N2EtNWZhYi00MzdlLThjYjctZjIzN2FjODdlZGYzIiwianRpIjoiZDU0MmU0ZTgtNTYxMS00M2MxLTk0YjktYWI3NzE0YThlZTdkIiwiZXhwIjoxNzk1OTE1MTU2LCJrZXkiOiI1ZGU2OTE2OS02ZGIxLTRhNGMtYjI1MC00MmZmYTJiODcyNzciLCJraWQiOiI5NTAxMzhjMi1kNzM5LTQ2OTUtOWU2Yy04YjNkYzk3NzZhMTIifQ.Hz0fyBELXj_e3ax9zjzsYn8IQA29GM9PpFFuzE_VeuY8eumtu9v_SvI1leTR1POhmXueIs9EACV0nbxhb3bu015CgEytWCjwGLEYMFjVJUMo1kOh5YZaufjyVvonoDqNlKk9hw2TRjQZRmXbVgDFmhHr7ruKH4WcIJNYTpm65OXruQzqfLaaMRcvkyP1Bw6bvNyxUw9zyDlkYxZc8M1W8AgFoCv4UmtfXzvLIt-LB0tI_A9mkvVTY8QKLSs5Yy0a5GR658pvPPy3gI7RRqQVVXAilaSoxqP5jndkLQ4AfOK2guGEyh7hX6stwbpRdFZ0biJIbqOyF53-Crdh8yH4Y4uSK5ExJq1UvEdPSa4wG_fcNqhdhfP3QmTuwynipfjkHvZiZZieFzrdUjtUDwoDzGm9fTSo4RkuV_R7Ni7cej5-wcP8h7SxVsOnwA7-xIZcFd-dPCdRwtxVZ-4AUnn8_UV-iUZRBZFLoki-Odb3DHrT-u8TDhAXd7sPUTaZ82GaMAam4zSGdPn4kycR_DwM0lCT0iT3hnxkTXuqrAJQrbhY3R9usEIWzZ_UJ0SUvBJA_i3MzpIpT-nx5TJ-gqGucA6cRDQjYcOH0kEqq9Lq-bPoP0fDf_ke65DneXA7aFhh75ujb-MjJXd4TcQ-7vSYMLyZU0lwn-ht75ZH_MNat34"
+        apikey={import.meta.env.VITE_API_KEY}
         zone="europe-1"
         backend="https://stashify.cloud/api"
         inputplaceholder="Ask Agentic RAG a question or make a request ..."
