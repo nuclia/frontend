eb94eefba66b774dc339861a4b7aea29cbe368cf
Eric BREHAULT <ebrehault@gmail.com>
2026-02-10 10:52:08 +0100

    rebase

--- apps/auth/src/app/lazy-user.module.ts
+++ apps/auth/src/app/lazy-user.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { UserModule, userRoutes } from '@nuclia/user';
-
-@NgModule({
-  imports: [UserModule, RouterModule.forChild(userRoutes)],
-=======
 import { authRoutes, AuthUserModule } from '@nuclia/user';
 
 @NgModule({
   imports: [AuthUserModule, RouterModule.forChild(authRoutes)],
->>>>>>> 0c3bc8f9 (login)
 })
 export class LazyUserModule {}
