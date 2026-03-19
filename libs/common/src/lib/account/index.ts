9b555d028d2fc38d0c2545653e40729b3f3b9e5b
operramon <oriol.perramon@progress.com>
2026-03-19 23:47:29 +0100

    Refactor assume role modal

--- libs/common/src/lib/account/index.ts
+++ libs/common/src/lib/account/index.ts
@@ -1,4 +1,5 @@
 export * from './account.module';
+export * from './assume-role-modal/assume-role-modal.component';
 export * from './metrics.service';
 export * from './account-status/account-status.component';
 export * from './billing/usage/budget.component';
