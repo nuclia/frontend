e69c7da75e93a2dc30474553ecfae72385a5a614
Eric BREHAULT <ebrehault@gmail.com>
2026-04-01 09:32:44 +0200

    split apart eager and lazy routes imports (#2713)

---
+++ apps/dashboard/src/app/app-routing.lazy.ts
@@ -0,0 +1,12 @@
+export {
+  AccountModule,
+  EntitiesModule,
+  LegacyMetricsModule,
+  MetricsModule,
+  ResourcesModule,
+  TASK_AUTOMATION_ROUTES,
+  UploadModule,
+  WIDGETS_ROUTES,
+} from '@flaps/common';
+
+export { LabelSetsModule } from '@flaps/core';
