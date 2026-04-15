233a9cccf2331c0c0c412d4f60e8956ea928e7ee
operramon <oriol.perramon@progress.com>
2026-04-15 19:38:26 +0200

    Show the Cowork Knowledge tier

--- libs/common/src/lib/account/billing/subscriptions/subscriptions.component.ts
+++ libs/common/src/lib/account/billing/subscriptions/subscriptions.component.ts
@@ -1,5 +1,16 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnDestroy } from '@angular/core';
-import { BehaviorSubject, combineLatest, filter, of, shareReplay, Subject, switchMap, takeUntil } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  filter,
+  map,
+  Observable,
+  of,
+  shareReplay,
+  Subject,
+  switchMap,
+  takeUntil,
+} from 'rxjs';
 import { AccountService, BillingService, Currency, FeaturesService } from '@flaps/core';
 import { SubscriptionService } from '../subscription.service';
 import { WINDOW } from '@ng-web-apis/common';
@@ -29,13 +40,16 @@ export class SubscriptionsComponent implements OnDestroy {
         customer ? this.billing.getCurrency(customer.billing_details?.country || '') : of(null),
       ),
     );
-  // temporarily hiding the starter plan
-  disableStarterPlan = true;
-  tiers: AccountTypes[] = ['v3starter', 'v3pro', 'v3enterprise'];
   isSubscribedToAws = this.billing.isSubscribedToAws;
   isManuallySubscribed = this.billing.isManuallySubscribed;
   unsubscribeAll = new Subject<void>();
   isTrial = this.features.isTrial;
+  isCowork = this.accountType.pipe(map((type) => type === 'cowork'));
+  tiers: Observable<AccountTypes[]> = this.isCowork.pipe(
+    map((isCowork) =>
+      isCowork ? ['cowork', 'v3starter', 'v3pro', 'v3enterprise'] : ['v3starter', 'v3pro', 'v3enterprise'],
+    ),
+  );
 
   constructor(
     private billing: BillingService,
