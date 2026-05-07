2f0473e50d186a59be1eddacd2335e11895bf243
ykulynych <gg.kulynych@gmail.com>
2026-05-07 15:02:27 +0200

    [sc-14638] Restored old activity page, renamed it to 'Detailed logs'

---
+++ libs/common/src/lib/metrics/activity/log.models.ts
@@ -0,0 +1,54 @@
+const maxLines = 6;
+const maxCharacters = 300;
+
+export interface LogValueString {
+  type: 'string';
+  value: string;
+  displayedValue: string;
+  showMore: boolean;
+}
+export interface LogValueObject {
+  type: 'object';
+  value: any;
+  displayedValue: string;
+  showMore: boolean;
+}
+
+export class LogEntry {
+  date: string;
+  id: string;
+  data: [string, LogValueString | LogValueObject][];
+
+  constructor(dataStr: string) {
+    try {
+      const data = JSON.parse(dataStr);
+      const { date, id, ...rest } = data;
+      this.date = date;
+      this.id = id;
+      this.data = Object.entries(rest).map(([key, value]) => {
+        if (typeof value === 'string') {
+          let displayedValue = value;
+          let showMore = false;
+          if (value.length > maxCharacters) {
+            displayedValue = value.slice(0, maxCharacters) + '…';
+            showMore = true;
+          }
+          return [key, { type: 'string', value, displayedValue, showMore }];
+        } else {
+          let displayedValue = JSON.stringify(value, null, 2);
+          let showMore = false;
+          const lines = displayedValue.split('\n');
+          if (lines.length > maxLines) {
+            displayedValue = lines.slice(0, maxLines).join('\n') + '…';
+            showMore = true;
+          }
+          return [key, { type: 'object', value, displayedValue, showMore }];
+        }
+      });
+    } catch (e) {
+      this.date = '';
+      this.id = '';
+      this.data = [];
+    }
+  }
+}
--- libs/common/src/lib/navbar/navbar.component.ts
+++ libs/common/src/lib/navbar/navbar.component.ts
@@ -183,7 +183,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   private isOnMetricsPage(path: string): boolean {
-    return /\/metrics\/(remi-analytics|usage-analytics|tokens-and-time-usage|resource-activity|search-activity)/.test(
+    return /\/metrics\/(remi-analytics|usage-analytics|tokens-and-time-usage|resource-activity|search-activity|detailed)/.test(
       path,
     );
   }
