cdebd71bf8802d03dd74aea4158bb8564777e539
Eric BREHAULT <ebrehault@gmail.com>
2026-05-21 14:14:51 +0200

    remove cookies banner, not needed here

--- libs/user/src/lib/signup/temporary/temporary-signup.component.ts
+++ libs/user/src/lib/signup/temporary/temporary-signup.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
-import { injectScript, SDKService } from '@flaps/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { SDKService } from '@flaps/core';
 import { PaButtonModule, PaTextFieldModule, PaTogglesModule } from '@guillotinaweb/pastanaga-angular';
 import { Subject } from 'rxjs';
 import { CommonModule } from '@angular/common';
@@ -13,22 +13,13 @@ import { UserContainerComponent } from '../../user-container';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, PaButtonModule, PaTextFieldModule, PaTogglesModule, TranslateModule, UserContainerComponent],
 })
-export class TemporaryContextBoxSignupComponent implements OnInit {
+export class TemporaryContextBoxSignupComponent {
   private sdk = inject(SDKService);
 
   unsubscribeAll = new Subject<void>();
   postUrl = `${this.sdk.nuclia.auth.getAuthUrl()}/signup/start`;
   appUrl = this.sdk.getOriginForApp('rag');
   agree = false;
-
-  ngOnInit(): void {
-    injectScript('https://cdn.cookielaw.org/consent/f9397248-1dbe-47fc-9dbf-c50e7dd51096-test/otSDKStub.js', [
-      {
-        key: 'data-domain-script',
-        value: 'f9397248-1dbe-47fc-9dbf-c50e7dd51096-test',
-      },
-    ]).subscribe();
-  }
 
   login() {
     this.sdk.nuclia.auth.redirectToOAuth();
