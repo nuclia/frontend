209c324aa150d7fea3924f6f0f2edfc311f79146
operramon <52449656+operramon@users.noreply.github.com>
2026-06-25 13:37:41 +0200

    [sc-15017] Simplify max parallelization (#2932)

--- libs/common/src/lib/ai-models/extraction/extraction-modal/extraction-modal.component.ts
+++ libs/common/src/lib/ai-models/extraction/extraction-modal/extraction-modal.component.ts
@@ -5,7 +5,10 @@ import { FeaturesService, SDKService } from '@flaps/core';
 import {
   ModalRef,
   PaButtonModule,
+  PaFocusableModule,
+  PaIconModule,
   PaModalModule,
+  PaPopupModule,
   PaTextFieldModule,
   PaTogglesModule,
 } from '@guillotinaweb/pastanaga-angular';
@@ -13,28 +16,16 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ExtractConfig, ExtractVLLMConfig, GenerativeProviders, LearningConfigurations } from '@nuclia/core';
 import { InfoCardComponent } from '@nuclia/sistema';
 import { LLMConfigurationComponent } from '../llm-configuration/llm-configuration.component';
-import {
-  BehaviorSubject,
-  catchError,
-  combineLatest,
-  defer,
-  distinctUntilChanged,
-  forkJoin,
-  map,
-  Observable,
-  of,
-  shareReplay,
-  startWith,
-  switchMap,
-  take,
-} from 'rxjs';
 
 @Component({
   imports: [
     CommonModule,
     InfoCardComponent,
     PaButtonModule,
+    PaFocusableModule,
+    PaIconModule,
     PaModalModule,
+    PaPopupModule,
     PaTextFieldModule,
     PaTogglesModule,
     ReactiveFormsModule,
@@ -58,11 +49,11 @@ export class ExtractionModalComponent implements OnInit {
   aiTablesEnabled = this.features.authorized.aiTableProcessing;
   visualLLMEnabled = this.features.authorized.visualLLMProcessing;
 
-  vllmConfig = new BehaviorSubject<ExtractVLLMConfig | undefined>(undefined);
-  vllmConfigStatus = new BehaviorSubject<FormControlStatus | undefined>(undefined);
+  vllmConfig?: ExtractVLLMConfig;
+  vllmConfigStatus?: FormControlStatus;
 
-  aiTables = new BehaviorSubject<ExtractVLLMConfig | undefined>(undefined);
-  aiTablesStatus = new BehaviorSubject<FormControlStatus | undefined>(undefined);
+  aiTables?: ExtractVLLMConfig;
+  aiTablesStatus?: FormControlStatus;
 
   configForm = new FormGroup({
     name: new FormControl<string>('', { validators: [Validators.required], nonNullable: true }),
@@ -75,25 +66,6 @@ export class ExtractionModalComponent implements OnInit {
     name: { required: 'validation.required' },
   };
 
-  showMaxParallelCalls = combineLatest([
-    this.features.isAccountManager,
-    defer(() => this.configForm.controls.vllm_config.valueChanges.pipe(startWith(this.vllm_config))),
-    defer(() => this.configForm.controls.ai_tables.valueChanges.pipe(startWith(this.ai_tables))),
-    this.vllmConfig.pipe(
-      map((config) => config?.llm?.generative_model),
-      distinctUntilChanged(),
-    ),
-    this.aiTables.pipe(
-      map((config) => config?.llm?.generative_model),
-      distinctUntilChanged(),
-    ),
-  ]).pipe(
-    switchMap(([isAccountManager, vllmEnabled, aiTablesEnabled, vllmModel, aiTablesModel]) =>
-      isAccountManager ? this.allModelsHaveKeys(vllmEnabled, aiTablesEnabled, vllmModel, aiTablesModel) : of(false),
-    ),
-    shareReplay(1),
-  );
-
   get vllm_config() {
     return this.configForm.controls.vllm_config.value;
   }
@@ -105,8 +77,8 @@ export class ExtractionModalComponent implements OnInit {
   get invalid() {
     return (
       this.configForm.invalid ||
-      (this.vllm_config && this.vllmConfigStatus.value !== 'VALID') ||
-      (this.ai_tables && this.aiTablesStatus.value !== 'VALID')
+      (this.vllm_config && this.vllmConfigStatus !== 'VALID') ||
+      (this.ai_tables && this.aiTablesStatus !== 'VALID')
     );
   }
 
@@ -138,63 +110,21 @@ export class ExtractionModalComponent implements OnInit {
     if (!this.configForm.valid) {
       return;
     }
-    this.showMaxParallelCalls.pipe(take(1)).subscribe((showParallelCalls) => {
-      const values = this.configForm.getRawValue();
-      const payload: ExtractConfig = {
-        name: values.name,
-      };
-      if (showParallelCalls) {
-        payload.max_parallel_llm_calls = values.max_parallel_llm_calls;
-      }
-      if (values.vllm_config) {
-        payload.vllm_config = this.vllmConfig.value;
-      }
-      if (values.ai_tables) {
-        payload.ai_tables = this.aiTables.value;
-      }
-      this.modal.close(payload);
-    });
+    const values = this.configForm.getRawValue();
+    const payload: ExtractConfig = {
+      name: values.name,
+      max_parallel_llm_calls: values.max_parallel_llm_calls,
+    };
+    if (values.vllm_config) {
+      payload.vllm_config = this.vllmConfig;
+    }
+    if (values.ai_tables) {
+      payload.ai_tables = this.aiTables;
+    }
+    this.modal.close(payload);
   }
 
   close(): void {
     this.modal.close();
-  }
-
-  allModelsHaveKeys(
-    vllmEnabled: boolean,
-    aiTablesEnabled: boolean,
-    vllmModel?: string,
-    aiTablesModel?: string,
-  ): Observable<boolean> {
-    if ((vllmEnabled && vllmModel) || (aiTablesEnabled && aiTablesModel)) {
-      const vllmHasKey = vllmModel ? this.isCustomModelWithKeys(vllmModel) : of(false);
-      const aiTablesHasKey = aiTablesModel ? this.isCustomModelWithKeys(aiTablesModel) : of(false);
-      return forkJoin([vllmHasKey, aiTablesHasKey]).pipe(
-        map(
-          ([vllmHasKey, aiTablesHasKey]) =>
-            (!vllmEnabled || (vllmEnabled && vllmHasKey)) && (!aiTablesEnabled || (aiTablesEnabled && aiTablesHasKey)),
-        ),
-      );
-    } else {
-      return of(false);
-    }
-  }
-
-  isCustomModelWithKeys(model: string): Observable<boolean> {
-    const customModel = Object.values(this.providers['default']?.models).find((modelInfo) => modelInfo.name === model);
-    return customModel
-      ? this.sdk.currentAccount.pipe(
-          take(1),
-          switchMap((modelConfig) =>
-            this.sdk.nuclia.db.getModelConfiguration(
-              customModel.name.split('/')[1],
-              modelConfig.id,
-              this.sdk.nuclia.options.zone || '',
-            ),
-          ),
-          map((model) => !!model.user_keys),
-          catchError(() => of(false)),
-        )
-      : of(false);
   }
 }
