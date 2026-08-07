288e97eaebc3c49153d764ad9ae6a8af93ffb95c
ykulynych <gg.kulynych@gmail.com>
2026-08-07 09:48:24 +0200

    [sc-15263] Show folder section on sharpoint connection info page

--- libs/sync/src/lib/logic/connectors/sharepoint.ts
+++ libs/sync/src/lib/logic/connectors/sharepoint.ts
@@ -1,4 +1,4 @@
-import { Observable, of } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { OAuthConnector } from './oauth';
 import { Section } from '../models';
 
@@ -10,49 +10,49 @@ export class SharepointImpl extends OAuthConnector {
   }
 
   override getParametersSections(): Observable<Section[]> {
-    return of([
-      {
-        id: 'credentials',
-        title: 'sync.connectors.oauth.credentials.title',
-        fields: [
-          {
-            id: 'tenant_id',
-            label: 'sync.connectors.sharepoint.tenant_id.label',
-            type: 'text',
-            required: true,
-          },
-          {
-            id: 'client_id',
-            label: 'sync.connectors.sharepoint.client_id.label',
-            type: 'text',
-            required: true,
-          },
-          {
-            id: 'pfx_base64',
-            label: 'sync.connectors.sharepoint.pfx_base64.label',
-            help: 'sync.connectors.sharepoint.pfx_base64.help',
-            type: 'file',
-            required: true,
-            accept: '.pfx',
-            handleFile: this.handlePfx,
-          },
-          {
-            id: 'pfx_password',
-            label: 'sync.connectors.sharepoint.pfx_password.label',
-            help: 'sync.connectors.sharepoint.pfx_password.help',
-            type: 'text',
-            required: true,
-            secret: true,
-          },
-          {
-            id: 'permissions',
-            label: 'sync.connectors.sharepoint.permissions.label',
-            help: 'sync.connectors.sharepoint.permissions.help',
-            type: 'hint',
-          },
-        ],
-      },
-    ]);
+    const credentialsSection: Section = {
+      id: 'credentials',
+      title: 'sync.connectors.oauth.credentials.title',
+      fields: [
+        {
+          id: 'tenant_id',
+          label: 'sync.connectors.sharepoint.tenant_id.label',
+          type: 'text',
+          required: true,
+        },
+        {
+          id: 'client_id',
+          label: 'sync.connectors.sharepoint.client_id.label',
+          type: 'text',
+          required: true,
+        },
+        {
+          id: 'pfx_base64',
+          label: 'sync.connectors.sharepoint.pfx_base64.label',
+          help: 'sync.connectors.sharepoint.pfx_base64.help',
+          type: 'file',
+          required: true,
+          accept: '.pfx',
+          handleFile: this.handlePfx,
+        },
+        {
+          id: 'pfx_password',
+          label: 'sync.connectors.sharepoint.pfx_password.label',
+          help: 'sync.connectors.sharepoint.pfx_password.help',
+          type: 'text',
+          required: true,
+          secret: true,
+        },
+        {
+          id: 'permissions',
+          label: 'sync.connectors.sharepoint.permissions.label',
+          help: 'sync.connectors.sharepoint.permissions.help',
+          type: 'hint',
+        },
+      ],
+    };
+
+    return super.getParametersSections().pipe(map((folderSections) => [credentialsSection, ...folderSections]));
   }
 
   private handlePfx(file: File): Observable<string> {
