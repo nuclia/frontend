9a36736daa5b1d7e06f8923d93dba3a373f61b63
Mat Pellerin <mpellerin42@gmail.com>
2026-09-07 09:40:41 +0200

    Remove platform app from nx workspace

--- apps/platform/src/app/random/random.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pla-random',
-  templateUrl: 'random.component.html',
-  styleUrls: ['./random.component.scss'],
-})
-export class RandomComponent {}
