1f1f83d823a030395fac6ef8268a4cc94f508ca1
Mat Pellerin <mpellerin42@gmail.com>
2026-09-21 10:53:50 +0200

    Fix build errors

--- libs/common/src/lib/resources/resource-list/title-cell/title-cell.component.ts
+++ libs/common/src/lib/resources/resource-list/title-cell/title-cell.component.ts
@@ -3,13 +3,13 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PaIconModule } from '@guillotinaweb/pastanaga-angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { SisIconsModule } from '@nuclia/sistema';
+import { BadgeComponent, SisIconsModule } from '@nuclia/sistema';
 import { isMemoryResource } from '../../memory/memory.helpers';
 import { ResourceWithLabels } from '../resource-list.model';
 
 @Component({
   selector: 'stf-title-cell',
-  imports: [RouterModule, PaIconModule, SisIconsModule, TranslateModule],
+  imports: [RouterModule, PaIconModule, SisIconsModule, TranslateModule, BadgeComponent],
   templateUrl: './title-cell.component.html',
   styleUrl: './title-cell.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
